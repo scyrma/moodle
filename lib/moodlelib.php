b98ba0f5b740ef4cdeb49c90f1a1a9f658e77ba7
Andrew Nicols <andrew@nicols.co.uk>
2015-04-01 09:40:47 +0800

    HOST: Add support for overriding mail Sender field

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -5991,8 +5991,10 @@ function email_to_user($user, $from, $subject, $messagetext, $messagehtml = '', 
         $noreplyaddress = $noreplyaddressdefault;
     }
 
-    // Make up an email address for handling bounces.
-    if (!empty($CFG->handlebounces)) {
+    if ($CFG->mailsender) {
+        $mail->Sender = $CFG->mailsender;
+    } else if (!empty($CFG->handlebounces)) {
+        // Make up an email address for handling bounces.
         $modargs = 'B'.base64_encode(pack('V', $user->id)).substr(md5($user->email), 0, 16);
         $mail->Sender = generate_email_processing_address(0, $modargs);
     } else {
