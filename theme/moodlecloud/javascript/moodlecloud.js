c6831f542164c7246645bf88f751ba63414e8a25
Andrew Nicols <andrew@nicols.co.uk>
2015-05-29 14:58:48 +0800

    Remove unnecessary globals in theme JS

--- theme/moodlecloud/javascript/moodlecloud.js
+++ theme/moodlecloud/javascript/moodlecloud.js
@@ -1,8 +1,6 @@
-function detectAdblock() {
+(function() {
     var div = document.getElementById('moodlefree_ad');
     if (div && div.clientHeight == 0) {
         div.innerHTML = 'ad block detected';
     }
-}
-
-detectAdblock();
+}());
