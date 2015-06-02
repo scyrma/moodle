905a050fc0aae6540d740036cb2955e4fc605118
Andrew Nicols <andrew@nicols.co.uk>
2015-06-02 15:39:49 +0800

    Add auth_moodlecloud to admin user on post-provision

--- local/moodlecloud/cli/post-provision.php
+++ local/moodlecloud/cli/post-provision.php
@@ -7,5 +7,11 @@ require_once(dirname(dirname(dirname(__DIR__))) . '/config.php');
 use local_moodlecloud\tasks\register as register_adhoc_task;
 use core\task\manager;
 
-var_dump(new register_adhoc_task());
+// The primary admin user should use moodlecloud authentication.
+if ($user = get_admin()) {
+    $user->auth = 'moodlecloud';
+    $DB->update_record($user);
+}
+
+// Setup the registration task.
 manager::queue_adhoc_task(new register_adhoc_task());
