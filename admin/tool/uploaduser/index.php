a7458b369fa80769776f4797b379c6e766c96413
Dan Poltawski <dan@moodle.com>
2015-06-03 12:11:13 +0100

    HOST-31 Enforce quota restrictions in user upload tool

--- admin/tool/uploaduser/index.php
+++ admin/tool/uploaduser/index.php
@@ -184,6 +184,10 @@ if ($formdata = $mform2->is_cancelled()) {
     $usersskipped  = 0;
     $weakpasswords = 0;
 
+    // BEGIN MOODLECLOUD HACK.
+    $cloudquota = core_hack_moodlecloud::number_of_user_slots_remaining();
+    // END MOODLECLOUD HACK.
+
     // caches
     $ccache         = array(); // course cache - do not fetch all courses here, we  will not probably use them all anyway!
     $cohorts        = array();
@@ -708,6 +712,14 @@ if ($formdata = $mform2->is_cancelled()) {
             }
 
         } else {
+            // BEGIN MOODLECLOUD HACK.
+            if ($usersnew >= $cloudquota) {
+                $upt->track('status', 'Quota limits have been exceeded.', 'error');
+                $userserrors++;
+                continue;
+            }
+            // END MOODLECLOUD HACK.
+
             // save the new user to the database
             $user->confirmed    = 1;
             $user->timemodified = time();
