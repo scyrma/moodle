a739f6052e2e04228d9fecc0df7a8a99abfdbd7f
Andrew Nicols <andrew@nicols.co.uk>
2015-06-11 15:27:54 +0800

    local_moodlecloud: Add script to fetch site name

---
+++ local/moodlecloud/sitename.php
@@ -0,0 +1,20 @@
+<?php
+
+define('AJAX_SCRIPT', true);
+require_once(dirname(dirname(__DIR__)) . '/config.php');
+
+$token = required_param('token', PARAM_ALPHANUM);
+
+if (isset($CFG->statistics_tokens) && is_array($CFG->statistics_tokens)) {
+    $validtokens = $CFG->statistics_tokens;
+} else {
+    $validtokens = array();
+}
+
+if (!in_array($token, $validtokens)) {
+    throw new require_login_exception('Invalid user token');
+}
+
+echo json_encode(array(
+        'sitename' => $SITE->fullname,
+    ));
