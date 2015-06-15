93f4c054e1be8f3d02986bf886721f0e3a41d77b
Andrew Davis <andrew@moodle.com>
2015-06-15 21:20:29 +0800

    added readspeaker to the footer

--- theme/moodlecloud/layout/columns2.php
+++ theme/moodlecloud/layout/columns2.php
@@ -90,6 +90,7 @@ echo $OUTPUT->doctype() ?>
         </p>
         <?php
         echo theme_moodlecloud_get_ad($OUTPUT->page->context);
+        echo theme_moodlecloud_get_readspeaker();
         echo $html->footnote;
         echo $OUTPUT->login_info();
         echo $OUTPUT->home_link();
