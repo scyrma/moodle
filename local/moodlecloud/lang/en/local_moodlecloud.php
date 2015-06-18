024df8610408b1cae751d9e0cfe5c243c04a60f6
Andrew Nicols <andrew@nicols.co.uk>
2015-06-18 14:45:40 +0800

    HOST-31/36: Sweeping changes to moodlecloud user + auth hacks
    * Move hacks from core_hack_moodlecloud to local_moodlecloud\restrictions
    * Break hacks apart into individual files
    * Reduce scope of capibility check
    * Ensure that user quotaing is configured
    * Throw over-quota exceptions in a single location

--- local/moodlecloud/lang/en/local_moodlecloud.php
+++ local/moodlecloud/lang/en/local_moodlecloud.php
@@ -24,3 +24,4 @@
 
 $string['pluginname'] = 'MoodleCloud';
 $string['reporting_task'] = 'MoodleCloud Statistics Reporting';
+$string['userquotahit'] = 'You have reached your quota for the number of users you may have on your site.';
