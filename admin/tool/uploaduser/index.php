b1354e8354a55f45eadf511af19d3fe42a821e24
Andrew Nicols <andrew@nicols.co.uk>
2015-06-18 14:45:40 +0800

    HOST-31/36: Sweeping changes to moodlecloud user + auth hacks
    * Move hacks from core_hack_moodlecloud to local_moodlecloud\restrictions
    * Break hacks apart into individual files
    * Reduce scope of capibility check
    * Ensure that user quotaing is configured
    * Throw over-quota exceptions in a single location

--- admin/tool/uploaduser/index.php
+++ admin/tool/uploaduser/index.php
@@ -186,7 +186,7 @@ if ($formdata = $mform2->is_cancelled()) {
     $weakpasswords = 0;
 
     // BEGIN MOODLECLOUD HACK.
-    $cloudquota = core_hack_moodlecloud::number_of_user_slots_remaining();
+    $cloudquota = local_moodlecloud\restrictions\userquota::number_of_user_slots_remaining();
     // END MOODLECLOUD HACK.
 
     // caches
@@ -726,7 +726,7 @@ if ($formdata = $mform2->is_cancelled()) {
 
         } else {
             // BEGIN MOODLECLOUD HACK.
-            if ($usersnew >= $cloudquota) {
+            if ($cloudquota && $usersnew >= $cloudquota) {
                 $upt->track('status', 'Quota limits have been exceeded.', 'error');
                 $userserrors++;
                 continue;
