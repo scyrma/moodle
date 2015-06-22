0be84fbc84d79c7dc73c3a3a92942b9245f11174
Andrew Nicols <andrew@nicols.co.uk>
2015-06-22 12:37:18 +0800

    auth_moodlecloud: Add SSO hack for first login

---
+++ auth/moodlecloud/classes/sso.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace auth_moodlecloud;
+
+defined('MOODLE_INTERNAL') || die();
+
+class sso {
+
+    private static $cookiename = 'MoodleCloudSSO';
+
+    public static function require_login() {
+        self::attempt_cookie_login();
+    }
+
+    private static function attempt_cookie_login() {
+        global $DB, $_COOKIE;
+
+        $attemptlogin = false;
+
+        // Only allow users with moodlecloud auth to log in.
+        $admin = $DB->get_record('user', array('auth' => 'moodlecloud'));
+
+        // Only allow the admin user in this way on their first login for the moment.
+        // In the future we will create a more established SSO system.
+        if (!empty($admin->firstaccess) || !empty($admin->lastaccess) || !empty($admin->lastlogin)) {
+            return;
+        }
+
+        if (isset($_COOKIE[self::$cookiename])) {
+            $cookievalue = $_COOKIE[self::$cookiename];
+
+            // Unset it now.
+            $ssodomain  = get_config('auth_moodlecloud', 'ssodomain');
+            setcookie(self::$cookiename, $cookievalue, time() - 3600, '/');
+
+            // Check the value of the cookie against the SSO source.
+            $attemptlogin = helper::call('ssoin', array(
+                    'publictoken'       => $cookievalue,
+                ));
+        }
+
+        if ($attemptlogin) {
+            complete_user_login($admin);
+            return true;
+        }
+    }
+
+}
