75d17a98edd25c303f1194653b532f5c4d906c05
Andrew Davis <andrew@moodle.com>
2015-06-24 05:45:59 +0800

    centered teacher ad, changed FAQ link display text

--- theme/moodlecloud/lang/en/theme_moodlecloud.php
+++ theme/moodlecloud/lang/en/theme_moodlecloud.php
@@ -55,7 +55,7 @@ $string['contentbackground_desc'] = 'The background colour of the main content o
 $string['customcss'] = 'Custom CSS';
 $string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
 
-$string['faq'] = 'Free Moodle FAQ';
+$string['faq'] = 'MoodleCloud FAQ';
 $string['footnote'] = 'Footnote';
 $string['footnotedesc'] = 'Whatever you add to this textarea will be displayed in the footer throughout your Moodle site.';
 
