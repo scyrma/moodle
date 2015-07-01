b558076433962d307147f0c7bd30071fd69f6c9c
Andrew Nicols <andrew@nicols.co.uk>
2015-07-01 10:05:10 +0800

    theme_moodlecloud: Fix adblock detector

--- theme/moodlecloud/javascript/moodlecloud.js
+++ theme/moodlecloud/javascript/moodlecloud.js
@@ -1,11 +1,34 @@
-(function() {
-
+require(['core/str', 'jquery', 'core/config'], function(str, $, config) {
     var checkFunction = function() {
-        var div = document.getElementById('moodlecloud_ad');
-        if (div && div.clientHeight === 0) {
-            div.innerHTML = 'ad block detected';
+        var ads = $('#moodlecloud_ad');
+        if (ads.length && ads.height() === 0 && !ads.data('notified')) {
+            YUI().use('moodle-core-notification-alert', function() {
+                str.get_strings([
+                    {
+                        key: 'adunblock_title',
+                        component: 'theme_moodlecloud'
+                    },
+                    {
+                        key: 'adunblock_message',
+                        component: 'theme_moodlecloud'
+                    }
+                ]).done(function(strings) {
+                    new M.core.alert({
+                            'title': strings[0],
+                            message: strings[1]
+                        });
+
+                        var settings = {
+                            sesskey: config.sesskey
+                        };
+                        $.post(config.wwwroot + '/theme/moodlecloud/adsblocked.php', settings);
+                        ads.data('notified', true);
+                });
+            });
         }
     };
 
-    checkFunction();
-}());
+    // Defer the call to the adunblock check by five seconds to account for
+    // some network latency in serving the ads.
+    setTimeout(checkFunction, 5000);
+});
