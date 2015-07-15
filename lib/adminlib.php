401a9f1934f737cdd7fe02a0ec2356be92910bed
Andrew Nicols <andrew@nicols.co.uk>
2015-07-15 12:04:29 +0800

    auth: Add support for blocked auth plugins

--- lib/adminlib.php
+++ lib/adminlib.php
@@ -6493,6 +6493,17 @@ class admin_setting_manageauths extends admin_setting {
             }
         }
 
+// START MOODLECLOUD HACK.
+if (isset($CFG->moodlecloud_blocked_auth)) {
+    foreach ($CFG->moodlecloud_blocked_auth as $k => $mcblocked) {
+        if (!$mcblocked) {
+            continue;
+        }
+        unset($displayauths[$k]);
+    }
+}
+// END MOODLECLOUD HACK.
+
         $return = $OUTPUT->heading(get_string('actauthhdr', 'auth'), 3, 'main');
         $return .= $OUTPUT->box_start('generalbox authsui');
 
