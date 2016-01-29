dfaf41aeb1e3b00e90dbb861c842e0410a9d4489
Ryan Wyllie <ryan@moodle.com>
2016-01-29 04:40:25 +0000

    HQ-944 theme_school: Initial commit
    Added in Tikli theme.

---
+++ theme/tikli/version.php
@@ -0,0 +1,10 @@
+<?php
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2015101302;
+$plugin->release   = '2.0.2';
+$plugin->requires  = 2014051201;
+$plugin->component = 'theme_tikli';
+$plugin->dependencies = array(
+    'theme_bootstrapbase'  => 2014051200,
+);
