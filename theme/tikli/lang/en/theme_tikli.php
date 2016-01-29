9502ee87df64c8c148ae74ce569ebe54177118ba
Ryan Wyllie <ryan@moodle.com>
2016-01-29 08:26:16 +0000

    HQ-944 theme_school: Removed lots from frontpage
    Removed lots of sections from the frontpage and removed the
    associated settings and language strings.

--- theme/tikli/lang/en/theme_tikli.php
+++ theme/tikli/lang/en/theme_tikli.php
@@ -28,10 +28,10 @@ $string['region-side-pre'] = 'Left';
 /*logo*/
 $string['logo'] = 'Logo';
 $string['logodesc'] = 'You may add the logo to be displayed on the header. Note- Preferred aspect ratio is 6:1. In case you wish to customise, you can do so from the     custom CSS box below.';
-$string['logobackgroundimage'] = 'Logo Background Image'; 
+$string['logobackgroundimage'] = 'Logo Background Image';
 $string['logobackgroundimagedesc'] = 'You may add a background image for the logo.';
 $string['logoorsitename'] = 'Choose site logo format';
-$string['logoorsitenamedesc'] = 'You may customise how the site header logo looks like. The options available are: Logo - Only the logo will be shown; Sitename - Only the sitename will be shown; Icon+sitename - An icon along with the sitename will be shown.';//
+$string['logoorsitenamedesc'] = 'You may customise how the site header logo looks like. The options available are: Logo - Only the logo will be shown; Sitename - Only the sitename will be shown; Icon+sitename - An icon along with the sitename will be shown.';
 $string['onlylogo'] = 'Logo Only';
 $string['onlysitename'] = 'Sitename Only';
 $string['iconsitename'] = 'Icon and sitename both';
@@ -167,7 +167,7 @@ $string['slideimagedesc'] = 'You may insert the necessary images for the slider 
 $string['slidertext'] = 'Add Slider text';
 $string['slidertextdesc'] = 'You may insert the text content of your slider. Preferably in HTML.';
 $string['sliderurl'] = 'Slider text button link';
-$string['sliderbuttontext'] = 'Add Text button on slider'; 
+$string['sliderbuttontext'] = 'Add Text button on slider';
 $string['sliderbuttontextdesc'] = 'You may insert a text button on your slider.';
 $string['sliderurldesc'] = 'You may insert the link of the page where the user will be redirected at once they click on the text button.';
 $string['slideinterval'] = 'Slide interval';
@@ -215,76 +215,6 @@ $string['thirdsubsectioncolumnsecondtextdesc'] = ' ';
 $string['thirdsubsectioncolumnlink'] = 'Add URL';
 $string['thirdsubsectioncolumnlinkdesc'] = ' ';
 
-
-/*******************************parallaxcountersection*************************************/
-
-$string['parallaxcountersection'] = 'Number counter';
-$string['parallaxcountersectiondesc'] = ' ';
-
-$string['parallaxcounterheading'] = 'Add heading ';
-$string['parallaxcounterheadingdesc'] = ' ';
-
-$string['parallaxcountersubheading'] = 'Add sub heading ';
-$string['parallaxcountersubheadingdesc'] = ' ';
-
-$string['parallaxcountercount'] = 'No of counter ';
-$string['parallaxcountercountdesc'] = 'Maximum 4';
-
-$string['parallaxcountercountnumber'] = 'Enter Number ';
-$string['parallaxcountercountnumberdesc'] = '';
-
-$string['parallaxcountercountnumbertext'] = 'Add text';
-$string['parallaxcountercountnumbertextdesc'] = ' ';
-
-$string['parallaxbackgroundimage'] = 'Upload background image.';
-$string['parallaxbackgroundimagedesc'] = ' ';
-
-/***************************staticnumbersection********************************/
-$string['staticnumbersection'] = 'Facts';
-$string['staticnumbersectiondesc'] = '';
-
-$string['staticnumbersectioncount'] = 'Number of facts';
-$string['staticnumbersectioncountdesc'] = 'Max 4';
-
-$string['staticnumberheading'] = 'Add heading';
-$string['staticnumberheadingdesc'] = '';
-
-$string['staticnumber'] = 'Add some number or text';
-$string['staticnumberdesc'] = '';
-
-$string['staticnumbertext'] = 'Add text';
-$string['staticnumbertextdesc'] = '';
-
-$string['staticnumbersubtext'] = 'Add sub text';
-$string['staticnumbersubtextdesc'] = '';
-/****************************map section start*********************************/
-$string['map'] = 'Map section';
-$string['mapdesc'] = '';
-
-$string['place'] = 'Enter your place (city/town/village)';
-$string['placedesc'] = '';
-$string['placevalue'] = 'Kolkata';
-
-
-$string['country'] = 'Enter your country';
-$string['countrydesc'] = '';
-$string['countryvalue'] = 'India';
-/****************************map section end**********************************/
-/****************************contact support and email start******************************/
-$string['support'] = 'Enter some information';
-$string['supportdesc'] = 'Enter some information';
-
-$string['somesupportinfo'] = 'Add Some information';
-$string['somesupportinfodesc'] = '';
-
-$string['someinfoimage'] = 'Upload image';
-$string['someinfoimagedesc'] = '';
-$string['contactemailsection'] = 'Contact section';
-$string['contactemailsectiondesc'] = '';
-$string['emailcontact'] = 'Email';
-$string['emailcontactdesc'] = '';
-$string['emailsubject'] = 'Email subject';
-$string['emailsubjectdesc'] = 'You may add the subject for your email.';
 /*block section 1*/
 $string['frontpageblocksection1'] = 'Body section 1 title';
 $string['frontpageblocksectiondesc1'] = 'This is where you may add the text for section 1';
@@ -306,141 +236,6 @@ $string['frontpageblocklinksection3'] = 'Body section 3 link';
 $string['frontpageblocklinksectiondesc3'] = 'Here, you may insert the link at which the user will be redirected at.';
 $string['frontpageblockdescriptionsection3'] = 'Body section 3 description';
 $string['frontpageblockdescriptionsectiondesc3'] = 'A brief description in the context of the title.';
-$string['contactwithusheading'] = '‘Contact Us’ text customisation';
-$string['contactwithusheadingdesc'] = 'You may change the text content here. The default is ‘Contact Us’.';
-$string['contactwithusfontawesomeicon'] = 'Upload the ‘Contact Us’ icon';
-$string['contactwithusfontawesomeicondesc'] = 'You may add an icon to be displayed beside the text ‘ Contact Us’.';
-$string['contactwithus'] = 'Footer column 3 (Right column) [Contact Us]';
-$string['contactwithusdesc'] = '';
-$string['socialfontawesomeicon1'] = 'Upload social link icon 1';
-$string['socialfontawesomeicondesc1'] = 'You may upload the icon for social link 1.';
-$string['socialfontawesomeicon2'] = 'Upload social link icon 2';
-$string['socialfontawesomeicondesc2'] = 'You may upload the icon for social link 2.';
-$string['socialfontawesomeicon3'] = 'Upload social link icon 3';
-$string['socialfontawesomeicondesc3'] = 'You may upload the icon for social link 3.';
-
-$string['socialfontawesomeicon4'] = 'Upload social link icon 4';
-$string['socialfontawesomeicondesc4'] = 'You may upload the icon for social link 4.';
-$string['socialicon'] = 'Social link for icon';
-$string['socialicondesc'] = 'You may add a link at which the user will be redirected once they click on the social link icon ';
-
-$string['addressfontawesomeicon'] = 'Upload your Address icon';
-$string['addressfontawesomeicondesc'] = 'You may upload an icon for your address';
-$string['address'] = 'Enter Address';
-$string['addressdesc'] = 'You may enter your Address.';
-$string['someinfo'] = 'You may add some brief text for footer column 3 ';
-$string['someinfodesc'] = '';
-$string['phonefontawesomeicon'] = 'Upload your Phone icon';
-$string['phonefontawesomeicondesc'] = 'Upload your Phone icon';
-$string['phone'] = 'Enter Phone Number';
-$string['phonedesc'] = 'You may enter your Phone Number.';
-
-$string['emailfontawesomeicon'] = 'Upload your Email icon';
-$string['emailfontawesomeicondesc'] = 'Upload your Email icon';
-$string['email'] = 'Enter Email Address';
-$string['emaildesc'] = 'You may enter your Email.';
-
-$string['marketspotfontawesomeicon'] = 'Footer column 1 icon';
-$string['marketspotfontawesomeicondesc'] = 'You may add an icon for footer column 1.';
-$string['marketspot'] = 'Footer Content column 1';
-$string['marketspotdesc'] = 'This section relates to the bottom portion (column 1) of your frontpage.';
-$string['marketspothead'] = 'Footer column title';
-$string['marketspotheaddesc'] = 'You may add a title for this column.';
-
-
-$string['marketspotsectionfontawesomeicon1'] = 'Section 1 icon';
-$string['marketspotsectionfontawesomeicondesc1'] = 'You may add an icon for section 1';
-$string['marketspotsection1'] = 'Section 1 title';
-$string['marketspotsectiondesc1'] = 'You may add a title for the section';
-$string['marketspotsectionlink1'] = 'Section 1 link';
-$string['marketspotsectionlinkdesc1'] = 'Here, you may insert the link at which the user will be redirected at when they click on the title.';
-$string['marketspotsectiontext1'] = 'Section 1 Description';
-$string['marketspotsectiontextdesc1'] = 'A brief description in the context of the title';
-
-$string['marketspotsectionfontawesomeicon2'] = 'Section 2 icon';
-$string['marketspotsectionfontawesomeicondesc2'] = 'You may add an icon for section 2';
-$string['marketspotsection2'] = 'Section 2 title';
-$string['marketspotsectiondesc2'] = 'You may add a title for the section';
-$string['marketspotsectionlink2'] = 'Section 2 link';
-$string['marketspotsectionlinkdesc2'] = 'Here, you may insert the link at which the user will be redirected at when they click on the title.';
-$string['marketspotsectiontext2'] = 'Section 2 Description';
-$string['marketspotsectiontextdesc2'] = 'A brief description in the context of the title';
-
-$string['marketspotsectionfontawesomeicon3'] = 'Section 3 icon';
-$string['marketspotsectionfontawesomeicondesc3'] = 'You may add an icon for section 3';
-$string['marketspotsection3'] = 'Section 3 title';
-$string['marketspotsectiondesc3'] = 'You may add a title for the section';
-$string['marketspotsectionlink3'] = 'Section 3 link';
-$string['marketspotsectionlinkdesc3'] = 'Here, you may insert the link at which the user will be redirected at when they click on the title.';
-$string['marketspotsectiontext3'] = 'Section 3 Description';
-$string['marketspotsectiontextdesc3'] = 'A brief description in the context of the title';
-
-$string['marketspotsectionfontawesomeicon4'] = 'Section 4 icon';
-$string['marketspotsectionfontawesomeicondesc4'] = 'You may add an icon for section 4';
-$string['marketspotsection4'] = 'Section 4 title';
-$string['marketspotsectiondesc4'] = 'You may add a title for the section.';
-$string['marketspotsectionlink4'] = 'Section 4 link';
-$string['marketspotsectionlinkdesc4'] = 'Here, you may insert the link at which the user will be redirected at when they click on the title.';
-$string['marketspotsectiontext4'] = 'Section 1 Description';
-$string['marketspotsectiontextdesc4'] = 'A brief description in the context of the title';
-
-$string['marketspotsectionbelowlink'] = 'Footer Column 1 link';
-$string['marketspotsectionbelowlinkdesc'] = 'Here, you may insert the link at which the user will be redirected at when they click on the footer column 1 link.';
-$string['marketspotsectionbelowlinkname'] = 'Footer Column 1 link text';
-$string['marketspotsectionbelowlinknamedesc'] = '';
-
-$string['secondmarketspotfontawesomeicon'] = 'Second Market Spot icon';
-$string['secondmarketspotfontawesomeicondesc'] = 'Upload Second icon for Market Spot Heading';
-$string['secondmarketspot'] = 'Footer column 2 ( Middle column)';
-$string['secondmarketspotdesc'] = 'This section relates to the bottom portion (column 2) of your frontpage.';
-
-$string['secondmarketspotsectionfontawesomeicon1'] = 'Section 1 icon';
-$string['secondmarketspotsectionfontawesomeicondesc1'] = 'You may add an icon for section 1.';
-$string['secondmarketspotsection1'] = 'Section 1 title';
-$string['secondmarketspotsectiondesc1'] = 'You may add a title for the section.';
-$string['secondmarketspotsectionlink1'] = 'Section 1 link';
-$string['secondmarketspotsectionlinkdesc1'] = 'Here, you may insert the link at which the user will be redirected at when they click on the title.';
-$string['secondmarketspotsectiontext1'] = 'Section 1 Description';
-$string['secondmarketspotsectiontextdesc1'] = 'A brief description in the context of the title';
-
-$string['secondmarketspotsectionfontawesomeicon2'] = 'Section 2 icon';
-$string['secondmarketspotsectionfontawesomeicondesc2'] = 'You may add an icon for section 2';
-$string['secondmarketspotsection2'] = 'Section 2 title';
-$string['secondmarketspotsectiondesc2'] = 'You may add a title for the section';
-$string['secondmarketspotsectionlink2'] = 'Section 2 link';
-$string['secondmarketspotsectionlinkdesc2'] = 'Here, you may insert the link at which the user will be redirected at when they click on the title.';
-$string['secondmarketspotsectiontext2'] = 'Section 2 Description';
-$string['secondmarketspotsectiontextdesc2'] = 'A brief description in the context of the title';
-
-$string['secondmarketspotsectionfontawesomeicon3'] = 'Section 3 icon';
-$string['secondmarketspotsectionfontawesomeicondesc3'] = 'You may add an icon for section 3';
-$string['secondmarketspotsection3'] = 'Section 3 title';
-$string['secondmarketspotsectiondesc3'] = 'You may add a title for the section';
-$string['secondmarketspotsectionlink3'] = 'Section 3 link';
-$string['secondmarketspotsectionlinkdesc3'] = 'Here, you may insert the link at which the user will be redirected at when they click on the title.';
-$string['secondmarketspotsectiontext3'] = 'Section 3 Description';
-$string['secondmarketspotsectiontextdesc3'] = 'A brief description in the context of the title';
-
-$string['secondmarketspotsectionfontawesomeicon4'] = 'Section 4 icon';
-$string['secondmarketspotsectionfontawesomeicondesc4'] = 'You may add an icon for section 4';
-$string['secondmarketspotsection4'] = 'Section 4 title';
-$string['secondmarketspotsectiondesc4'] = 'You may add a title for the section';
-$string['secondmarketspotsectionlink4'] = 'Section 4 link';
-$string['secondmarketspotsectionlinkdesc4'] = 'Here, you may insert the link at which the user will be redirected at when they click on the title.';
-$string['secondmarketspotsectiontext4'] = 'Section 4 Description';
-$string['secondmarketspotsectiontextdesc4'] = 'A brief description in the context of the title';
-
-$string['secondmarketspotsectionbelowlink'] = 'secondmarketspotsectionbelowlink';
-$string['secondmarketspotsectionbelowlinkdesc'] = 'secondmarketspotsectionbelowlinkdesc';
-$string['secondmarketspotsectionbelowlinkname'] = 'secondmarketspotsectionbelowlinkname';
-$string['secondmarketspotsectionbelowlinknamedesc'] = 'secondmarketspotsectionbelowlinknamedesc';
-
-
-$string['downloadresources'] = 'Download Resources';
-$string['downloadresourcesdesc'] = 'Downloadresources desc';
-
-$string['forumdicussion'] = 'Forum Dicussion';
-$string['forumdicussiondesc'] = 'Forum Dicussion desc';
 
 /**********************************Testimonials*************************************/
 $string['feedback'] = 'Testimonials';
@@ -470,9 +265,6 @@ $string['feedbackslidenamedesc'] = '';
 $string['feedbackslidereview'] = 'Enter user details';
 $string['feedbackslidereviewdesc'] = '';
 
-
-
-
 $string['quicklinksheading'] = 'Enter heading text for Quick Links';
 $string['quicklinksheadingdesc'] = 'You may add heading text for Quick Links section.';
 $string['quicklinksheadingvalue'] = 'Quick links';
@@ -482,7 +274,7 @@ $string['quicklinkscolumnsdesc'] = 'You may add the required number of columns t
 $string['quicklinksrows'] = 'Enter number of rows for column ';
 $string['quicklinksrowsdesc'] = 'You may add the required number of rows that column 1 in Quick Links section will have.';
 
-$string['text'] = 'Enter Text'; 
+$string['text'] = 'Enter Text';
 $string['textdesc'] = 'Enter text for the link where the user will be redirected.';
 $string['link'] = 'Enter Link';
 $string['linkdesc'] = 'Enter the link address where the user will be redirected.';
