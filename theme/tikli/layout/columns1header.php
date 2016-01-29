c91a58537b91df195c7155e8d7bda2dc8a480850
Ryan Wyllie <ryan@moodle.com>
2016-01-29 07:41:35 +0000

    HQ-944 theme_school: Removed analytics

--- theme/tikli/layout/columns1header.php
+++ theme/tikli/layout/columns1header.php
@@ -43,7 +43,6 @@ $colorscheme = get_config('theme_tikli', 'colorscheme');
 </head>
 <body <?php echo $OUTPUT->body_attributes(); ?>>
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
-<?php include $CFG->dirroot . '/theme/tikli/analyticstracking.php'; ?>
 <header role="banner" class="navbar navbar-fixed-top">
   <nav role="navigation" class="navbar-inner">
     <?php if (get_config('theme_tikli', 'logoorsitename') === "logo") { ?>
