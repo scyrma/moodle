ccd22c2cdf9e3424ec31e24a467b1f6ab3be1611
Ryan Wyllie <ryan@moodle.com>
2016-02-01 04:59:19 +0000

    HQ-944 theme_school: Small cleanup of js

--- theme/tikli/layout/login.php
+++ theme/tikli/layout/login.php
@@ -12,12 +12,9 @@ $isregistration = $DB->get_record('config', array('name'=>'registerauth'));
   	<meta name="viewport" content="width=device-width, initial-scale=1">
   	<title><?php echo $OUTPUT->page_title(); ?></title>
   	<link rel="shortcut icon" href="<?php echo $OUTPUT->favicon(); ?>" />
-  	<link href="<?php echo $CFG->wwwroot ?>/theme/tikli/css/styles.css" rel="stylesheet">  
+  	<link href="<?php echo $CFG->wwwroot ?>/theme/tikli/css/styles.css" rel="stylesheet">
   	<script src="<?php echo $CFG->wwwroot ?>/theme/tikli/js/jquery-2.1.4.js"></script>
 	<script src="<?php echo $CFG->wwwroot ?>/theme/tikli/js/bootstrap.min.js"></script>
-	<script src="<?php echo $CFG->wwwroot ?>/theme/tikli/js/jquery.bxslider.min.js"></script>
-	<script src="<?php echo $CFG->wwwroot ?>/theme/tikli/js/jquery.scroll.js"></script>
-	<script src="<?php echo $CFG->wwwroot ?>/theme/tikli/js/engine.js"></script>
 	<script src="<?php echo $CFG->wwwroot ?>/theme/tikli/js/login.js"></script>
 	<?php
       include($CFG->dirroot . '/theme/tikli/settings/colorchange.php');
@@ -32,9 +29,9 @@ $isregistration = $DB->get_record('config', array('name'=>'registerauth'));
 	<div id="page" class="container-fluid login-page">
 		<div id="page-content" class="row-fluid">
 			<section id="region-main" class="container login-regin-main">
-				<?php 
+				<?php
 				echo $OUTPUT->course_content_header();
-				echo $OUTPUT->main_content(); 
+				echo $OUTPUT->main_content();
 				echo $OUTPUT->course_content_footer();
 				?>
 				<?php if($isregistration->value == 'email') { ?><div class="signup-link"><?php echo get_string('firsttime'); ?><a href="<?php echo $CFG->wwwroot; ?>/login/signup.php?"><?php echo ' '.get_string('startsignup'); ?></a></div><?php } ?>
