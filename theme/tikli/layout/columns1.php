29f8a61089d8702a4779df140910ae28b1aecf64
Ryan Wyllie <ryan@moodle.com>
2016-02-01 03:36:04 +0000

    HQ-944 theme_school: consolidate layouts
    Merged a few of the layout files and consolidated some of
    the headers into a single file.

--- theme/tikli/layout/columns1.php
+++ theme/tikli/layout/columns1.php
@@ -1,29 +1,11 @@
 <?php
 // Get the HTML for the settings bits.
 $html = theme_tikli_get_html_for_settings($OUTPUT, $PAGE);
-require('columns1header.php');
+require('header.php');
 if (!isloggedin() && $isregistration->value == 'email') { ?>
     <input type="hidden" name="custommenu" value="nologinselfreg" id="custommenu">
 <?php } ?>
 <div id="page" class="container-fluid">
-
-    <?php if ($CFG->version >= 2015051100) {
-        //echo $OUTPUT->full_header();
-    } else { ?>
-   <!--  <header id="page-header" class="clearfix">
-        <div id="page-navbar" class="clearfix">
-            <div class = "container">
-                <div class="row">
-                    <nav class="breadcrumb-nav"><?php echo $OUTPUT->navbar(); ?></nav>
-                    <div class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></div>
-                </div>
-            </div>
-        </div>
-        <div id="course-header">
-            <?php echo $OUTPUT->course_header(); ?>
-        </div>
-    </header> -->
-    <?php } ?>
 
     <div id="page-content" class="row-fluid">
         <section id="region-main" class="span12">
@@ -35,9 +17,9 @@ if (!isloggedin() && $isregistration->value == 'email') { ?>
         </section>
     </div>
 
-    <?php 
+    <?php
         include('footer.php');
-        echo $OUTPUT->standard_end_of_body_html() 
+        echo $OUTPUT->standard_end_of_body_html()
     ?>
 
 </div>
