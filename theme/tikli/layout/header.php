d5f3681a10862849c8c6bf207a40608c40b31dd1
Ryan Wyllie <ryan@moodle.com>
2016-02-02 06:58:34 +0000

    HQ-944 theme_school: Move logo logic into renderer

--- theme/tikli/layout/header.php
+++ theme/tikli/layout/header.php
@@ -3,16 +3,6 @@
 $html = theme_tikli_get_html_for_settings($OUTPUT, $PAGE);
 global $USER, $PAGE, $CFG, $DB;
 
-$logo = $PAGE->theme->setting_file_url('logo', 'logo');
-if(empty($logo)) {
-    $logo = $CFG->wwwroot.'/theme/tikli/pix/logo-2.png';
-}
-
-$iconlogo = $PAGE->theme->setting_file_url('icon', 'icon');
-if(empty($iconlogo)) {
-    $iconlogo = $CFG->wwwroot.'/theme/tikli/pix/icon-logo.png';
-}
-
 echo $OUTPUT->doctype();
 
 $isregistration = $DB->get_record('config', array('name'=>'registerauth'));
@@ -30,25 +20,7 @@ $isregistration = $DB->get_record('config', array('name'=>'registerauth'));
     <?php echo $OUTPUT->standard_top_of_body_html() ?>
     <header role="banner" class="navbar navbar-fixed-top">
         <nav role="navigation" class="navbar-inner">
-            <?php if (get_config('theme_tikli', 'logoorsitename') === "logo") { ?>
-                <div class="logo-wr">
-                    <a class="logo-img" href="<?php echo $CFG->wwwroot; ?>">
-                        <img alt="logo" src="<?php echo $logo;?>" />
-                    </a>
-                </div>
-            <?php } else if (get_config('theme_tikli', 'logoorsitename') === "sitename") { ?>
-                <div class="logo-wr">
-                    <a class="logo-img" href="<?php echo $CFG->wwwroot; ?>">
-                        <h1><?php echo $SITE->fullname; ?></h1>
-                    </a>
-                </div>
-            <?php } else if (get_config('theme_tikli', 'logoorsitename') === "iconsitename") { ?>
-                <div class="logo-wr">
-                    <a class="logo-img" href="<?php echo $CFG->wwwroot; ?>">
-                        <h1><span class="logoicon"><img alt="logo" src="<?php echo $iconlogo;?>" /></span><span><?php echo $SITE->fullname; ?></span></h1>
-                    </a>
-                </div>
-            <?php } ?>
+            <?php echo $OUTPUT->logo(); ?>
 
             <button class="side-pre-menu menu-toggle">
                 <span>toggle menu</span>
