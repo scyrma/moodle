1ea7d303e534a3759931e6b8b8f45eee48ba878a
Ryan Wyllie <ryan@moodle.com>
2016-02-04 03:12:49 +0000

    HQ-944 theme_school: Moved frontpage slider into renderer

--- theme/tikli/layout/frontpage.php
+++ theme/tikli/layout/frontpage.php
@@ -3,35 +3,8 @@
 $html = theme_tikli_get_html_for_settings($OUTPUT, $PAGE);
 GLOBAL $DB, $CFG, $OUTPUT, $USER;
 
-$addtext = get_config('theme_tikli', 'addtext');
-$slideinterval = get_config('theme_tikli', 'slideinterval');
-$slideautoplay = get_config('theme_tikli', 'sliderautoplay');
-
-if(get_config('theme_tikli', 'videotype') === "0") {
-	$iframevideo = get_config('theme_tikli', 'video');
-} else {
-	$uploadedvideo = $PAGE->theme->setting_file_url('uploadvideo', 'uploadvideo');
-}
 $isregistration = $DB->get_record('config', array('name'=>'registerauth'));
 $url = "'".$CFG->wwwroot."/theme/tikli/js/frontpageslider/jquery.cslider.js"."'";
-
-$hasfeedbackheading = get_config('theme_tikli', 'feedbackheading');
-$hasfeedbacksubheading = get_config('theme_tikli', 'feedbacksubheading');
-$hasfeedbackiframe = get_config('theme_tikli', 'feedbackiframe');
-$hasfeedbackbrieftext = get_config('theme_tikli', 'feedbackbrieftext');
-
-for($feedbackslides = 1; $feedbackslides <= get_config('theme_tikli', 'feedbackslidecount'); $feedbackslides = $feedbackslides + 1) {
-	for($feedinner = 1; $feedinner <= 4; $feedinner = $feedinner + 1) {
-		$hasimg = get_config('theme_tikli', 'feedbackslideimage_'.$feedinner.'_'.$feedbackslides);
-		if (!empty($hasimg)) {
-			$hasfeedbacks[$feedbackslides]["feedbackslideimage_".$feedinner] = $PAGE->theme->setting_file_url('feedbackslideimage_'.$feedinner.'_'.$feedbackslides, 'feedbackslideimage_'.$feedinner.'_'.$feedbackslides);
-		} else {
-			$hasfeedbacks[$feedbackslides]["feedbackslideimage_".$feedinner] = $CFG->wwwroot."/theme/tikli/css/img/userimage.png";
-		}
-		$hasfeedbacks[$feedbackslides]["feedbackslidename_".$feedinner] = get_config('theme_tikli', 'feedbackslidename_'.$feedinner.'_'.$feedbackslides);
-		$hasfeedbacks[$feedbackslides]["feedbackslidereview_".$feedinner] = get_config('theme_tikli', 'feedbackslidereview_'.$feedinner.'_'.$feedbackslides);
-	}
-}
 
 echo $OUTPUT->doctype();?>
 <html <?php echo $OUTPUT->htmlattributes(); ?>>
@@ -63,92 +36,14 @@ echo $OUTPUT->doctype();?>
         			<i class="fa fa-arrow-circle-down"></i>
       			</a>
       			<div class="nav-collapse collapse">
-      				<?php echo $OUTPUT->custom_menu();?>
+      				<?php echo $OUTPUT->custom_menu(); ?>
       			</div>
       		<?php } ?>
       		</div>
 
 			</div>
-			<?php if(get_config('theme_tikli', 'frontpageimagecontent') == 0) { ?><div class="welcome-block"><?php } else if (get_config('theme_tikli', 'frontpageimagecontent') == 1) { ?><div class="welcome-block-slider"><?php } ?>
 
-						<?php if(get_config('theme_tikli', 'frontpageimagecontent') == 0) { // if static content ?>
-							<?php if($addtext || $iframevideo || $uploadedvideo) { ?>
-								<?php if(get_config('theme_tikli', 'frontpagevideoalignment') == 1) { //video right?>
-                                <div class="container">
-									<div class="row">
-									<div class="span6 static-content-align">
-										<?php if(!empty($addtext)) { echo $addtext; }?>
-									</div>
-									<?php if(!empty($iframevideo)) { ?>
-										<div class="span6 static-content-align">
-											<?php if(!empty($iframevideo)) { echo $iframevideo; }?>
-										</div>
-									<?php } ?>
-									<?php if(!empty($uploadedvideo)) { ?>
-										<div class="span6 static-content-align">
-											<video width="560" height="315" controls>
-												<?php if(!empty($uploadedvideo)) { ?><source src="<?php echo $uploadedvideo;?>" type="video/mp4"> <?php } ?>
-											</video>
-										</div>
-									<?php } ?>
-									<?php } else if (get_config('theme_tikli', 'frontpagevideoalignment') == 0) { ?>
-									<?php if(!empty($iframevideo)) { ?>
-									<div class="container">
-									<div class="row">
-										<div class="span6 static-content-align">
-											<?php if(!empty($iframevideo)) { echo $iframevideo; }?>
-										</div>
-									<?php } ?>
-									<?php if(!empty($uploadedvideo)) { ?>
-										<div class="span6 static-content-align">
-											<video width="560" height="315" controls>
-												<?php if(!empty($uploadedvideo)) { ?><source src="<?php echo $uploadedvideo;?>" type="video/mp4"> <?php } ?>
-											</video>
-										</div>
-									<?php } ?>
-									<div class="span6 static-content-align">
-										<?php if(!empty($addtext)) { echo $addtext; }?>
-									</div>
-									<?php } ?>
-							<?php } ?>
-                            </div>
-						</div><!-- END of .container -->
-						<?php } else if (get_config('theme_tikli', 'frontpageimagecontent') == 1) { // if slider
-							$numberofsliders = get_config('theme_tikli', 'slidercount');?>
-							<!--<div class="span12">-->
-								<div id="da-slider" class="da-slider">
-									<?php if(!empty($numberofsliders)) { ?>
-									<?php for($slidecount = 1; $slidecount <= $numberofsliders; $slidecount++) {
-										$sliderimageurl = $PAGE->theme->setting_file_url('slideimage'.$slidecount, 'slideimage'.$slidecount);
-										$sliderimagetext = get_config('theme_tikli', 'slidertext'.$slidecount);
-										$sliderimagelink = get_config('theme_tikli', 'sliderurl'.$slidecount);
-										$sliderbuttontext = get_config('theme_tikli', 'sliderbuttontext'.$slidecount);
-										if(!empty($sliderimagetext) || !empty($sliderimagelink) || !empty($sliderimageurl)) {
-									?>
-									<div class="da-slide" data-interval="<?php if(!empty($slideinterval)) { echo $slideinterval; } ?>" data-autoplay = "<?php if(!empty($slideautoplay)) { echo $slideautoplay; } ?>">
-										<?php if(!empty($sliderimagetext)) {
-											echo $sliderimagetext;
-										} ?>
-										<?php if(!empty($sliderimagelink)) { ?>
-											<a href=<?php echo $sliderimagelink;?> class="da-link"><?php if($sliderbuttontext) { echo $sliderbuttontext; } ?></a>
-										<?php } ?>
-										<?php if(!empty($sliderimageurl)) { ?>
-											<div class="da-img"><img src="<?php echo $sliderimageurl; ?>" alt="image01" /></div>
-										<?php } else { ?>
-											<div class="da-img"><img src="<?php echo $CFG->wwwroot.'/theme/tikli/css/images/slider-img.png';?>" alt="image01" /></div>
-										<?php } ?>
-									</div>
-                                    <nav class="da-arrows">
-                                        <span class="da-arrows-prev"></span>
-                                        <span class="da-arrows-next"></span>
-                                    </nav>
-									<?php }
-									}
-									} ?>
-								</div>
-							<!--</div>-->
-						<?php } ?>
-			</div><!-- END of .welcome-block -->
+            <?php echo $OUTPUT->frontpage_header_content(); ?>
 		</header><!-- END of header -->
 
 		<div class="content">
