33edf6d8942958459f48a751bdb5b827de3fc8b2
Ryan Wyllie <ryan@moodle.com>
2016-02-04 05:58:45 +0000

    HQ-944 theme_school: Moved coursecategories into renderer

--- theme/tikli/classes/core_renderer.php
+++ theme/tikli/classes/core_renderer.php
@@ -17,6 +17,71 @@ class theme_tikli_core_renderer extends theme_bootstrapbase_core_renderer {
         'bootstrap.min.js'
     );
 
+    private function get_course_details() {
+        global $DB, $CFG;
+
+        $courses = $DB->get_records_sql('SELECT c.* FROM {course} c where id != ? and visible = ?',array(1, 1));
+
+        if (empty($courses)) {
+            return array();
+        }
+
+        $coursedetailsarray = array();
+        foreach ($courses as $key => $coursevalue) {
+            $url = new \moodle_url('/course/view.php', array('id' => $coursevalue->id));
+            $enrolledusersurl = new \moodle_url('/enrol/users.php', array('id' => $coursevalue->id));
+
+            $courseinfo = array(
+                'url' => $url->out(),
+                'enrolledusersurl' => $enrolledusersurl->out(),
+                'name' => $coursevalue->fullname,
+                'summary' => $coursevalue->summary,
+                'teachers' => array(),
+            );
+
+            $courseteacher = $DB->get_records_sql('SELECT u.*
+            FROM {course} c
+            JOIN {context} ct ON c.id = ct.instanceid
+            JOIN {role_assignments} ra ON ra.contextid = ct.id
+            JOIN {user} u ON u.id = ra.userid
+            JOIN {role} r ON r.id = ra.roleid Where c.id = ? and r.shortname = ?', array($coursevalue->id, 'editingteacher'));
+            if(!empty($courseteacher)) {
+                foreach ($courseteacher as $keycourseteacher => $courseteachervalue) {
+                    $profileurl = new \moodle_url('/user/profile.php', array('id' => $courseteachervalue->id));
+
+                    $courseinfo['teachers'][] = array(
+                        'name' => $courseteachervalue->firstname." ".$courseteachervalue->lastname,
+                        'profileurl' => $profileurl->out()
+                    );
+                }
+            } else {
+                $courseinfo['teachers'][] = array(
+                    // TODO: Lang strings.
+                    'name' => 'Not assigned',
+                    'profileurl' => 'javascript:void(0);'
+                );
+            }
+
+            $courseimage = '';
+            $coursecontext = context_course::instance($coursevalue->id);
+            $isfile = $DB->get_records_sql("Select * from {files} where contextid = ? and filename != ?", array($coursecontext->id, "."));
+            if ($isfile) {
+                foreach ($isfile as $key1 => $isfilevalue) {
+                    $courseimage =  $CFG->wwwroot . "/pluginfile.php/" . $isfilevalue->contextid ."/". $isfilevalue->component . "/" . $isfilevalue->filearea . "/" . $isfilevalue->filename;
+                }
+            }
+            if (empty($courseimage)) {
+                $courseimage = $CFG->wwwroot."/theme/tikli/data/nopic.jpg";
+            }
+
+            $courseinfo['imageurl'] = $courseimage;
+
+            $coursedetailsarray[] = $courseinfo;
+        }
+
+        return $coursedetailsarray;
+    }
+
     public function full_header() {
         $html = html_writer::start_tag('header', array('id' => 'page-header', 'class' => 'clearfix'));
         //$html .= $this->context_header();
@@ -306,66 +371,15 @@ class theme_tikli_core_renderer extends theme_bootstrapbase_core_renderer {
     }
 
     public function frontpage_courses() {
-        global $DB, $CFG;
+        $coursedetails = $this->get_course_details();
 
-        $colorscheme = get_config('theme_tikli', 'colorscheme');
-        $courses = $DB->get_records_sql('SELECT c.* FROM {course} c where id != ? and visible = ?',array(1, 1));
-
-        if (empty($courses)) {
+        if (empty($coursedetails)) {
             return "";
         }
 
-        $coursedetailsarray = array();
-        foreach ($courses as $key => $coursevalue) {
-            $url = new \moodle_url('/course/view.php', array('id' => $coursevalue->id));
-
-            $courseinfo = array(
-                'url' => $url->out(),
-                'name' => $coursevalue->fullname,
-                'teachers' => array(),
-            );
-
-            $courseteacher = $DB->get_records_sql('SELECT u.*
-            FROM {course} c
-            JOIN {context} ct ON c.id = ct.instanceid
-            JOIN {role_assignments} ra ON ra.contextid = ct.id
-            JOIN {user} u ON u.id = ra.userid
-            JOIN {role} r ON r.id = ra.roleid Where c.id = ? and r.shortname = ?', array($coursevalue->id, 'editingteacher'));
-            if(!empty($courseteacher)) {
-                foreach ($courseteacher as $keycourseteacher => $courseteachervalue) {
-                    $profileurl = new \moodle_url('/user/profile.php', array('id' => $courseteachervalue->id));
-
-                    $courseinfo['teachers'][] = array(
-                        'name' => $courseteachervalue->firstname." ".$courseteachervalue->lastname,
-                        'profileurl' => $profileurl->out()
-                    );
-                }
-            } else {
-                $courseinfo['teachers'][] = array(
-                    // TODO: Lang strings.
-                    'name' => 'Not assigned',
-                    'profileurl' => 'javascript:void(0);'
-                );
-            }
-
-            $courseimage = '';
-            $coursecontext = context_course::instance($coursevalue->id);
-            $isfile = $DB->get_records_sql("Select * from {files} where contextid = ? and filename != ?", array($coursecontext->id, "."));
-            if($isfile) {
-                foreach ($isfile as $key1 => $isfilevalue) {
-                    $courseimage =  $CFG->wwwroot . "/pluginfile.php/" . $isfilevalue->contextid ."/". $isfilevalue->component . "/" . $isfilevalue->filearea . "/" . $isfilevalue->filename;
-                }
-            }
-            if(empty($courseimage)) {
-                $courseimage = $CFG->wwwroot."/theme/tikli/data/nopic.jpg";
-            }
-
-            $courseinfo['imageurl'] = $courseimage;
-
-            $coursedetailsarray[] = $courseinfo;
-        }
-
+        $colorscheme = get_config('theme_tikli', 'colorscheme');
         $categories = array();
+        $categoriesurl = new \moodle_url('/course/index.php');
 
         for($quicklinkcols = 1; $quicklinkcols <= get_config('theme_tikli', 'quicklinkscolumns'); $quicklinkcols = $quicklinkcols + 1) {
             for($quicklinkrows = 1; $quicklinkrows <= get_config('theme_tikli', 'quicklinksrows'.$quicklinkcols); $quicklinkrows = $quicklinkrows + 1) {
@@ -378,10 +392,8 @@ class theme_tikli_core_renderer extends theme_bootstrapbase_core_renderer {
             }
         }
 
-        $categoriesurl = new \moodle_url('/course/index.php');
-
         $context = array(
-            'courses' => $coursedetailsarray,
+            'courses' => $coursedetails,
             'categories' => $categories,
             'categoriesurl' => $categoriesurl->out(),
             'heading' => get_config('theme_tikli', 'coursesectionheading'),
@@ -505,80 +517,33 @@ class theme_tikli_core_renderer extends theme_bootstrapbase_core_renderer {
                 return "";
         }
     }
-}
 
-/**
- * overridding core_course_renderer class for adding alt tag.
- * @copyright 2010 Sam Hemelryk
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-class theme_tikli_core_course_renderer extends core_course_renderer {
-    /**
-     * Returns HTML to display course content (summary, course contacts and optionally category name)
-     *
-     * This method is called from coursecat_coursebox() and may be re-used in AJAX
-     *
-     * @param coursecat_helper $chelper various display options
-     * @param stdClass|course_in_list $course
-     * @return string
-     */
-    protected function coursecat_coursebox_content(coursecat_helper $chelper, $course) {
-        global $CFG;
-        if ($chelper->get_show_courses() < self::COURSECAT_SHOW_COURSES_EXPANDED) {
-            return '';
+    public function coursecategory_courses() {
+        $coursedetails = $this->get_course_details();
+
+        if (empty($coursedetails)) {
+            return "";
         }
-        if ($course instanceof stdClass) {
-            require_once($CFG->libdir. '/coursecatlib.php');
-            $course = new course_in_list($course);
-        }
-        $content = '';
-        if ($course->has_summary()) {
-            $content .= html_writer::start_tag('div', array('class' => 'summary'));
-            $content .= $chelper->get_course_formatted_summary($course,
-                    array('overflowdiv' => true, 'noclean' => true, 'para' => false));
-            $content .= html_writer::end_tag('div');
-        }
-        $contentimages = $contentfiles = '';
-        foreach ($course->get_course_overviewfiles() as $file) {
-            $isimage = $file->is_valid_image();
-            $url = file_encode_url("$CFG->wwwroot/pluginfile.php",
-                    '/'. $file->get_contextid(). '/'. $file->get_component(). '/'.
-                    $file->get_filearea(). $file->get_filepath(). $file->get_filename(), !$isimage);
-            if ($isimage) {
-                $contentimages .= html_writer::tag('div',
-                        html_writer::empty_tag('img', array('src' => $url, 'alt' => 'courseimage')),
-                        array('class' => 'courseimage'));
-            } else {
-                $image = $this->output->pix_icon(file_file_icon($file, 24), $file->get_filename(), 'moodle');
-                $filename = html_writer::tag('span', $image, array('class' => 'fp-icon')).
-                        html_writer::tag('span', $file->get_filename(), array('class' => 'fp-filename'));
-                $contentfiles .= html_writer::tag('span',
-                        html_writer::link($url, $filename),
-                        array('class' => 'coursefile fp-filename-icon'));
-            }
-        }
-        $content .= $contentimages. $contentfiles;
-        if ($course->has_course_contacts()) {
-            $content .= html_writer::start_tag('ul', array('class' => 'teachers'));
-            foreach ($course->get_course_contacts() as $userid => $coursecontact) {
-                $name = $coursecontact['rolename'].': '.
-                        html_writer::link(new moodle_url('/user/view.php',
-                                array('id' => $userid, 'course' => SITEID)),
-                            $coursecontact['username']);
-                $content .= html_writer::tag('li', $name);
-            }
-            $content .= html_writer::end_tag('ul');
-        }
-        if ($chelper->get_show_courses() == self::COURSECAT_SHOW_COURSES_EXPANDED_WITH_CAT) {
-            require_once($CFG->libdir. '/coursecatlib.php');
-            if ($cat = coursecat::get($course->category, IGNORE_MISSING)) {
-                $content .= html_writer::start_tag('div', array('class' => 'coursecat'));
-                $content .= get_string('category').': '.
-                        html_writer::link(new moodle_url('/course/index.php', array('categoryid' => $cat->id)),
-                                $cat->get_formatted_name(), array('class' => $cat->visible ? '' : 'dimmed'));
-                $content .= html_writer::end_tag('div');
-            }
-        }
-        return $content;
+
+        $colorscheme = get_config('theme_tikli', 'colorscheme');
+        $coursesearchurl = new \moodle_url('/course/search.php');
+        $addcourseurl = new \moodle_url('/course/edit.php', array('category' => 1, 'returnto' => 'category'));
+        $coursecontext = context_course::instance(1);
+
+        $context = array(
+            'courses' => $coursedetails,
+            'imageurls' => array(
+                'plus' => $this->get_theme_source_css(sprintf("/img/%s/i-plus.png", $colorscheme)),
+                'courselink' => $this->get_theme_source_css(sprintf("/img/%s/i-c-1.png", $colorscheme)),
+                'enrolleduserslink' => $this->get_theme_source_css(sprintf("/img/%s/i-c-2.png", $colorscheme)),
+            ),
+            'urls' => array(
+                'coursesearch' => $coursesearchurl->out(),
+                'addcourse' => $addcourseurl->out(),
+            ),
+            'showaddcourse' => has_capability('moodle/course:create', $coursecontext),
+        );
+
+        return $this->render_from_template('theme_tikli/coursecategory_courses', $context);
     }
 }
