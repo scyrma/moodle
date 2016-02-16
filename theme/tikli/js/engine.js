8429809af9ad444a548547f67a5aaf2702bb95fb
Ryan Wyllie <ryan@moodle.com>
2016-02-16 02:57:20 +0000

    HQ-944 theme_school: Made filter subcategory button on course category

--- theme/tikli/js/engine.js
+++ theme/tikli/js/engine.js
@@ -67,14 +67,10 @@ $(".btn-navbar").on("click",function() {
 	$('.usermenu-show').removeClass('usermenu-show');
 });
 
-$("#show-subcategories").click(function() {
-    $('#all-category-picker-container').addClass('hidden');
-    $('#subcategory-picker-container').removeClass('hidden');
-});
-
-$("#show-all-categories").click(function() {
-    $('#subcategory-picker-container').addClass('hidden');
-    $('#all-category-picker-container').removeClass('hidden');
+$('#category-picker-filter').click(function() {
+    $('#all-category-picker-container').toggleClass('hidden');
+    $('#subcategory-picker-container').toggleClass('hidden');
+    $(this).toggleClass('active');
 });
 
 $( window ).load(function() {
