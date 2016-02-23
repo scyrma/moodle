d4618b9cd4363a0b4c2c3da36e7bb1f0ce4f919c
Ryan Wyllie <ryan@moodle.com>
2016-02-23 04:41:05 +0000

    HQ-944 theme_school: Made footer into template

--- theme/school/layout/coursecategory.php
+++ theme/school/layout/coursecategory.php
@@ -42,8 +42,8 @@ if ($CFG->forcelogin) {
     </div>
 
     <?php
-        include('footer.php');
-        echo $OUTPUT->standard_end_of_body_html()
+        echo $OUTPUT->theme_footer();
+        echo $OUTPUT->standard_end_of_body_html();
     ?>
 </div>
 </body>
