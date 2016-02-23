a423acb6f946a986fd788f6fab350b4c446d3c5a
Ryan Wyllie <ryan@moodle.com>
2016-02-23 04:41:05 +0000

    HQ-944 theme_school: Made footer into template

--- theme/school/layout/columns1.php
+++ theme/school/layout/columns1.php
@@ -1,6 +1,7 @@
 <?php
 // Get the HTML for the settings bits.
 $html = theme_school_get_html_for_settings($OUTPUT, $PAGE);
+
 require('header.php');
 if (!isloggedin() && $isregistration->value == 'email') { ?>
     <input type="hidden" name="custommenu" value="nologinselfreg" id="custommenu">
@@ -18,8 +19,8 @@ if (!isloggedin() && $isregistration->value == 'email') { ?>
     </div>
 
     <?php
-        include('footer.php');
-        echo $OUTPUT->standard_end_of_body_html()
+        echo $OUTPUT->theme_footer();
+        echo $OUTPUT->standard_end_of_body_html();
     ?>
 
 </div>
