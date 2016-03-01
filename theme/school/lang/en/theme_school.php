6de29702f11ec976ec4fd407ff840c76416e33ae
Ryan Wyllie <ryan@moodle.com>
2016-03-01 07:56:26 +0000

    HQ-944 theme_school: Updated link for cloud portal

--- theme/school/lang/en/theme_school.php
+++ theme/school/lang/en/theme_school.php
@@ -20,6 +20,7 @@ $string['region-side-post'] = 'Right';
 $string['region-side-pre'] = 'Left';
 $string['slidernext'] = 'Next slide';
 $string['sliderprev'] = 'Previous slide';
+$string['cloudportallink'] = 'Moodle cloud portal link';
 
 /*logo*/
 $string['logo'] = 'Logo';
