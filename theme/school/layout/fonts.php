2c0a7bc321d5f276145c182a02cfd47a9ea08707
Ryan Wyllie <ryan@moodle.com>
2016-03-02 05:11:35 +0000

    HQ-944 theme_school: Added license comment to files

--- theme/school/layout/fonts.php
+++
@@ -1,4 +0,0 @@
-<?php
-if ($fontselect === '2') { ?>
-    <link href='//fonts.googleapis.com/css?family=<?php echo $headingfont.'|'.$bodyfont.$fontcharacterset;?>' rel='stylesheet' type='text/css'>
-<?php } ?>
