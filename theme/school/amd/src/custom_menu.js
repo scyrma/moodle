c7d239152b625b0d80d982ccb93d7384c29059d5
Ryan Wyllie <ryan@moodle.com>
2016-04-05 03:43:41 +0000

    HQ-944 theme_school: Custom menu javascript and styling

--- theme/school/amd/src/custom_menu.js
+++ theme/school/amd/src/custom_menu.js
@@ -1,34 +1,57 @@
 ﻿define(['jquery'], function($) {
 
     var customMenu = $('#custom-menu');
+    var breakWidth = 0;
 
-    var toggleButtonMode = function(breakWidth) {
+    var calculateBreakWidth = function() {
+        if (breakWidth) {
+            return breakWidth;
+        }
+
+        if (customMenu.length) {
+            var windowWidth = $(window).width();
+            var siblings = customMenu.siblings();
+            var menuWidth = customMenu.outerWidth();
+            var siblingWidth = 0;
+            siblings.each(function(index, sibling) {
+                sibling = $(sibling);
+                if (sibling.is(':visible')) {
+                    siblingWidth += sibling.outerWidth();
+                }
+            });
+            var navElementsWidth = menuWidth + siblingWidth;
+
+            var diffWidth = windowWidth - navElementsWidth;
+            if (diffWidth < 0) {
+                // The navbar elements are taking up more than the width of the screen,
+                // which means they are in responsive mode, so we don't need to do anything.
+                breakWidth = windowWidth - diffWidth;
+            } else {
+                // The +10 gives us a buffer;
+                breakWidth = windowWidth - diffWidth + 10;
+            }
+            return breakWidth;
+        } else {
+            return 0;
+        }
+    };
+
+    var toggleButtonMode = function() {
         var currentWindowWidth = $(window).width();
         // This is the cutoff for hiding the custom menu anyway.
         if (currentWindowWidth < 980) {
             return;
         }
 
-        if (currentWindowWidth <= breakWidth) {
-            customMenu.addClass('button-mode');
-        } else {
-            customMenu.removeClass('button-mode');
+        var currentBreakWidth = calculateBreakWidth();
+        if (currentBreakWidth) {
+            if (currentWindowWidth <= breakWidth) {
+                customMenu.addClass('button-mode');
+            } else {
+                customMenu.removeClass('button-mode');
+            }
         }
-    }
-
-    var breakWidth = $(window).width();
-    if (customMenu.length) {
-        var windowWidth = $(window).width();
-        var navElementsWidth = customMenu.outerWidth() + customMenu.siblings().outerWidth();
-
-        var diffWidth = windowWidth - navElementsWidth;
-        if (diffWidth < 0) {
-            diffWidth = 0;
-        }
-
-        // The +10 gives us a buffer;
-        breakWidth = breakWidth - diffWidth + 10;
-    }
+    };
 
     return {
         init: function() {
@@ -45,9 +68,9 @@
                     }
                 });
 
-                toggleButtonMode(breakWidth);
+                toggleButtonMode();
                 $(window).resize(function() {
-                    toggleButtonMode(breakWidth);
+                    toggleButtonMode();
                 });
             }
         }
