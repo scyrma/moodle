ddcf2dca445fff28888e9a53bc61f4ae3e2ea2f2
Mathieu Petit-Clair <mathieu@petitclair.ca>
2016-11-02 13:15:03 +0800

    CLOUD: use parameter to allow cache generation.

--- cache/locallib.php
+++ cache/locallib.php
@@ -67,12 +67,14 @@ class cache_config_writer extends cache_config {
      * infinite loop situations caused by the cache throwing exceptions during its initialisation.
      */
     protected function config_save() {
+        global $CFG;
         // START MOODLECLOUD HACK.
         // Return early without ever changing the configuration. This is always managed by centrally and shared amongst
         // all sites.
-        return;
+        if (empty($CFG->mc_force_cache_generation)) {
+            return;
+        }
         // END MOODLECLOUD HACK.
-        global $CFG;
         static $confighash = '';
         $cachefile = static::get_config_file_path();
         $directory = dirname($cachefile);
