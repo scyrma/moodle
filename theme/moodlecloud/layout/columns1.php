e76066b541aaf930001ac87872900eed7dd7eb6f
Craig R Morton <craig@moodle.com>
2016-11-16 11:28:31 +0800

    MC-237: 3.2 review - School theme messaging changes
    MC-240: fix incompatibility if required
    
    Updating the Gruntfile to watch for SCSS file changes and any LESS changes in the /theme school dir
    
    Layout work for the messaging icons on all themes
    
    Hiding the default 'pix' icons on school sites and using Font Awesome instead
    
    Doing thr reverse for non-school sites

--- theme/moodlecloud/layout/columns1.php
+++ theme/moodlecloud/layout/columns1.php
@@ -51,7 +51,10 @@ echo $OUTPUT->doctype() ?>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </a>
-            <?php echo $OUTPUT->user_menu(); ?>
+            <?php
+            echo $OUTPUT->user_menu();
+            echo $OUTPUT->navbar_plugin_output();
+            ?>
             <div class="nav-collapse collapse">
                 <?php echo $OUTPUT->custom_menu(); ?>
                 <ul class="nav pull-right">
