bfa7a7edadc7fe34401bb0f200afae6489e76bcc
Jordan Tomkinson <jordan@moodle.com>
2016-11-16 11:03:36 +0800

    conflict resolution when merging with FEATURE-moodlecloud, part 1

--- local/logging/db/tasks.php
+++ local/logging/db/tasks.php
@@ -26,13 +26,4 @@
 defined('MOODLE_INTERNAL') || die();
 
 $tasks = array(
-    array(
-        'classname' => 'local_logging\task\report_task',
-        'blocking' => 0,
-        'minute' => '*',
-        'hour' => '*',
-        'day' => '*',
-        'month' => '*',
-        'dayofweek' => '*'
-    )
 );
