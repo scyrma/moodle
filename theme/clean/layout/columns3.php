9b06f971383cc30ab25c53fc386bb0bcf31d1a77
Craig R Morton <craig@moodle.com>
2016-11-16 10:45:15 +0800

    MC-214: Add the google analytics tracker on the Moodle sites
    Adding the GATC to Boost, Clean and missing sections of the MoodleCloud theme

--- theme/clean/layout/columns3.php
+++ theme/clean/layout/columns3.php
@@ -97,6 +97,7 @@ echo $OUTPUT->doctype() ?>
         echo $OUTPUT->login_info();
         echo $OUTPUT->home_link();
         echo $OUTPUT->standard_footer_html();
+        echo theme_clean_get_gatc();
         ?>
     </footer>
 
