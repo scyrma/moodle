337256cdade53c33c8a81dc22f58b41513d8af27
Craig R Morton <craig@moodle.com>
2016-12-08 14:35:51 +0800

    MC-342: Ads to display on Boost theme
    Removing the reference to a theme_moodlecloud lang string in theme_boost lib.php

--- theme/boost/lib.php
+++ theme/boost/lib.php
@@ -169,7 +169,7 @@ function theme_boost_get_ad($context) {
     $PAGE->requires->strings_for_js(array(
         'adunblock_title',
         'adunblock_message',
-    ), 'theme_moodlecloud');
+    ), 'theme_boost');
 
     $adconfig = array(
         'id'            => 'moodlecloud_ad',
@@ -189,7 +189,6 @@ function theme_boost_get_ad($context) {
         }
     } else {
         // User is not an administrator.
-
         if (defined('MOODLECLOUD_FEATURE_STUDENTADS_DISABLED') && MOODLECLOUD_FEATURE_STUDENTADS_DISABLED) {
             // Student ads are disabled.
             return '';
