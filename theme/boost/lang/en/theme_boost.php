f7992d28ddd68f68581439b842859e9a1f006066
Craig R Morton <craig@moodle.com>
2017-01-06 13:48:07 +0800

    MC-361: Changing the background image on Boost theme needs to be easy for the user.
    theme_boost: Adding a file picker to the Boost theme settings page for the background image

--- theme/boost/lang/en/theme_boost.php
+++ theme/boost/lang/en/theme_boost.php
@@ -32,6 +32,8 @@ $string['adunblock_message'] = '
     <p>Thanks, MoodleCloud team.</p>';
 $string['adunblock_title'] = 'Ad blocker detected :(';
 $string['advancedsettings'] = 'Advanced settings';
+$string['backgroundimage'] = 'Background image';
+$string['backgroundimage_desc'] = 'The image to display as a background of the site. The background image you upload here will override the background image in your theme preset files.';
 $string['brandcolor'] = 'Brand colour';
 $string['brandcolor_desc'] = 'The accent colour.';
 $string['choosereadme'] = 'Boost is a modern highly-customisable theme. This theme is intended to be used directly, or as a parent theme when creating new themes utilising Bootstrap 4.';
