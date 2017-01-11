bb717c75064b92d51c85878705c8a9bf7fe7e7d3
Mathieu Petit-Clair <mathieu@petitclair.ca>
2017-01-11 14:54:42 +0800

    local_filestorage: upgrade to 2.8.31 of AWS SDK.

--- local/filestorage/vendor/composer/autoload_real.php
+++ local/filestorage/vendor/composer/autoload_real.php
@@ -23,28 +23,30 @@ class ComposerAutoloaderInit57e01d047f8ba46959e0062d35ec21db
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit57e01d047f8ba46959e0062d35ec21db', 'loadClassLoader'));
 
-        $map = require __DIR__ . '/autoload_namespaces.php';
-        foreach ($map as $namespace => $path) {
-            $loader->set($namespace, $path);
-        }
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        if ($useStaticLoader) {
+            require_once __DIR__ . '/autoload_static.php';
 
-        $map = require __DIR__ . '/autoload_psr4.php';
-        foreach ($map as $namespace => $path) {
-            $loader->setPsr4($namespace, $path);
-        }
+            call_user_func(\Composer\Autoload\ComposerStaticInit57e01d047f8ba46959e0062d35ec21db::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
 
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
         }
 
         $loader->register(true);
 
         return $loader;
     }
-}
-
-function composerRequire57e01d047f8ba46959e0062d35ec21db($file)
-{
-    require $file;
 }
