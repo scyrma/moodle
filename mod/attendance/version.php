9197a50919b0b15553483caf6d0536c479ba84e7
Mathieu Petit-Clair <mathieu@petitclair.ca>
2017-02-17 15:05:29 +0800

    mod_attendance: commit 9ef35c4ea4

--- mod/attendance/version.php
+++ mod/attendance/version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017020203;
-$plugin->requires = 2017011900;
-$plugin->release = '3.3.1';
+$plugin->version  = 2016121301;
+$plugin->requires = 2016111800;
+$plugin->release = '3.2.3';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
