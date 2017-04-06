2d1c62a9f59e849c6ecf291531654341a1ff5e18
Mathieu Petit-Clair <mathieu@petitclair.ca>
2017-04-06 14:29:18 +0800

    s3: update AWS SDK to v3

--- local/filestorage/vendor/composer/autoload_real.php
+++ local/filestorage/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit57e01d047f8ba46959e0062d35ec21db
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit57e01d047f8ba46959e0062d35ec21db', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit57e01d047f8ba46959e0062d35ec21db
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit57e01d047f8ba46959e0062d35ec21db::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire57e01d047f8ba46959e0062d35ec21db($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire57e01d047f8ba46959e0062d35ec21db($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
