56f4bc7eeaee52f421369746249d3c1b90471f31
Craig R Morton <crmpicco@aol.co.uk>
2017-04-24 17:19:39 +0800

    MC-538: Capture clicks on the Portal SSO tab
    Adding Google Analytics event tracking to the Moodlecloud theme

--- theme/moodlecloud/javascript/moodlecloud.js
+++ theme/moodlecloud/javascript/moodlecloud.js
@@ -31,4 +31,19 @@ require(['core/str', 'jquery', 'core/config'], function(str, $, config) {
     // Defer the call to the adunblock check by five seconds to account for
     // some network latency in serving the ads.
     setTimeout(checkFunction, 5000);
+
+    $('#portal-link-container a').click(function(e) {
+        if ("ga" in window) {
+            // we have multiple trackers, one for region and one for global, so we need to iterate over each
+            // tracker and send the event data to each
+            trackers = ga.getAll();
+            $.each(trackers, function(i, tracker) {
+                if (tracker) {
+                    tracker.send('event', 'SSO Tab', 'Click', '', {
+                        transport: 'beacon'
+                    });
+                }
+            });
+        }
+    });
 });
