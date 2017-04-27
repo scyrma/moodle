e118fd37c0d4cdaffd9f15c2f639b734739c5a4c
Jordan Tomkinson <jordan@moodle.com>
2017-04-27 15:33:23 +0800

    Replace AWS SDK with catalysts local/aws SDK which brings some minor fixes

--- local/filestorage/classes/exception/quota_exception.php
+++ local/filestorage/classes/exception/quota_exception.php
@@ -2,7 +2,7 @@
 
 namespace local_filestorage\exception;
 
-require_once(dirname(dirname(__DIR__)) . '/vendor/autoload.php');
+require_once(dirname(dirname(__DIR__)) . '/sdk/aws-autoloader.php');
 
 /**
  * File Quota exception.
