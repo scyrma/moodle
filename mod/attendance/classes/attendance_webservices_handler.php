c9d563a4e069ce43e5a8bf791ddc7dcbcd8821a7
Mathieu Petit-Clair <mathieu@petitclair.ca>
2017-05-02 13:33:10 +0800

    mod_attendance: commit 6d8c2c2418

--- mod/attendance/classes/attendance_webservices_handler.php
+++ mod/attendance/classes/attendance_webservices_handler.php
@@ -14,13 +14,15 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
+ * Web Services for Attendance plugin.
  *
- * @package    local_attendance
+ * @package    mod_attendance
  * @copyright  2015 Caio Bressan Doneda
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once(dirname(__FILE__).'/../../../config.php');
+defined('MOODLE_INTERNAL') || die();
+
 require_once(dirname(__FILE__).'/../locallib.php');
 require_once(dirname(__FILE__).'/structure.php');
 require_once(dirname(__FILE__).'/../../../lib/sessionlib.php');
