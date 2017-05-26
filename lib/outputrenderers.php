2d1bedf8c2b4b5ae2bade630ffa2f675cd18faae
Cameron Ball <cameron@moodle.com>
2017-05-26 12:01:07 +0800

    outputrenderers.php: Don't rechcek notifications in footer

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -1303,6 +1303,8 @@ class core_renderer extends renderer_base {
         }
         $footer = str_replace($this->unique_performance_info_token, $performanceinfo, $footer);
 
+        // START MOODLECLOUD HACK.
+        /*
         // Only show notifications when we have a $PAGE context id.
         if (!empty($PAGE->context->id)) {
             $this->page->requires->js_call_amd('core/notification', 'init', array(
@@ -1310,6 +1312,8 @@ class core_renderer extends renderer_base {
                 \core\notification::fetch_as_array($this)
             ));
         }
+        */
+        // END MOODLECLOUD HACK.
         $footer = str_replace($this->unique_end_html_token, $this->page->requires->get_end_code(), $footer);
 
         $this->page->set_state(moodle_page::STATE_DONE);
