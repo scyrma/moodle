4890c13238f45ee75d79a1fe77c64b81c9f4b385
Jordan Tomkinson <jordan@moodle.com>
2017-06-08 13:14:23 +0800

    new ad-hoc task: getairnotifierkey to be executed on post-provision.php

---
+++ local/moodlecloud/classes/tasks/getairnotifierkey.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace local_moodlecloud\tasks;
+use core\task\adhoc_task;
+use core\task\manager;
+use moodle_url;
+use curl;
+use stdClass;
+
+defined('MOODLE_INTERNAL') || die();
+
+class getairnotifierkey extends adhoc_task {
+    public function execute() {
+        global $DB;
+
+        $registered = $DB->count_records('registration_hubs', array('huburl' => HUB_MOODLEORGHUBURL, 'confirmed' => 1));
+
+
+        // Check for valid DNS.
+        if ($registered) {
+            mtrace("Moodlecloud Airnotifier: site is registered on hub, proceeding");
+            $airnotifierkey = $CFG->airnotifieraccesskey;
+            if (empty($airnotifierkey)) {
+                mtrace("Moodlecloud Airnotifier: site has no airnotifier key. Requesting");
+                // setup $USER as the site admin.
+                $USER = $DB->get_record('user', array('id' => 2, 'deleted' => 0), '*', MUST_EXIST);
+                // setup airnotifier manager
+                $manager = new \message_airnotifier_manager();
+                // request key
+                if ($key = $manager->request_accesskey()) {
+                    set_config('airnotifieraccesskey', $key);
+                    $msg = get_string('keyretrievedsuccessfully', 'message_airnotifier');
+                } else {
+                    $msg = get_string('errorretrievingkey', 'message_airnotifier');
+                    manager::queue_adhoc_task(new getairnotifierkey());
+                }
+                mtrace("Moodlecloud Airnotifier: $msg");
+            }else {
+                mtrace("Moodlecloud Airnotifier: Airnotifier key alreaxy exists!");
+            }
+        } else {
+            mtrace("Moodlecloud Airnotifier: site is not registered on hub yet. Queueing self again.");
+            manager::queue_adhoc_task(new getairnotifierkey());
+        }
+    }
+
+}
