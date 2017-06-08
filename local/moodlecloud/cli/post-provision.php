1961eb1fbd28cb1536012f2b6546955f027c3fb4
Jordan Tomkinson <jordan@moodle.com>
2017-06-08 13:14:23 +0800

    new ad-hoc task: getairnotifierkey to be executed on post-provision.php

--- local/moodlecloud/cli/post-provision.php
+++ local/moodlecloud/cli/post-provision.php
@@ -5,7 +5,11 @@ define('CLI_SCRIPT', true);
 require_once(dirname(dirname(dirname(__DIR__))) . '/config.php');
 
 use local_moodlecloud\tasks\register as register_adhoc_task;
+use local_moodlecloud\tasks\getairnotifierkey as getairnotifierkey_adhoc_task;
 use core\task\manager;
 
 // Setup the registration task.
 manager::queue_adhoc_task(new register_adhoc_task());
+// Setup the airnotifier task.
+manager::queue_adhoc_task(new getairnotifierkey_adhoc_task());
+
