6a3c931f01e20a8728a37892850bb047e92ca591
Cameron Ball <cameron@moodle.com>
2017-06-23 10:47:49 +0800

    Initial commit

---
+++ admin/tool/fileslist/classes/collection.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Database row collection
+ *
+ * @package    local_cloud
+ * @copyright  2017 Cameron Ball <cameron@cameron1729.xyz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_fileslist;
+
+defined('MOODLE_INTERNAL') || die();
+
+use Iterator;
+
+interface collection extends Iterator {
+    public function get_count() : int;
+    public function get_all() : array;
+}
