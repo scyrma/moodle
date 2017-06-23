732f0e452469fd11a6604666dd69f3d3be881d52
Cameron Ball <cameron@moodle.com>
2017-06-23 10:47:49 +0800

    Initial commit

---
+++ admin/tool/fileslist/classes/container.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace tool_fileslist;
+
+use core_user;
+use stdClass;
+
+final class container {
+    private static $usercache = [];
+
+    public static function get_file_repository() : file_repository {
+        global $DB;
+        return new file_repository(
+            new db_rows($DB, 'files'),
+            new file_factory(
+                get_file_storage(),
+                function(int $uid) : stdClass {
+                    return self::$usercache[$uid] = self::$usercache[$uid] ?? core_user::get_user($uid);
+                }
+            )
+        );
+    }
+}
