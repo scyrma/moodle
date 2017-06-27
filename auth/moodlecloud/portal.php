56885eb869aef4aac8a09189ab46f5134cbb938f
Jordan Tomkinson <jordan@moodle.com>
2017-06-27 15:47:22 +0800

    auth/moodlecloud/portal.php: checks for logged in user, better portal redirect for non-admins

--- auth/moodlecloud/portal.php
+++ auth/moodlecloud/portal.php
@@ -2,8 +2,11 @@
 
 require_once('../../config.php');
 
-if ($USER->auth === 'moodlecloud') {
+if (isset($USER->auth) && $USER->auth === 'moodlecloud') {
     $auth = get_auth_plugin($USER->auth);
     $url = $auth->get_sso_url();
     redirect($url->out());
+}else {
+    $ssoserver = get_config('auth_moodlecloud', 'ssoserver');
+    redirect($ssoserver.'/en/portal/view/'.$dynamicsite);
 }
