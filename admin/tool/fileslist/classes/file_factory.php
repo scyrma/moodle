a741b5181fa021f1bf5642fee03c8458f88ceea9
Cameron Ball <cameron@moodle.com>
2017-06-28 11:22:12 +0800

    Address confusion surrounding accumulator lambda method with comments.
    Also, convert nested ternary operator in to if statements with early returns
    to improve clarity.

--- admin/tool/fileslist/classes/file_factory.php
+++ admin/tool/fileslist/classes/file_factory.php
@@ -12,6 +12,10 @@ use stdClass;
 
 final class file_factory {
     private $fs;
+
+    /**
+     * callable $getuser A callable that knows how to return a user object.
+     */
     private $getuser;
 
     public function __construct(file_storage $fs, callable $usercache) {
@@ -24,6 +28,8 @@ final class file_factory {
     ) : file {
 
         return new stored_file(
+            // Use the array_accumulator to massage the data from the DB in to the correct
+            // format for the stored_file constructor.
             ...(lambda::array_accumulator($this->fs->get_file_instance($record))
                 ->get_id()->to_int()
                 ->get_userid()->to_int()->transform($this->getuser)
@@ -35,15 +41,28 @@ final class file_factory {
                 ->get_filesize()->to_int()
                 ->get_mimetype()
                 ->insert(function(\stored_file $storedfile, $acc) {
-                    return new moodle_url(
-                        ...($storedfile->get_filearea() == 'private'
-                            ? ['/user/profile.php', ['id' => $storedfile->get_userid()]]
-                            : ($acc->get(2) instanceof context_module || $acc->get(2) instanceof context_course
-                               ? ['/course/view.php', ['id' => $acc->get(2)->get_course_context()->instanceid]]
-                               : [null]
-                            )
-                        )
-                    );
+                    if ($acc->get(2) instanceof context_module ||
+                        $acc->get(2) instanceof context_course
+                    ) {
+                        return new moodle_url('/course/view.php', ['id' => $acc->get(2)->get_course_context()->instanceid]);
+                    }
+
+                    // NB: storedfile refers to Moodle core's stored file class. Not the stored_file class from our plugin.
+                    if ($storedfile->get_filearea() == 'private') {
+                        return new moodle_url('/user/profile.php', ['id' => $storedfile->get_userid()]);
+                    }
+
+                    return new moodle_url(null);
+
+                    // return new moodle_url(
+                    //     ...($storedfile->get_filearea() == 'private'
+                    //         ? ['/user/profile.php', ['id' => $storedfile->get_userid()]]
+                    //         : ($acc->get(2) instanceof context_module || $acc->get(2) instanceof context_course
+                    //            ? ['/course/view.php', ['id' => $acc->get(2)->get_course_context()->instanceid]]
+                    //            : [null]
+                    //         )
+                    //     )
+                    // );
                 })
                 ->dump()
             )
