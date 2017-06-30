a76e014a8f3ea62e52273b52b61a1bf0639b6bf5
Cameron Ball <cameron@moodle.com>
2017-06-30 10:37:09 +0800

    Refactor some code to make things clearer and add comments/doc where necessary.
    A reasonably large change was made to lambda::array_accumulator - it's now called
    array_builder and its methods more clearly describe what it does. The return anon
    class also implements the array_builder interface.

--- admin/tool/fileslist/classes/stored_file.php
+++ admin/tool/fileslist/classes/stored_file.php
@@ -1,20 +1,95 @@
 <?php declare(strict_types=1);
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Stored file class.
+ *
+ * @package    tool_fileslist
+ * @copyright  2017 Cameron Ball <cameron@cameron1729.xyz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 namespace tool_fileslist;
+
+defined('MOODLE_INTERNAL') || die();
 
 use context;
 use moodle_url;
 use stdClass;
 
+/**
+ * Class representing a file stored by Moodle.
+ *
+ * Note this is similar but distinct from core's stored_file class.
+ * This class is simply a data container/aggregate for elements we need
+ * to display to users.
+ *
+ * @copyright 2017 Cameron Ball <cameron@cameron1729.xyz>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 final class stored_file implements file {
+    /**
+     * @var int $id The file's id in the database.
+     */
+    private $id;
+
+    /**
+     * @var stdClass $user User object (i.e. a record from the users table).
+     */
+    private $user;
+
+    /**
+     * @var context $context Context associated with the file.
+     */
     private $context;
+
+    /**
+     * @var string $component Component the file belongs to.
+     */
     private $component;
+
+    /**
+     * @var string $filearea Filearea the file belongs to.
+     */
     private $filearea;
+
+    /**
+     * @var string $path Path to the file.
+     */
     private $path;
+
+    /**
+     * @var string $name Name of the file.
+     */
     private $name;
+
+    /**
+     * @var int $size File size in bytes.
+     */
     private $size;
-    private $userid;
+
+    /**
+     * @var string $mimetype MIME type of the file.
+     */
     private $mimetype;
+
+    /**
+     * @var moodle_url $locationurl URL pointing to where the file is used in Moodle.
+     */
+    private $locationurl;
 
     public function __construct(
         int $id,
