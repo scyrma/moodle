3785d4857b845a1a611f63918299afeeb9d1293a
Cameron Ball <cameron@moodle.com>
2017-06-30 10:37:09 +0800

    Refactor some code to make things clearer and add comments/doc where necessary.
    A reasonably large change was made to lambda::array_accumulator - it's now called
    array_builder and its methods more clearly describe what it does. The return anon
    class also implements the array_builder interface.

--- admin/tool/fileslist/classes/external/file_exporter.php
+++ admin/tool/fileslist/classes/external/file_exporter.php
@@ -1,13 +1,46 @@
 <?php declare(strict_types=1);
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * File exporter.
+ *
+ * @package    tool_fileslist
+ * @copyright  2017 Cameron Ball <cameron@cameron1729.xyz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 namespace tool_fileslist\external;
 
-use tool_fileslist\file;
+defined('MOODLE_INTERNAL') || die();
+
+use context_system;
 use core\external\exporter;
 use renderer_base;
-use context_system;
+use tool_fileslist\file;
 
+/**
+ * Class for exporting a file object.
+ *
+ * @copyright 2017 Cameron Ball <cameron@cameron1729.xyz>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 final class file_exporter extends exporter {
+    /**
+     * @var file $file The file object to export.
+     */
     private $file;
 
     public function __construct(file $file, array $related = []) {
