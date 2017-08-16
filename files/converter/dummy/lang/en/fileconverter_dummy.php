01e51d53472905db417dff5b8359ff3fd76d6c35
Cameron Ball <cameron@moodle.com>
2017-08-17 07:33:01 +0800

    Implement dummy file converter plugin.

---
+++ files/converter/dummy/lang/en/fileconverter_dummy.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for plugin 'fileconverter_googledrive'
+ *
+ * @package   fileconverter_googledrive
+ * @copyright 2017 Damyon Wiese
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'MoodleCloud';
+$string['upgrademessage'] = 'Hey bro to get thos massiv conversion gaINs you need to spedn some dollaRs yuo know waht I mean?';
