c71fd57c312c39d80ef999441d431dbac3fcd470
Cameron Ball <cameron@moodle.com>
2017-08-17 09:20:19 +0800

    No need to do clever stuff, files are deduplicated automagically.

--- files/converter/dummy/classes/converter.php
+++ files/converter/dummy/classes/converter.php
@@ -43,8 +43,7 @@ class converter implements converter_interface {
         // set_sourcefile is a hack to make the document converter API think it's reconverting the same
         // file every time. This makes it clean up old records in the files table so the user's quota
         // shouldn't get taken up by a million placeholder PDFs.
-        $conversion->set_sourcefile(get_file_storage()->get_file_by_id(1))
-                   ->store_destfile_from_path($CFG->dirroot . '/files/converter/dummy/placeholder.pdf')
+        $conversion->store_destfile_from_path($CFG->dirroot . '/files/converter/dummy/placeholder.pdf')
                    ->set('status', conversion::STATUS_COMPLETE)
                    ->set('statusmessage', 'Can I hab a ubrgrade pls?')
                    ->update();
