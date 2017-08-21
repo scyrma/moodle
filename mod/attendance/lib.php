ed88300377418e623917e5b882026b1677f5a099
Mathieu Petit-Clair <mathieu@petitclair.ca>
2017-08-21 10:34:03 +0800

    mod_attendance: commit 5f1114072f

--- mod/attendance/lib.php
+++ mod/attendance/lib.php
@@ -483,9 +483,13 @@ function attendance_print_settings_tabs($selected = 'settings') {
         get_string('coursesummary', 'attendance'), get_string('coursesummary', 'attendance'), false);
 
     if (get_config('attendance', 'enablewarnings')) {
-        $tabs[] = new tabobject('atrisk', $CFG->wwwroot . '/mod/attendance/atrisk.php',
-            get_string('atriskreport', 'attendance'), get_string('atriskreport', 'attendance'), false);
+        $tabs[] = new tabobject('absentee', $CFG->wwwroot . '/mod/attendance/absentee.php',
+            get_string('absenteereport', 'attendance'), get_string('absenteereport', 'attendance'), false);
     }
+
+    $tabs[] = new tabobject('resetcalendar', $CFG->wwwroot.'/mod/attendance/resetcalendar.php',
+        get_string('resetcalendar', 'attendance'), get_string('resetcalendar', 'attendance'), false);
+
     ob_start();
     print_tabs(array($tabs), $selected);
     $tabmenu = ob_get_contents();
