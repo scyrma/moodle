659754222bd6b6844839c6ad771921993358e282
Mathieu Petit-Clair <mathieu@petitclair.ca>
2017-08-21 10:33:36 +0800

    block_xp: commit 85c3371829

--- blocks/xp/classes/filter.php
+++ blocks/xp/classes/filter.php
@@ -110,6 +110,20 @@ class block_xp_filter implements renderable {
     }
 
     /**
+     * Get the record data.
+     *
+     * @return stdClass
+     */
+    public function export() {
+        $record = new stdClass();
+        $record->courseid = $this->courseid;
+        $record->points = $this->points;
+        $record->ruledata = $this->ruledata;
+        $record->sortorder = $this->sortorder;
+        return $record;
+    }
+
+    /**
      * Return whether or not the filter is editable.
      *
      * @return boolean
@@ -194,6 +208,12 @@ class block_xp_filter implements renderable {
     protected function load_rule() {
         $ruledata = json_decode($this->ruledata, true);
         $this->rule = block_xp_rule::create($ruledata);
+
+        // There was a problem loading the rule, let's ignore it and create an empty one.
+        // Most likely the class used went missing, or someone played tricks on us.
+        if ($this->rule === false) {
+            $this->set_rule(new \block_xp_ruleset());
+        }
     }
 
     /**
@@ -219,12 +239,7 @@ class block_xp_filter implements renderable {
         if (!$this->editable) {
             throw new coding_exception('Non-editable filters cannot be saved.');
         }
-        $record = (object) array(
-            'courseid' => $this->courseid,
-            'ruledata' => $this->ruledata,
-            'points' => $this->points,
-            'sortorder' => $this->sortorder,
-        );
+        $record = $this->export();
         if (!$this->id) {
             $this->id = $DB->insert_record('block_xp_filters', $record);
         } else {
