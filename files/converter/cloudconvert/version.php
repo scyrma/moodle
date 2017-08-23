472e4f8c9bbbc9dfc5bdb3ab8bdff3dd78b33e46
Cameron Ball <cameron@moodle.com>
2017-08-23 13:04:54 +0800

    Stop

--- files/converter/cloudconvert/version.php
+++ files/converter/cloudconvert/version.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
+ * Version details.
  *
  * @package   fileconverter_cloudconvert
  * @copyright 2017 Cameron Ball <cameron@cameron1729.xyz>
