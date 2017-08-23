8791b7e46564fe901c7b90fd287a3b00984e6d5c
Cameron Ball <cameron@moodle.com>
2017-08-23 10:59:28 +0800

    Code cleanup

--- files/converter/cloudconvert/lang/en/fileconverter_cloudconvert.php
+++ files/converter/cloudconvert/lang/en/fileconverter_cloudconvert.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for fileconvert_cloudconvert.
+ * Strings for fileconverter_cloudconvert.
  *
  * @package    fileconverter_cloudconvert
  * @copyright  2017 Cameron Ball <cameron@cameron1729.xyz>
