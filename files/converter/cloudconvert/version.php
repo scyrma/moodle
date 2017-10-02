258f5d834f78de3d8ba05b2e0b3e279013b15595
Cameron Ball <cameron@moodle.com>
2017-10-02 16:26:37 +0800

    Update plugin dependencies

--- files/converter/cloudconvert/version.php
+++ files/converter/cloudconvert/version.php
@@ -27,3 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2017051501;         // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2017050500;         // Requires this Moodle version
 $plugin->component = 'fileconverter_cloudconvert'; // Full name of the plugin (used for diagnostics).
+$plugin->dependencies = [
+    'local_logging' => ANY_VERSION,
+    'local_filestorage' => ANY_VERSION
+];
