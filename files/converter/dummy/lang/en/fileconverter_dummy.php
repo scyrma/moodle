d01f17252db0064774d7c05325649cd51f982671
Cameron Ball <cameron@moodle.com>
2017-10-26 12:12:42 +0800

    Update plugin name

--- files/converter/dummy/lang/en/fileconverter_dummy.php
+++ files/converter/dummy/lang/en/fileconverter_dummy.php
@@ -24,5 +24,5 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'Default';
-$string['upgrademessage'] = 'Default document conversion is not available on your Free site. Starter and Moodle for School plans include default document conversion for over 30 file types. To get access to this default document converter, upgrade your plan in the MoodleCloud portal.';
+$string['pluginname'] = 'MoodleCloud Converter';
+$string['upgrademessage'] = 'MoodleCloud document conversion is not available on your Free site. Starter and Moodle for School plans include default document conversion for over 30 file types. To get access to this default document converter, upgrade your plan in the MoodleCloud portal.';
