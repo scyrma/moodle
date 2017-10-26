74b9b7bb9e45684221207765105d49e7a9b81687
Cameron Ball <cameron@moodle.com>
2017-10-26 16:03:00 +0800

    Bump version

--- files/converter/dummy/version.php
+++ files/converter/dummy/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017081500;         // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2017081501;         // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2017050500;         // Requires this Moodle version
 $plugin->component = 'fileconverter_dummy'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [
