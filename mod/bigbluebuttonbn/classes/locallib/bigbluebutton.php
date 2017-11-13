69d2e7e65339c6b56cc4cc193a6fe8a91531d1be
Mathieu Petit-Clair <mathieu@petitclair.ca>
2017-11-13 14:13:17 +0800

    mod_bbb: commit 3bcc67fd5d

---
+++ mod/bigbluebuttonbn/classes/locallib/bigbluebutton.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The mod_bigbluebuttonbn locallib/bigbluebutton.
+ *
+ * @author    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
+ * @copyright 2017 - present Blindside Networks Inc
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ */
+
+namespace mod_bigbluebuttonbn\locallib;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/bigbluebuttonbn/locallib.php');
+
+class bigbluebutton {
+
+    /**
+     * @param string $action
+     * @param array  $data
+     * @param array  $metadata
+     * @return string
+     */
+    public static function action_url($action = '', $data = array(), $metadata = array()) {
+        $baseurl = \mod_bigbluebuttonbn\locallib\config::get('server_url').'api/'.$action.'?';
+        $params = '';
+
+        foreach ($data as $key => $value) {
+            $params .= '&'.$key.'='.urlencode($value);
+        }
+
+        foreach ($metadata as $key => $value) {
+            $params .= '&'.'meta_'.$key.'='.urlencode($value);
+        }
+
+        return $baseurl.$params.'&checksum='.sha1($action.$params.\mod_bigbluebuttonbn\locallib\config::get('shared_secret'));
+    }
+}
