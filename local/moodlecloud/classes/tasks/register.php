8f08c3ed2c3840bcb7a5f2d7a195d7b6d499ca51
Cameron Ball <cameron@moodle.com>
2017-11-14 15:55:48 +0800

    Update register task to use new registration class

--- local/moodlecloud/classes/tasks/register.php
+++ local/moodlecloud/classes/tasks/register.php
@@ -10,8 +10,6 @@ use stdClass;
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once($CFG->dirroot . '/' . $CFG->admin . '/registration/lib.php');
-
 class register extends adhoc_task {
     public function execute($wait_for_dns = true) {
         global $DB;
@@ -66,18 +64,18 @@ class register extends adhoc_task {
 
     public function configure($huburl) {
         global $DB;
-        $registrationmanager = new \registration_manager();
 
         // update user from signup - fetch country
         $admin = update_user_record_by_id(2);
 
         // Ensure that the hub detailsare in place.
         $hub = new stdClass();
-        $hub->token = $registrationmanager->get_site_secret_for_hub($huburl);
+        $hub->token = get_site_identifier();
         $hub->secret = $hub->token;
         $hub->huburl = $huburl;
-        $hub->hubname = 'Moodle.org';
+        $hub->hubname = 'Moodle.net';
         $hub->confirmed = 0;
+        $hub->timemodified = time();
 
         // Grab some useful items here.
         $cleanhuburl = clean_param($huburl, PARAM_ALPHANUMEXT);
@@ -85,43 +83,55 @@ class register extends adhoc_task {
 
         // Set the default values.
         $sitename = format_string($site->fullname, true, array('context' => \context_course::instance(SITEID)));
-        set_config('site_name_'             . $cleanhuburl, $sitename,                  'hub');
+        set_config('site_name_'             . $cleanhuburl, $sitename,                                          'hub');
 
         // Set the site description.
-        set_config('site_description_'      . $cleanhuburl, $sitename,                  'hub');
+        set_config('site_description_'      . $cleanhuburl, $sitename,                                          'hub');
 
         $contactname = fullname($admin, true);
-        set_config('site_contactname_'      . $cleanhuburl, $contactname,               'hub');
+        set_config('site_contactname_'      . $cleanhuburl, $contactname,                                       'hub');
 
-        set_config('site_contactemail_'     . $cleanhuburl, $admin->email,              'hub');
-        set_config('site_privacy_'          . $cleanhuburl, HUB_SITENOTPUBLISHED,       'hub');
+        set_config('site_contactemail_'     . $cleanhuburl, $admin->email,                                      'hub');
+        set_config('site_privacy_'          . $cleanhuburl, \core\hub\registration::HUB_SITENOTPUBLISHED,       'hub');
 
         // 0 = registrationcontactno.
         $contactable = 0;
-        set_config('site_contactable_'      . $cleanhuburl, $contactable,               'hub');
+        set_config('site_contactable_'      . $cleanhuburl, $contactable,                                       'hub');
 
         // Do not alert administrators - we handle upgrades anyway.
         $emailalert = 0;
-        set_config('site_emailalert_'       . $cleanhuburl, $emailalert,                'hub');
+        set_config('site_emailalert_'       . $cleanhuburl, $emailalert,                                        'hub');
 
         // Use the admin's country as site's country
-        set_config('site_country_'          . $cleanhuburl, $admin->country,            'hub');
+        set_config('site_country_'          . $cleanhuburl, $admin->country,                                    'hub');
 
         // By default set this to the current language.
-        set_config('site_language_'         . $cleanhuburl, explode('_', current_language())[0], 'hub');
+        set_config('site_language_'         . $cleanhuburl, explode('_', current_language())[0],                'hub');
+
+        // Don't send comm news.
+        $commnews = 0;
+        set_config('site_commnews_'         . $cleanhuburl, $commnews,                                          'hub');
 
         // Add the new hub details to the database.
-        $hub->id = $registrationmanager->add_registeredhub($hub);
+        $hub->id = $DB->insert_record('registration_hubs', $hub);
+    }
+
+    private function get_unregistered_hub() {
+        return \Closure::bind(
+            function() {
+                return self::get_registration(false);
+            },
+            null,
+            \core\hub\registration::class
+        )();
     }
 
     private function register($huburl) {
         global $CFG;
 
-        $registrationmanager = new \registration_manager();
-
         // Now retrieve everything again.
-        if ($hub = $registrationmanager->get_unconfirmedhub($huburl)) {
-            $params = $registrationmanager->get_site_info($huburl);
+        if ($hub = $this->get_unregistered_hub()) {
+            $params = \core\hub\registration::get_site_info();
             $params['token']    = $hub->token;
             $params['url']      = $CFG->wwwroot;
 
