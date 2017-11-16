6e3f3546eee34f802975a7cbe1f87c9d899e3b80
Cameron Ball <cameron@moodle.com>
2017-11-16 15:37:05 +0800

    Remove erroneous <= in upgrade script

--- files/converter/cloudconvert/db/upgrade.php
+++ files/converter/cloudconvert/db/upgrade.php
@@ -23,7 +23,7 @@ function xmldb_fileconverter_cloudconvert_upgrade($oldversion) {
     // Moodle v3.3.0 release upgrade line.
     // Put any upgrade step following this.
 
-    if ($oldversion <= 2017051501) {
+    if ($oldversion < 2017051501) {
         $existingrecord = $DB->get_record('config', ['name' => 'converter_plugins_sortorder']);
 
         if($existingrecord) {
