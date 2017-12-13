12b9a200e6bb39ee1cd348475d0a98998c777ca8
Craig R Morton <crmpicco@aol.co.uk>
2017-12-13 15:36:10 +0000

    MC-349: Add upsell text to the Ads Blocked modal for free sites
    Removing the Google Analytics event tracking as the majority of adblockers will block GATC from being fired

--- theme/moodlecloud/javascript/moodlecloud.js
+++ theme/moodlecloud/javascript/moodlecloud.js
@@ -50,19 +50,4 @@ require(['core/str', 'jquery', 'core/config'], function(str, $, config) {
             });
         }
     });
-
-    $('.adunblock-upgrade-link').click(function(e) {
-        if ("ga" in window) {
-            // we have multiple trackers, one for region and one for global, so we need to iterate over each
-            // tracker and send the event data to each
-            var trackers = ga.getAll();
-            $.each(trackers, function(i, tracker) {
-                if (tracker) {
-                    tracker.send('event', 'Adunblock Upgrade Link', 'Click', '', {
-                        transport: 'beacon'
-                    });
-                }
-            });
-        }
-    });
 });
