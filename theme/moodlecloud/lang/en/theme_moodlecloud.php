b2998d426270a6bb78b020309ac259b598ca6ff3
Craig R Morton <crmpicco@aol.co.uk>
2017-12-15 09:56:41 +0000

    MC-349: Add upsell text to the Ads Blocked modal for free sites
    Left-aligning the paragraph text on the MoodleCloud theme
    
    Updating the text following feedback from the Brentmeister General and Chris

--- theme/moodlecloud/lang/en/theme_moodlecloud.php
+++ theme/moodlecloud/lang/en/theme_moodlecloud.php
@@ -80,16 +80,16 @@ $string['textcolor_desc'] = 'The colour of the text.';
 $string['adunblock_title'] = 'Ad blocker detected :(';
 $string['adunblock_message_admin'] = '
     <p>We\'ve detected that your browser is blocking ads on this site.</p>
-    <p>MoodleCloud provides a Free plan to users and we depend on advertising to help pay for it.</p>
-    <p>We\'d really appreciate it if you can change your Ad blocker settings to add this site to your whitelist. For more information on how to unblock ads visit <a href="https://moodle.com/cloud/faq#adblock" target="_blank">our FAQs</a></p>    
+    <p>MoodleCloud provides a Free plan to users and advertising helps pay for it.</p>
+    <p>We\'d really appreciate it if you would change your Ad blocker settings to add this site to your whitelist. For more information on how to unblock ads visit <a href="https://moodle.com/cloud/faq#adblock" target="_blank">our FAQs</a></p>    
     <p>Alternatively, consider upgrading to a paid plan and support us directly.</p>
-    <p>A Starter plan hides the ads from your students. There are still some ads shown to admins and teacher roles regarding Moodle services to help you get more from your Moodlecloud site.</p>
+    <p>A Starter plan hides the ads from students. Some ads are shown to admin and teacher roles about Moodle services to help you to get more from your MoodleCloud site.</p>
     <p>Any Moodle for School plan hides all ads from all users.</p>
     <p>You can upgrade anytime in the <a href=\'{$a->sso_link}?adblocker_modal=1\' target=\'_blank\' class=\'adunblock-upgrade-link\'>MoodleCloud portal</a></p>
     <p>Thanks for supporting MoodleCloud!</p>';
 $string['adunblock_message_student'] = '
     <p>We\'ve detected that your browser is blocking ads on this site.</p>
-    <p>MoodleCloud provides a Free plan to users and we depend on advertising to help pay for it.</p>
+    <p>MoodleCloud provides a Free plan to users and advertising helps pay for it.</p>
     <p>We\'d really appreciate it if you can change your Ad blocker settings to add this site to your whitelist. For more information on how to unblock ads visit <a href="https://moodle.com/cloud/faq#adblock" target="_blank">our FAQs</a></p>    
     <p>Alternatively, talk to your site admin to upgrade your site to a paid plan and support us directly.</p>
     <p>Thanks for supporting MoodleCloud!</p>';
