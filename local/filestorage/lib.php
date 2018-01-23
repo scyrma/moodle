e7fdee3c76a11abe27c1d62c9914afc93438ba6f
Jordan Tomkinson <jordan@moodle.com>
2018-01-23 14:01:03 +0800

    WIP

--- local/filestorage/lib.php
+++ local/filestorage/lib.php
@@ -3,6 +3,11 @@
 use local_filestorage\file_storage\file_system_s3;
 use local_filestorage\exception\quota_exception;
 
+require_once(__DIR__ . '/sdk/aws-autoloader.php');
+//use Aws\DynamoDb\DynamoDbClient;
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+
 function local_filestorage_before_file_created($newfile, $fileinfo) {
     if (!defined('FILESTORAGE_QUOTA')) {
         return;
@@ -32,5 +37,131 @@ function local_filestorage_before_file_created($newfile, $fileinfo) {
 
     if (($current + $filesize) > FILESTORAGE_QUOTA) {
         throw new quota_exception($current, $filesize);
+    }
+}
+
+function local_filestorage_after_file_created($newfile) {
+    if ($newfile->filearea == "draft") {
+        return;
+    }
+
+    // $dynamicsite comes from config.php
+    global $dynamicsite;
+
+    $sdk = new Aws\Sdk([
+        'endpoint'   => null,
+        'region'   => 'us-east-1',
+        'version'  => 'latest'
+    ]);
+
+    $dynamodb = $sdk->createDynamoDb();
+    $marshaler = new Marshaler();
+
+    $table = 's3metadata';
+
+    $userid = $newfile->userid;
+    if (!$userid) {
+        $userid = "None";
+    }
+
+    $item = $marshaler->marshalJson('
+        {
+            "id": "'.$dynamicsite.':'.$newfile->id.'",
+            "contenthash": "'.$newfile->contenthash.'",
+            "site": "'.$dynamicsite.'",
+            "filename": "'.$newfile->filename.'",
+            "filesize": "'.$newfile->filesize.'",
+            "userid": "'.$userid.'",
+            "filearea": "'.$newfile->filearea.'",
+            "timecreated": "'.$newfile->timecreated.'",
+            "deleted": 0
+        }
+    ');
+
+    $params = [
+        'TableName' => $table,
+        'Item' => $item
+    ];
+
+    try {
+        $result = $dynamodb->putItem($params);
+        error_log("Added item to DynamoDB: ".$newfile->contenthash);
+    } catch (DynamoDbException $e) {
+        error_log("Unable to add item to DynamoDB: ".$e->getMessage());
+    }
+}
+
+function local_filestorage_after_file_deleted($file) {
+    if ($file->filearea == "draft") {
+        return;
+    }
+    // $dynamicsite comes from config.php
+    global $dynamicsite;
+
+    $sdk = new Aws\Sdk([
+        'endpoint'   => null,
+        'region'   => 'us-east-1',
+        'version'  => 'latest'
+    ]);
+
+    $dynamodb = $sdk->createDynamoDb();
+    $marshaler = new Marshaler();
+
+    $table = 's3metadata';
+
+    $userid = $file->userid;
+    if (!$userid) {
+        $userid = "None";
+    }
+
+    $key = $marshaler->marshalJson('
+        {
+            "id": "'.$dynamicsite.':'.$file->id.'"
+        }
+    ');
+
+    /* 
+    // Update the item.
+    $eav = $marshaler->marshalJson('
+        {
+            ":d": 1,
+            ":ch": "'.$file->contenthash.'",
+            ":s": "'.$dynamicsite.'",
+            ":fn": "'.$file->filename.'",
+            ":fs": "'.$file->filesize.'",
+            ":u": "'. $userid .'",
+            ":fa": "'.$file->filearea.'",
+            ":tc": "'.$file->timecreated.'"
+        }
+    ');
+
+    $params = [
+        'TableName' => $table,
+        'Key' => $key,
+        'UpdateExpression' =>
+            'set deleted = :d, contenthash = :ch, site = :s, filename = :fn, filesize = :fs, userid = :u, filearea = :fa, timecreated = :tc',
+        'ExpressionAttributeValues' => $eav,
+        'ReturnValues' => 'UPDATED_NEW'
+    ];
+    
+    try {
+        $result = $dynamodb->updateItem($params);
+        error_log("Updated (deleted) dynamodb item: ".$file->contenthash);
+    } catch (DynamoDbException $e) {
+        error_log("Unable to (delete) update dynamodb item: ".$file->contenthash." : ".$e->getMessage());
+    }
+    */
+
+    // Delete the item
+    $params = [
+        'TableName' => $table,
+        'Key' => $key
+    ];
+
+    try {
+        $result = $dynamodb->deleteItem($params);
+        error_log("Deleted item from dynamodb: ".$file->contenthash);
+    } catch (DynamoDbException $e) {
+        error_log("Unable to delete from dynamodb item: ".$file->contenthash." : ".$e->getMessage());
     }
 }
