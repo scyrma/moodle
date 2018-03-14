68447a3a254b5e456d3ded76df2bd3046aae4b5a
Cameron Ball <cameron@moodle.com>
2018-03-14 14:24:15 +0800

    Add upgrade step to correct existing touchpoint settings
    This patch also stops free sites being able to disable site notifications.

--- local/moodlecloud/db/upgrade.php
+++ local/moodlecloud/db/upgrade.php
@@ -92,5 +92,36 @@ function xmldb_local_moodlecloud_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2018030600, 'local', 'moodlecloud');
     }
 
+    if ($oldversion < 2018030601) {
+        $userlimitapproaching = get_config('moodlecloudnotifications', 'touchpoints_send_user_limit_warning');
+        $userlimitreached = get_config('moodlecloudnotifications', 'touchpoints_send_user_limit_reached');
+        $filelimitapproaching = get_config('moodlecloudnotifications', 'touchpoints_send_file_storage_limit_warning');
+        $filelimitreached = get_config('moodlecloudnotifications', 'touchpoints_send_file_storage_limit_reached');
+
+        set_config(
+            'touchpoints_send_user_limit_warning',
+            $userlimitapproaching == '0' ? 'sitenotifications' : 'emails,sitenotifications',
+            'moodlecloudnotifications'
+        );
+
+        set_config(
+            'touchpoints_send_user_limit_reached',
+            $userlimitreached == '0' ? 'sitenotifications' : 'emails,sitenotifications',
+            'moodlecloudnotifications'
+        );
+
+        set_config(
+            'touchpoints_send_file_storage_limit_warning',
+            $filelimitapproaching == '0' ? 'sitenotifications' : 'emails,sitenotifications',
+            'moodlecloudnotifications'
+        );
+
+        set_config(
+            'touchpoints_send_file_storage_limit_reached',
+            $filelimitreached == '0' ? 'sitenotifications' : 'emails,sitenotifications',
+            'moodlecloudnotifications'
+        );
+    }
+
     return true;
 }
