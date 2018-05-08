7cf3339f2ad5e2a69cf0a1120d05fa0b260c8aad
Cameron Ball <cameron@moodle.com>
2018-05-08 14:05:26 +0800

    Fix typo: Moodlecloud -> MoodleCloud

--- admin/tool/policy/db/install.php
+++ admin/tool/policy/db/install.php
@@ -30,7 +30,7 @@ function xmldb_tool_policy_install() {
 
     $policyid = $DB->insert_record('tool_policy', ['sortorder' => 0]);
     $versionid = $DB->insert_record('tool_policy_versions', [
-        'name' => 'Moodlecloud policy',
+        'name' => 'MoodleCloud policy',
         'type' => 0,
         'audience' => 0, // Change to 1 if this is for signup/logged in users only
         'usermodified' => 2, // admin
