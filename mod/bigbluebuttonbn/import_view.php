6ececfc4323ea02f4394e465993640ae1a96e9fe
Mathieu Petit-Clair <mathieu@petitclair.ca>
2018-06-14 12:11:22 +0800

    mod_bbb: commit d678a96489

--- mod/bigbluebuttonbn/import_view.php
+++ mod/bigbluebuttonbn/import_view.php
@@ -47,9 +47,7 @@ if (!isset($SESSION) || !isset($SESSION->bigbluebuttonbn_bbbsession)) {
 }
 
 if (!(boolean)\mod_bigbluebuttonbn\locallib\config::importrecordings_enabled()) {
-    // BEGIN MOODLECLOUD hack
-    // print_error(get_string('view_message_importrecordings_disabled', 'bigbluebuttonbn'));
-    // END MOODLECLOUD hack
+    print_error(get_string('view_message_importrecordings_disabled', 'bigbluebuttonbn'));
     return;
 }
 
