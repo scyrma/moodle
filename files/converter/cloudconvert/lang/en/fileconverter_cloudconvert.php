39e1a80be9e3894ab7e6b9767989d6d599888a93
Cameron Ball <cameron@moodle.com>
2018-06-20 14:48:14 +0800

    Update wording for plugin registry

--- files/converter/cloudconvert/lang/en/fileconverter_cloudconvert.php
+++ files/converter/cloudconvert/lang/en/fileconverter_cloudconvert.php
@@ -27,7 +27,7 @@ $string['purge_conversions'] = 'Purge converted files';
 $string['purge_conversions_warning'] = 'This will remove all files converted by the MoodleCloud document converter from your site, then disable the plugin.';
 $string['purge_conversions_complete'] = 'Success.';
 
-$string['privacy:metadata:fileconverter_cloudconvert:externalpurpose'] = 'This information is sent to the CloudConvet API in order for the file to be converted to another format. The file is temporarily kept on CloudConvert\'s servers and is deleted after the conversion is complete.';
+$string['privacy:metadata:fileconverter_cloudconvert:externalpurpose'] = 'This information is sent to an external document conversion service in order to be converted to another format. The file is temporarily kept on the provider\'s servers and is deleted after the conversion is complete. Currently CloudConvert is used to provide this service.';
 $string['privacy:metadata:fileconverter_cloudconvert:filecontent'] = 'The content of the file.';
 $string['privacy:metadata:fileconverter_cloudconvert:filemimetype'] = 'The MIME type of the file.';
-$string['privacy:metadata:fileconverter_cloudconvert:params'] = 'The query parameters passed to the CloudConvert API.';
+$string['privacy:metadata:fileconverter_cloudconvert:params'] = 'The query parameters passed to the external service.';
