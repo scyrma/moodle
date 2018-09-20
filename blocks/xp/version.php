73aec5aa9326125c9b05576eb7bca7a168056d4e
Mathieu Petit-Clair <mathieu@petitclair.ca>
2018-09-20 14:51:52 +0800

    block_xp: commit 7ff3beb24f

--- blocks/xp/version.php
+++ blocks/xp/version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version    = 2018050900;
+$plugin->version    = 2018091700;
 $plugin->requires   = 2014041500;
 $plugin->component  = 'block_xp';
 $plugin->maturity   = MATURITY_STABLE;
-$plugin->release    = '3.2.1';
+$plugin->release    = '3.4.0';
