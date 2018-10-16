8741fbb2069bab9837f01594028b66b71f021384
Jordan Tomkinson <jordan@moodle.com>
2018-10-16 09:37:04 +0300

    AWS SDK to version 3.69.7 - 2018-10-15

--- local/filestorage/sdk/Aws/Lambda/LambdaClient.php
+++ local/filestorage/sdk/Aws/Lambda/LambdaClient.php
@@ -2,6 +2,8 @@
 namespace Aws\Lambda;
 
 use Aws\AwsClient;
+use Aws\CommandInterface;
+use Aws\Middleware;
 
 /**
  * This client is used to interact with AWS Lambda
@@ -20,6 +22,8 @@ use Aws\AwsClient;
  * @method \GuzzleHttp\Promise\Promise deleteEventSourceMappingAsync(array $args = [])
  * @method \Aws\Result deleteFunction(array $args = [])
  * @method \GuzzleHttp\Promise\Promise deleteFunctionAsync(array $args = [])
+ * @method \Aws\Result deleteFunctionConcurrency(array $args = [])
+ * @method \GuzzleHttp\Promise\Promise deleteFunctionConcurrencyAsync(array $args = [])
  * @method \Aws\Result getAccountSettings(array $args = [])
  * @method \GuzzleHttp\Promise\Promise getAccountSettingsAsync(array $args = [])
  * @method \Aws\Result getAlias(array $args = [])
@@ -42,12 +46,20 @@ use Aws\AwsClient;
  * @method \GuzzleHttp\Promise\Promise listEventSourceMappingsAsync(array $args = [])
  * @method \Aws\Result listFunctions(array $args = [])
  * @method \GuzzleHttp\Promise\Promise listFunctionsAsync(array $args = [])
+ * @method \Aws\Result listTags(array $args = [])
+ * @method \GuzzleHttp\Promise\Promise listTagsAsync(array $args = [])
  * @method \Aws\Result listVersionsByFunction(array $args = [])
  * @method \GuzzleHttp\Promise\Promise listVersionsByFunctionAsync(array $args = [])
  * @method \Aws\Result publishVersion(array $args = [])
  * @method \GuzzleHttp\Promise\Promise publishVersionAsync(array $args = [])
+ * @method \Aws\Result putFunctionConcurrency(array $args = [])
+ * @method \GuzzleHttp\Promise\Promise putFunctionConcurrencyAsync(array $args = [])
  * @method \Aws\Result removePermission(array $args = [])
  * @method \GuzzleHttp\Promise\Promise removePermissionAsync(array $args = [])
+ * @method \Aws\Result tagResource(array $args = [])
+ * @method \GuzzleHttp\Promise\Promise tagResourceAsync(array $args = [])
+ * @method \Aws\Result untagResource(array $args = [])
+ * @method \GuzzleHttp\Promise\Promise untagResourceAsync(array $args = [])
  * @method \Aws\Result updateAlias(array $args = [])
  * @method \GuzzleHttp\Promise\Promise updateAliasAsync(array $args = [])
  * @method \Aws\Result updateEventSourceMapping(array $args = [])
@@ -57,4 +69,37 @@ use Aws\AwsClient;
  * @method \Aws\Result updateFunctionConfiguration(array $args = [])
  * @method \GuzzleHttp\Promise\Promise updateFunctionConfigurationAsync(array $args = [])
  */
-class LambdaClient extends AwsClient {}
+class LambdaClient extends AwsClient
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(array $args)
+    {
+        parent::__construct($args);
+        $list = $this->getHandlerList();
+        if (extension_loaded('curl')) {
+            $list->appendInit($this->getDefaultCurlOptionsMiddleware());
+        }
+    }
+
+    /**
+     * Provides a middleware that sets default Curl options for the command
+     *
+     * @return callable
+     */
+    public function getDefaultCurlOptionsMiddleware()
+    {
+        return Middleware::mapCommand(function (CommandInterface $cmd) {
+            $defaultCurlOptions = [
+                CURLOPT_TCP_KEEPALIVE => 1,
+            ];
+            if (!isset($cmd['@http']['curl'])) {
+                $cmd['@http']['curl'] = $defaultCurlOptions;
+            } else {
+                $cmd['@http']['curl'] += $defaultCurlOptions;
+            }
+            return $cmd;
+        });
+    }
+}
