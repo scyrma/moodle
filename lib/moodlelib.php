7326f8d48231b4e501f8f315cdeb4117c556a94c
Jordan Tomkinson <jordan@moodle.com>
2018-12-03 12:11:49 +0800

    MoodleCloud Patches. See CLOUD-3.5.2/FEATURE-moodlecloud for history

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -2698,6 +2698,10 @@ function require_login($courseorid = null, $autologinguest = true, $cm = null, $
         $setwantsurltome = false;
     }
 
+    // START MOODLECLOUD HACK.
+    \auth_moodlecloud\sso::require_login();
+    // END MOODLECLOUD HACK.
+
     // Redirect to the login page if session has expired, only with dbsessions enabled (MDL-35029) to maintain current behaviour.
     if ((!isloggedin() or isguestuser()) && !empty($SESSION->has_timed_out) && !empty($CFG->dbsessions)) {
         if ($preventredirect) {
@@ -9716,6 +9720,14 @@ function get_performance_info() {
     }
 
     $info['html'] = '<div class="performanceinfo siteinfo container-fluid px-md-0 overflow-auto mt-3">'.$info['html'].'</div>';
+    // BEGIN MOODLECLOUD HACK.
+    $info['dbread']         = $DB->perf_get_reads();
+    $info['allwrites']      = $DB->perf_get_writes();
+    $info['perfwrites']     = $PERF->logwrites;
+    $info['finalwrites']    = $info['allwrites'] - $info['perfwrites'];
+    // END MOODLECLOUD HACK.
+
+    $info['html'] = '<div class="performanceinfo siteinfo container-fluid">'.$info['html'].'</div>';
     return $info;
 }
 
