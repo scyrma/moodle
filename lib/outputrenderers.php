bf8dbce1a2db718e92d6b80236deb2d6289098bf
Jordan Tomkinson <jordan@moodle.com>
2018-12-03 12:11:49 +0800

    MoodleCloud Patches. See CLOUD-3.5.2/FEATURE-moodlecloud for history

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -1520,6 +1520,8 @@ class core_renderer extends renderer_base {
         }
         $footer = str_replace($this->unique_performance_info_token, $performanceinfo, $footer);
 
+        // START MOODLECLOUD HACK.
+        /*
         // Only show notifications when the current page has a context id.
         if (!empty($this->page->context->id)) {
             $this->page->requires->js_call_amd('core/notification', 'init', array(
@@ -1527,6 +1529,8 @@ class core_renderer extends renderer_base {
                 \core\notification::fetch_as_array($this)
             ));
         }
+        */
+        // END MOODLECLOUD HACK.
         $footer = str_replace($this->unique_end_html_token, $this->page->requires->get_end_code(), $footer);
 
         $this->page->set_state(moodle_page::STATE_DONE);
