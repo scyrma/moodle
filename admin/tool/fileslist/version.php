a0153f53dd6df6f6107eb5f4043bdb7ab2a6a59c
Cameron Ball <cameron@moodle.com>
2019-01-15 15:54:29 +0800

    Run grunt and bump version

--- admin/tool/fileslist/version.php
+++ admin/tool/fileslist/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017082100;            // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2019011500;            // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017050500;            // Requires this Moodle version.
 $plugin->component = 'tool_fileslist';      // Full name of the plugin (used for diagnostics).
