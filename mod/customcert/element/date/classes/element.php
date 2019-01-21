4b17f9d80f7ba435b58a5728d56fcf906e4fe082
Mathieu Petit-Clair <mathieu@petitclair.ca>
2019-01-21 09:21:37 +0800

    mod_customcert: commit 2ea80b1084

--- mod/customcert/element/date/classes/element.php
+++ mod/customcert/element/date/classes/element.php
@@ -68,12 +68,15 @@ class element extends \mod_customcert\element {
      * @param \mod_customcert\edit_element_form $mform the edit_form instance
      */
     public function render_form_elements($mform) {
-        global $COURSE;
+        global $CFG, $COURSE;
 
         // Get the possible date options.
         $dateoptions = array();
         $dateoptions[CUSTOMCERT_DATE_ISSUE] = get_string('issueddate', 'customcertelement_date');
-        $dateoptions[CUSTOMCERT_DATE_COMPLETION] = get_string('completiondate', 'customcertelement_date');
+        $completionenabled = $CFG->enablecompletion && ($COURSE->id == SITEID || $COURSE->enablecompletion);
+        if ($completionenabled) {
+            $dateoptions[CUSTOMCERT_DATE_COMPLETION] = get_string('completiondate', 'customcertelement_date');
+        }
         $dateoptions[CUSTOMCERT_DATE_COURSE_START] = get_string('coursestartdate', 'customcertelement_date');
         $dateoptions[CUSTOMCERT_DATE_COURSE_END] = get_string('courseenddate', 'customcertelement_date');
         $dateoptions[CUSTOMCERT_DATE_COURSE_GRADE] = get_string('coursegradedate', 'customcertelement_date');
@@ -187,7 +190,12 @@ class element extends \mod_customcert\element {
 
         // Ensure that a date has been set.
         if (!empty($date)) {
-            \mod_customcert\element_helper::render_content($pdf, $this, $this->get_date_format_string($date, $dateformat));
+            $date = $this->get_date_format_string($date, $dateformat);
+            // If we are previewing, we want to let the user know it's an example date so they don't get confused.
+            if ($preview) {
+                $date = get_string('exampledata', 'customcert', 'date') . ' ' . $date;
+            }
+            \mod_customcert\element_helper::render_content($pdf, $this, $date);
         }
     }
 
