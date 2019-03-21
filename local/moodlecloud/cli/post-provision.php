36178b9d767100e27e016c91c9b10084bf01a11e
Jordan Tomkinson <jordan@moodle.com>
2019-03-21 15:00:27 +0800

    local/moodlecloud: post-provision.php clears hub reg config

--- local/moodlecloud/cli/post-provision.php
+++ local/moodlecloud/cli/post-provision.php
@@ -4,6 +4,10 @@ define('CLI_SCRIPT', true);
 
 require_once(dirname(dirname(dirname(__DIR__))) . '/config.php');
 
+global $DB;
+$DB->execute("TRUNCATE m_registration_hubs");
+$DB->execute("DELETE FROM m_config_plugins WHERE plugin='hub'");
+
 use local_moodlecloud\tasks\register as register_adhoc_task;
 use local_moodlecloud\tasks\getairnotifierkey as getairnotifierkey_adhoc_task;
 use core\task\manager;
