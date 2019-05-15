8267558037bb04150ad3ffab4b58d03b56922c0b
Marina Glancy <marina@moodle.com>
2019-05-15 11:16:01 +0200

    WP-601 user visibility for multitenancy

--- lib/accesslib.php
+++ lib/accesslib.php
@@ -3114,9 +3114,11 @@ function get_assignable_roles(context $context, $rolenamedisplay = ROLENAME_ALIA
     $extrafields = '';
 
     if ($withusercounts) {
+        /** @uses \tool_tenant\tenancy::get_users_subquery */
+        $tenantwhere = component_class_callback('tool_tenant\\tenancy', 'get_users_subquery', [], '');
         $extrafields = ', (SELECT COUNT(DISTINCT u.id)
                              FROM {role_assignments} cra JOIN {user} u ON cra.userid = u.id
-                            WHERE cra.roleid = r.id AND cra.contextid = :conid AND u.deleted = 0
+                            WHERE ' . $tenantwhere . ' cra.roleid = r.id AND cra.contextid = :conid AND u.deleted = 0
                           ) AS usercount';
         $params['conid'] = $context->id;
     }
@@ -4044,6 +4046,9 @@ function get_role_users($roleid, context $context, $parent = false, $fields = ''
         $ejoin = "";
     }
 
+    /** @uses \tool_tenant\tenancy::get_users_subquery */
+    $tenantwhere = component_class_callback('tool_tenant\\tenancy', 'get_users_subquery', [], '');
+
     $sql = "SELECT DISTINCT $fields, ra.roleid
               FROM {role_assignments} ra
               JOIN {user} u ON u.id = ra.userid
@@ -4051,7 +4056,8 @@ function get_role_users($roleid, context $context, $parent = false, $fields = ''
             $ejoin
          LEFT JOIN {role_names} rn ON (rn.contextid = :coursecontext AND rn.roleid = r.id)
         $groupjoin
-             WHERE (ra.contextid = :contextid $parentcontexts)
+             WHERE $tenantwhere
+                   (ra.contextid = :contextid $parentcontexts)
                    $roleselect
                    $groupselect
                    $extrawheretest
