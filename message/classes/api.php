e5c0058e849bd9ab8643f04baf3fc776f750ad7c
Marina Glancy <marina@moodle.com>
2019-05-15 11:16:01 +0200

    WP-601 user visibility for multitenancy

--- message/classes/api.php
+++ message/classes/api.php
@@ -2944,6 +2944,13 @@ class api {
      * @return bool true if recipient hasn't blocked sender and sender can contact to recipient, false otherwise.
      */
     protected static function can_contact_user(int $recipientid, int $senderid) : bool {
+
+        /** @uses \tool_tenant\tenancy::is_user_hidden_by_tenancy */
+        if (component_class_callback('tool_tenant\\tenancy', 'is_user_hidden_by_tenancy',
+                [$recipientid, $senderid])) {
+            return false;
+        }
+
         if (has_capability('moodle/site:messageanyuser', \context_system::instance(), $senderid) ||
             $recipientid == $senderid) {
             // The sender has the ability to contact any user across the entire site or themselves.
