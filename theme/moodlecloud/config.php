1652aed58839ca7589ee76070a2ca7950c480c7d
Cameron Ball <cameron@moodle.com>
2019-05-20 11:59:39 +0800

    Remove old moodlecloud theme and replace it with the new boost based theme (commit: faa2a40e)

--- theme/moodlecloud/config.php
+++ theme/moodlecloud/config.php
@@ -18,26 +18,143 @@
  * Theme moodlecloud config file.
  *
  * @package    theme_moodlecloud
- * @copyright  2014 Frédéric Massart
+ * @copyright  2019 Michael Hawkins
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
 $THEME->name = 'moodlecloud';
-$THEME->parents = array('clean', 'bootstrapbase');
-
-$THEME->iconsystem = \core\output\icon_system::FONTAWESOME;
-$THEME->doctype = 'html5';
-$THEME->sheets = array('custom');
-$THEME->lessfile = 'moodle';
-$THEME->parents_exclude_sheets = array('bootstrapbase' => array('moodle'), 'clean' => array('custom'));
-$THEME->lessvariablescallback = 'theme_moodlecloud_less_variables';
-$THEME->extralesscallback = 'theme_moodlecloud_extra_less';
-$THEME->supportscssoptimisation = false;
-$THEME->yuicssmodules = array();
-$THEME->enable_dock = true;
-$THEME->editor_sheets = array();
-
+$THEME->sheets = [];
+$THEME->editor_sheets = [];
+$THEME->parents = ['boost', 'classic'];
+$THEME->enable_dock = false; // Not supported by the Classic theme.
+$THEME->yuicssmodules = [];
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';
-$THEME->csspostprocess = 'theme_moodlecloud_process_css';
+$THEME->extrascsscallback = 'theme_moodlecloud_get_extra_scss';
+$THEME->prescsscallback = 'theme_moodlecloud_get_pre_scss';
+$THEME->precompiledcsscallback = 'theme_moodlecloud_get_precompiled_css';
 
-$THEME->javascripts_footer = array('moodlecloud');
+$THEME->scss = function($theme) {
+    return theme_moodlecloud_get_main_scss_content($theme);
+};
+
+$THEME->layouts = [
+    // Most backwards compatible layout without the blocks - this is the layout used by default.
+    'base' => array(
+        'file' => 'columns.php',
+        'regions' => array(),
+    ),
+    // Standard layout with blocks, this is recommended for most pages with general information.
+    'standard' => array(
+        'file' => 'columns.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-pre',
+    ),
+    // Main course page.
+    'course' => array(
+        'file' => 'columns.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-pre',
+        'options' => array('langmenu' => true),
+    ),
+    'coursecategory' => array(
+        'file' => 'columns.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    // Part of course, typical for modules - default page layout if $cm specified in require_login().
+    'incourse' => array(
+        'file' => 'columns.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    // The site home page.
+    'frontpage' => array(
+        'file' => 'columns.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-pre',
+        'options' => array('nofullheader' => true),
+    ),
+    // Server administration scripts.
+    'admin' => array(
+        'file' => 'columns.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    // My dashboard page.
+    'mydashboard' => array(
+        'file' => 'columns.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-pre',
+        'options' => array('nonavbar' => true, 'langmenu' => true, 'nocontextheader' => true),
+    ),
+    // My public page.
+    'mypublic' => array(
+        'file' => 'columns.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    'login' => array(
+        'theme' => 'boost',
+        'file' => 'login.php',
+        'regions' => array(),
+        'options' => array('langmenu' => true),
+    ),
+
+    // Pages that appear in pop-up windows - no navigation, no blocks, no header.
+    'popup' => array(
+        'theme' => 'classic',
+        'file' => 'contentonly.php',
+        'regions' => array(),
+        'options' => array('nofooter' => true, 'nonavbar' => true),
+    ),
+    // No blocks and minimal footer - used for legacy frame layouts only!
+    'frametop' => array(
+        'theme' => 'classic',
+        'file' => 'contentonly.php',
+        'regions' => array(),
+        'options' => array('nofooter' => true, 'nocoursefooter' => true),
+    ),
+    // Embeded pages, like iframe/object embeded in moodleform - it needs as much space as possible.
+    'embedded' => array(
+        'theme' => 'boost',
+        'file' => 'embedded.php',
+        'regions' => array()
+    ),
+    // Used during upgrade and install, and for the 'This site is undergoing maintenance' message.
+    // This must not have any blocks, links, or API calls that would lead to database or cache interaction.
+    // Please be extremely careful if you are modifying this layout.
+    'maintenance' => array(
+        'theme' => 'boost',
+        'file' => 'maintenance.php',
+        'regions' => array(),
+    ),
+    // Should display the content and basic headers only.
+    'print' => array(
+        'theme' => 'classic',
+        'file' => 'contentonly.php',
+        'regions' => array(),
+        'options' => array('nofooter' => true, 'nonavbar' => false),
+    ),
+    // The pagelayout used when a redirection is occuring.
+    'redirect' => array(
+        'theme' => 'boost',
+        'file' => 'embedded.php',
+        'regions' => array(),
+    ),
+    // The pagelayout used for reports.
+    'report' => array(
+        'file' => 'columns.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    // The pagelayout used for safebrowser and securewindow.
+    'secure' => array(
+        'theme' => 'classic',
+        'file' => 'secure.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre'
+    )
+];
