c2065599547095dbfc62dcbf7137e60ad93d56e3
Cameron Ball <cameron@moodle.com>
2019-05-29 14:27:22 +0800

    Remove try again link since moodle strips it out

--- lang/en/error.php
+++ lang/en/error.php
@@ -200,7 +200,7 @@ $string['csvinvalidcolsnum'] = 'Invalid CSV file - each line must include 49 or 
 $string['csvloaderror'] = 'An error occurred while loading the CSV file: {$a}';
 $string['csvweirdcolumns'] = 'Invalid CSV file format - number of columns is not constant!';
 // START MOODLECLOUD HACK
-$string['dbconnectionfailed'] = '<p>Sorry, something went wrong.</p><p>We\'re working on it and we\'ll get it fixed as soon as we can.</p><p><a href="javascript:history.go(0)">Try Again</a></p>';
+$string['dbconnectionfailed'] = '<p>Sorry, something went wrong.</p><p>We\'re working on it and we\'ll get it fixed as soon as we can.</p>';
 // END MOODLECLOUD HACK
 $string['dbdriverproblem'] = '<p>Error: database driver problem detected</p>
 <p>The site administrator should verify server configuration</p><p>{$a}</p>';
