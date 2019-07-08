1b12c3696d81ae21686f75330f5d7f7e55e28e9b
Moodle Workplace <workplace@moodle.com>
2019-07-08 02:20:13 +0000

    Moodle Workplace plugin tool_datastore (build 30537)

--- admin/tool/datastore/version.php
+++ admin/tool/datastore/version.php
@@ -24,11 +24,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019062500;
-$plugin->requires  = 2019052000.04;
-$plugin->release   = '3.7';
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->component = 'tool_datastore';
+// This plugin is part of Moodle Workplace product.
+$plugin->component    = 'tool_datastore';
+$plugin->release      = '3.7.1';
+$plugin->version      = 2019070700;
+$plugin->requires     = 2019052001;
+$plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [
-    'tool_tenant' => 2019062500
+    'tool_tenant'     => 2019070700,
 ];
