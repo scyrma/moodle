799185c3dcfc79b601aff33621b280d7bd51012b
Cameron Ball <cameron@moodle.com>
2019-07-29 14:42:35 +0800

    Use an array to detect blocked tasks for the task logger

--- local/moodlecloud/classes/moodlecloud_task_logger.php
+++ local/moodlecloud/classes/moodlecloud_task_logger.php
@@ -16,7 +16,8 @@ class moodlecloud_task_logger extends database_logger {
         float $timestart,
         float $timeend
     ) {
-        if (strpos(get_class($task), 'moodlecloud') === false) {
+        global $CFG;
+        if (!in_array(get_class($task), $CFG->moodlecloud_blocked_task_logs ?? [])) {
             parent::store_log_for_task($task, $logpath, $failed, $dbreads, $dbwrites, $timestart, $timeend);
         }
     }
