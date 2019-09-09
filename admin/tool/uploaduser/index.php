4946d6bda05cf522c4762bde5712845a568f6dce
Daniel Neis Araujo <danielneis@gmail.com>
2019-09-09 17:02:18 -0300

    WP-744 tool_uploaduser: mark courses complete

--- admin/tool/uploaduser/index.php
+++ admin/tool/uploaduser/index.php
@@ -1156,6 +1156,15 @@ if ($formdata = $mform2->is_cancelled()) {
                 }
             }
         }
+
+        foreach ($filecolumns as $column) {
+            /** @uses \tool_wp\tool_uploaduser::process_user_after_enrol() */
+            if (!component_class_callback('tool_wp\tool_uploaduser', 'process_user_after_enrol',
+                    [$user, $filecolumns, $upt, &$ccache], true)) {
+                $upt->track('tool_wp', get_string('cannotupdateuser', 'error'), 'error');
+                continue;
+            }
+        }
         $validation[$user->username] = core_user::validate($user);
     }
     $upt->close(); // close table
