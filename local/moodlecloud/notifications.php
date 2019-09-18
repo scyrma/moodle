61fe16091cca364eb4b26e5a3d4f2fc22fd0a2b6
Cameron Ball <cameron@moodle.com>
2019-09-18 15:39:26 +0800

    Remove more references to moodlecloud in notification strings

--- local/moodlecloud/notifications.php
+++ local/moodlecloud/notifications.php
@@ -11,8 +11,8 @@ require_login();
 $PAGE->set_context(context_system::instance());
 $PAGE->set_pagelayout('admin');
 
-$PAGE->set_title("{$SITE->shortname}: " . get_string('moodlecloudsitenotifications', 'local_moodlecloud'));
-$PAGE->set_heading(get_string('moodlecloudsitenotifications', 'local_moodlecloud'));
+$PAGE->set_title("{$SITE->shortname}: " . get_string('sitenotifications', 'local_moodlecloud'));
+$PAGE->set_heading(get_string('sitenotifications', 'local_moodlecloud'));
 
 echo $OUTPUT->header();
 echo $OUTPUT->render_from_template('local_moodlecloud/notification_area', [
