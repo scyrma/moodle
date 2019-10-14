9513eef48a477735ded07a86edfdb694e2dcc59a
Cameron Ball <cameron@moodle.com>
2019-10-14 13:24:23 +0800

    Implement new 'super admin' check

--- local/moodlecloud/classes/siteowner_configmulticheckbox.php
+++ local/moodlecloud/classes/siteowner_configmulticheckbox.php
@@ -36,7 +36,7 @@ final class siteowner_configmulticheckbox extends admin_setting_configmulticheck
         }
 
         // Only write setting for Superman.
-        if ($USER->auth === 'moodlecloud') {
+        if (local_moodlecloud_is_super_admin($CFG->moodlecloud_super_admins, $USER->id)) {
             return parent::write_setting($data);
         }
 
