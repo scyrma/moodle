32fa5213602805325156bfb9a1dfa35e91d6d766
Marina Glancy <marina@moodle.com>
2019-10-18 17:23:02 +0200

    WP-1137 display workplace copyright

--- admin/renderer.php
+++ admin/renderer.php
@@ -313,8 +313,8 @@ class core_admin_renderer extends plugin_renderer_base {
         $output .= $this->mobile_configuration_warning($mobileconfigured);
         $output .= $this->forgotten_password_url_warning($invalidforgottenpasswordurl);
 
-        /** @uses \tool_wp\copyright::copyright() */
-        $output .= component_class_callback('tool_wp\copyright', 'copyright', [], '');
+        /** @uses \tool_wp\copyright::workplace() */
+        $output .= component_class_callback('tool_wp\workplace', 'copyright', [], '');
 
         //////////////////////////////////////////////////////////////////////////////////////////////////
         ////  IT IS ILLEGAL AND A VIOLATION OF THE GPL TO HIDE, REMOVE OR MODIFY THIS COPYRIGHT NOTICE ///
