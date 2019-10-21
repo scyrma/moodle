3373922a1dbae6c55607f7c529dc49a69a50dadb
Cameron Ball <cameron@moodle.com>
2019-10-21 11:43:56 +0800

    Prevent deletion of MCRP customer account

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -4185,6 +4185,12 @@ function delete_user(stdClass $user) {
         return false;
     }
 
+    // BEGIN MOODLECLOUD HACK
+    if (local_moodlecloud_is_super_admin($CFG->moodlecloud_super_admins, $user->id)) {
+        throw new moodle_exception("Customer's account cannot be deleted");
+    }
+    // END MOODLECLOUD HACK
+
     // Allow plugins to use this user object before we completely delete it.
     if ($pluginsfunction = get_plugins_with_function('pre_user_delete')) {
         foreach ($pluginsfunction as $plugintype => $plugins) {
