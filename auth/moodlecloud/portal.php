33a4de53c3b7945b67ca031185c30eb2b23a6996
Cameron Ball <cameron@moodle.com>
2019-10-21 13:01:10 +0800

    Revert some of the super admin checks back to moodlecloud auth check (specifically ones around portal SSO links)

--- auth/moodlecloud/portal.php
+++ auth/moodlecloud/portal.php
@@ -2,7 +2,7 @@
 
 require_once('../../config.php');
 
-if (local_moodlecloud_is_super_admin($CFG->moodlecloud_super_admins, $USER->id)) {
+if ($USER->auth == "moodlecloud") {
     $auth = get_auth_plugin($USER->auth);
     $url = $auth->get_sso_url();
     redirect($url->out());
