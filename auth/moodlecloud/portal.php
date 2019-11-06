c955cbb868ff19b372b201381835784e08bb7d72
Cameron Ball <cameron@moodle.com>
2019-11-06 11:25:10 +0800

    Add missing isset, oh yeah

--- auth/moodlecloud/portal.php
+++ auth/moodlecloud/portal.php
@@ -2,7 +2,7 @@
 
 require_once('../../config.php');
 
-if ($USER->auth == "moodlecloud") {
+if (isset($USER->auth) && $USER->auth == "moodlecloud") {
     $auth = get_auth_plugin($USER->auth);
     $url = $auth->get_sso_url();
     redirect($url->out());
