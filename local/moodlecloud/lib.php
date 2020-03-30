102d757bd2a9ac02082d2f8776a1ca6f6bd70e89
Cameron Ball <cameron@moodle.com>
2020-03-30 10:00:47 +0800

    MC-1605: Free trial
    - Add countdown in main navbar
    - Update auth_moodlecloud to ask signup for the upgrade tab

--- local/moodlecloud/lib.php
+++ local/moodlecloud/lib.php
@@ -43,7 +43,23 @@ function local_moodlecloud_render_navbar_output(renderer_base $renderer) {
     $userpercentage = userquota::site_has_unlimited_quota() ? 1 : min(1, 1 - userquota::number_of_user_slots_remaining() / MOODLECLOUD_USER_QUOTA);
     $storagepercentage = local_filestorage_site_has_unlimited_quota() ? 1 : min(1, file_system_s3::unique_storage_size_used() / FILESTORAGE_QUOTA);
 
+    $daysremainingcalculation = function(int $start, int $duration) : string {
+        $startdate = (new DateTimeImmutable)->setTimestamp($start);
+        $enddate = $startdate->add(new DateInterval('P' . (string)$duration . 'D'));
+        $daysleft = $enddate->diff(new DateTimeImmutable('now'))->format('%a');
+        return $daysleft . ($daysleft > 1 ? " days" : " day");
+    };
+
+    $upgradeurl = (new moodle_url('/auth/moodlecloud/portal.php', ['gotoupgradetab' => true]))->out();
+
     return
+        // TODO: This should be done using language strings and templates, but we need this to go out without cache purge
+        // also inline styles are being used because adding it to the CSS file requires cache purge.
+        ((defined('MOODLECLOUD_TRIAL_START') && defined('MOODLECLOUD_TRIAL_DURATION'))
+         ? "<div style=\"color: white; display: inline-block; padding-top: 8px; margin-left: 20px; margin-right: 12px; background: #f95b12; height: 100%; padding-left: 12px; padding-right: 12px;\" id=\"trial-text\">Your MoodleCloud Free Trial will expire in " . $daysremainingcalculation(MOODLECLOUD_TRIAL_START, MOODLECLOUD_TRIAL_DURATION) . ". <a href=\"$upgradeurl\" style=\"color: white; text-decoration: underline;\">Upgrade</a> to keep this site active.</div>"
+         : ""
+        )
+        .
         (($DB->count_records('moodlecloud_notifications') === 0)
             ? ""
             : $renderer->render_from_template(
