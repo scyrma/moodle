1ed30c28d42ae1ed5c3c94e211cf80e5a20123d1
Cameron Ball <cameron@moodle.com>
2020-03-30 10:00:47 +0800

    MC-1605: Free trial
    - Add countdown in main navbar
    - Update auth_moodlecloud to ask signup for the upgrade tab

--- auth/moodlecloud/portal.php
+++ auth/moodlecloud/portal.php
@@ -2,9 +2,11 @@
 
 require_once('../../config.php');
 
+$gotoupgradetab = optional_param('gotoupgradetab', false, PARAM_BOOL);
+
 if (isset($USER->auth) && $USER->auth == "moodlecloud") {
     $auth = get_auth_plugin($USER->auth);
-    $url = $auth->get_sso_url();
+    $url = $auth->get_sso_url($gotoupgradetab);
     redirect($url->out());
 }else {
     $signupurl = get_config('auth_moodlecloud', 'signupurl');
