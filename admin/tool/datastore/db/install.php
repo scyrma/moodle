92298739867458b347a5810b9c2be10bca8ce6ca
Moodle Workplace <workplace@moodle.com>
2020-05-12 08:01:54 +0000

    Moodle Workplace plugin tool_datastore (build 46390)

---
+++ admin/tool/datastore/db/install.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Code to be executed after the plugin installation
+ *
+ * @package     tool_datastore
+ * @copyright   2020 Moodle Pty Ltd <support@moodle.com>
+ * @author      2020 Paul Holden <paulh@moodle.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Execute post-install method, schedule ad-hoc task to migrate existing course completion data
+ *
+ * @return bool
+ */
+function xmldb_tool_datastore_install() {
+    if (!during_initial_install()) {
+        $task = new \tool_datastore\task\migrate_course_completion();
+
+        \core\task\manager::queue_adhoc_task($task, true);
+    }
+
+    return true;
+}
