a792a12be34196ab774affae8405c227997c2d5a
Moodle Workplace <workplace@moodle.com>
2020-05-12 08:01:54 +0000

    Moodle Workplace plugin tool_certification (build 46390)

---
+++ admin/tool/certification/classes/tool_reportbuilder/datasources/report_certifications.php
@@ -0,0 +1,164 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class report_certifications
+ *
+ * @package    tool_certification
+ * @author     2019 David Matamoros <davidmc@moodle.com>
+ * @copyright  2019 Moodle Pty Ltd <support@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace tool_certification\tool_reportbuilder\datasources;
+
+use tool_certification\local\helpers\certification_entity;
+use tool_certification\local\helpers\certification_format;
+use tool_certification\local\helpers\certificationuser_entity;
+use tool_program\local\helpers\program_entity;
+use tool_program\local\helpers\programcontent_entity;
+use tool_reportbuilder\datasource;
+use tool_reportbuilder\local\entities\course;
+use tool_reportbuilder\local\entities\user;
+use lang_string;
+use tool_reportbuilder\local\helpers\columns;
+use tool_reportbuilder\report_column;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class report_certifications
+ *
+ * @package    tool_certification
+ * @author     2019 David Matamoros <davidmc@moodle.com>
+ * @copyright  2019 Moodle Pty Ltd <support@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class report_certifications extends datasource {
+
+    /**
+     * Initialise report
+     */
+    protected function initialise(): void {
+        $this->set_main_table('tool_certification', 'tc');
+        $this->add_base_join('INNER JOIN {tool_program} tp ON tp.id = tc.program');
+
+        $this->set_downloadable(true);
+        $this->set_columns();
+
+        // Default columns.
+        if ($column = $this->get_column('tool_certification:fullname')) {
+            $column->set_is_default(true, 1);
+            $column->set_is_sortable(true, true);
+        }
+        if ($column = $this->get_column('tool_program:fullnamewithimage')) {
+            $column->set_is_default(true, 2);
+            $column->set_is_sortable(true, true);
+        }
+        if ($column = $this->get_column('tool_program:numbercoursesunique')) {
+            $column->set_is_default(true, 3);
+            $column->set_is_sortable(true, true);
+        }
+        if ($column = $this->get_column('tool_program_content:coursesinsetcommaseparatedlinks')) {
+            $column->set_is_default(true, 4);
+            $column->set_is_sortable(true, true);
+        }
+
+        // Add default conditions.
+        $conditions = $this->get_conditions();
+        $conditions['tool_certification:archived']->set_is_default(true, ['archived_op' => 2, 'archived' => 0]);
+        $conditions['tool_program:archived']->set_is_default(true, ['archived_op' => 2, 'archived' => 0]);
+        $conditions['tool_program:visible']->set_is_default(true, ['visible_op' => 1, 'visible' => 1]);
+
+        // Add default filters.
+        $filters = $this->get_filters();
+        $filters['tool_certification:fullname']->set_is_default(true);
+        $filters['tool_program:programselector']->set_is_default(true);
+        $filters['tool_certification:archived']->set_is_default(true);
+        $filters['tool_program:course']->set_is_default(true);
+        $filters['tool_certification:timecreated']->set_is_default(true);
+        $filters['tool_certification:timemodified']->set_is_default(true);
+    }
+
+    /**
+     * Get the visible name of the report.
+     *
+     * @return string
+     */
+    public static function get_name(): string {
+        return get_string('certifications', 'tool_certification');
+    }
+
+    /**
+     * Returns the certification_user/user join.
+     *
+     * @return array
+     */
+    private static function get_users_joins(): array {
+        // Added tcc join here because status in certification user uses completion table.
+        return [
+            'LEFT JOIN {tool_certification_users} tcu ON tcu.certificationid = tc.id',
+            'LEFT JOIN {user} u ON u.id = tcu.userid',
+            'LEFT JOIN {tool_certification_compltion} tcc '.
+            'ON tcc.certificationid = tcu.certificationid AND tcc.userid = tcu.userid AND tcc.timerevoked = 0 AND tcc.islast = 1',
+            'LEFT JOIN {tool_program_users} tpu '.
+            'ON tpu.programid = tcc.programid AND tcu.certificationid = tpu.certificationid AND tcu.userid = tpu.userid'
+        ];
+    }
+
+    /**
+     * Set the columns available for the report and the definition of each.
+     *
+     */
+    protected function set_columns(): void {
+        $setsjoin = 'LEFT JOIN {tool_program_sets} tps ON tps.programid = tp.id';
+
+        $coursesjoins = ['LEFT JOIN {tool_program_courses} tpc ON tpc.setid = tps.id',
+            'LEFT JOIN {course} c ON c.id = tpc.courseid'];
+
+        $this->add_entity(new certification_entity());
+
+        $this->add_entity((new certificationuser_entity())
+            ->add_joins(self::get_users_joins()));
+
+        $this->add_entity(new program_entity());
+
+        $this->add_entity((new programcontent_entity())
+            ->add_join($setsjoin));
+
+        $this->add_entity((new user())
+            ->add_join(self::get_users_joins()[0])
+            ->add_join(self::get_users_joins()[1]));
+
+        $this->add_entity((new course())
+            ->add_join($setsjoin)
+            ->add_joins($coursesjoins)
+            ->set_entity_title(new lang_string('programcourse', 'tool_certification')));
+
+        // Actions column.
+        $column = (new report_column(
+            'actions',
+            new \lang_string('actions', 'tool_certification'),
+            'tool_certification'
+        ))
+            ->add_fields('tc.id')
+            ->add_callback([certification_format::class, 'actions']);
+        columns::disable_column_aggregation($column);
+        $this->add_column($column);
+    }
+}
---
+++ admin/tool/certification/tests/outcome_allocation_test.php
@@ -0,0 +1,211 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * File contains the unit tests for outcome allocation class.
+ *
+ * @package    tool_certification
+ * @category   test
+ * @author     2019 Daniel Neis Araujo <daniel@moodle.com>
+ * @copyright  2019 Moodle Pty Ltd <support@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+use tool_certification\tool_dynamicrule\outcome\allocation;
+use tool_tenant\tenancy;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Unit tests for outcome\allocation class.
+ *
+ * @package    tool_certification
+ * @group      tool_certification
+ * @author     2019 Daniel Neis Araujo <daniel@moodle.com>
+ * @copyright  2019 Moodle Pty Ltd <support@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class tool_certification_outcome_allocation_testcase extends advanced_testcase {
+
+    /**
+     * Set up
+     */
+    public function setUp() {
+        global $CFG;
+        $this->resetAfterTest();
+        if (!file_exists("{$CFG->dirroot}/{$CFG->admin}/tool/dynamicrule/")) {
+            $this->markTestSkipped('Can not find tool_dynamicrule');
+        }
+    }
+
+    /**
+     * Get dynamic rule generator
+     *
+     * @return tool_dynamicrule_generator
+     */
+    protected function get_generator(): tool_dynamicrule_generator {
+        /** @var tool_dynamicrule_generator $dynamicrulegenerator */
+        $dynamicrulegenerator = self::getDataGenerator()->get_plugin_generator('tool_dynamicrule');
+        return $dynamicrulegenerator;
+    }
+
+    /**
+     * Get tenant generator
+     *
+     * @return tool_tenant_generator|component_generator_base
+     */
+    public function get_tenant_generator(): tool_tenant_generator {
+        return self::getDataGenerator()->get_plugin_generator('tool_tenant');
+    }
+
+    /**
+     * Test get_title
+     */
+    public function test_get_title(): void {
+        $outcome = new allocation();
+        $this->assertNotEmpty($outcome->get_title());
+    }
+
+    /**
+     * Test validate_config_form
+     */
+    public function test_validate_config_form(): void {
+        $outcome = new allocation();
+        $configform = ['certificationid' => 123];
+        $this->assertArrayHasKey('certificationid', $outcome->validate_config_form($configform));
+
+        $defaulttenantid = tenancy::get_default_tenant_id();
+        /** @var tool_certification_generator $certificationgenerator */
+        $certificationgenerator = self::getDataGenerator()->get_plugin_generator('tool_certification');
+        $certification1 = $certificationgenerator->generate_certification(['tenantid' => $defaulttenantid]);
+
+        $configform = ['certificationid' => $certification1->get('id')];
+        $this->assertArrayNotHasKey('certificationid', $outcome->validate_config_form($configform));
+    }
+
+    /**
+     * Test apply_to_users disable enrolment
+     */
+    public function test_apply_to_users(): void {
+        global $DB;
+
+        /** @var tool_tenant_generator $tenantgenerator */
+        $tenantgenerator = $this->get_tenant_generator();
+        $tenant = $tenantgenerator->create_tenant();
+
+        /** @var tool_certification_generator $certificationgenerator */
+        $certificationgenerator = self::getDataGenerator()->get_plugin_generator('tool_certification');
+        $certification1 = $certificationgenerator->generate_certification(['tenantid' => $tenant->id]);
+        $user1 = self::getDataGenerator()->create_user();
+        $user2 = self::getDataGenerator()->create_user();
+        $tenantgenerator->allocate_user($user1->id, $tenant->id);
+        $tenantgenerator->allocate_user($user2->id, $tenant->id);
+
+        $rule0 = $this->get_generator()->create_rule(['tenantid' => $tenant->id]);
+
+        $configdata = ['certificationid' => $certification1->get('id'), 'startdatetype' => 0, 'startdateabsolute' => 0];
+        $outcome = allocation::create($rule0->id, $configdata);
+
+        $outcome->apply_to_users([$user1, $user2]);
+
+        $users = $DB->get_records('tool_certification_users', [], '', 'userid');
+        $this->assertEquals([$user1->id, $user2->id], array_column($users, 'userid'));
+
+        // We try to allocate same users again.
+        $outcome->apply_to_users([$user1, $user2]);
+        $users = $DB->get_records('tool_certification_users', [], '', 'userid');
+        $this->assertEquals([$user1->id, $user2->id], array_column($users, 'userid'));
+        $this->assertCount(2, $users);
+    }
+
+    /**
+     * Test get_description
+     */
+    public function test_get_description(): void {
+
+        $this->resetAfterTest();
+
+        /** @var tool_certification_generator $certificationgenerator */
+        $certificationgenerator = self::getDataGenerator()->get_plugin_generator('tool_certification');
+        $certification1 = $certificationgenerator->generate_certification();
+
+        $rule1 = $this->get_generator()->create_rule();
+        $configdata = [
+            'certificationid' => $certification1->get('id'),
+            'startdatetype' => \tool_certification\constants::DATE_NONE
+        ];
+        $outcome1 = allocation::create($rule1->id, $configdata);
+
+        $this->assertEquals(get_string('outcomeallocationdescription', 'tool_certification',
+            $certification1->get('fullname')), $outcome1->get_description());
+
+        $now = time();
+        $rule2 = $this->get_generator()->create_rule();
+        $configdata = [
+            'certificationid' => $certification1->get('id'),
+            'startdatetype' => \tool_certification\constants::DATE_ABSOLUTE,
+            'startdateabsolute' => $now
+        ];
+        $outcome1 = allocation::create($rule2->id, $configdata);
+        $startdate = userdate($now, get_string('strftimedatefullshort'));
+        $strparams = ['certificationname' => $certification1->get('fullname'), 'startdate' => $startdate];
+        $expected = get_string('outcomeallocationdescriptionwithdate', 'tool_certification', $strparams);
+        $this->assertEquals($expected, $outcome1->get_description());
+    }
+
+    /**
+     * Test is_configuration_valid
+     */
+    public function test_is_configuration_valid(): void {
+        /** @var tool_tenant_generator $tenantgenerator */
+        $tenantgenerator = $this->get_tenant_generator();
+        $tenant = $tenantgenerator->create_tenant();
+
+        $certificationgenerator = self::getDataGenerator()->get_plugin_generator('tool_certification');
+        $certification = $certificationgenerator->generate_certification(['tenantid' => $tenant->id]);
+
+        // Users in program1.
+        $rule1 = $this->get_generator()->create_rule(['tenantid' => $tenant->id]);
+        $configdata = ['certificationid' => $certification->get('id')];
+        $condition = allocation::create($rule1->id, $configdata);
+
+        $this->assertTrue($condition->is_configuration_valid());
+
+        // Test certification is archived.
+        \tool_certification\api::archive_certification($certification->get('id'));
+        $this->assertFalse($condition->is_configuration_valid());
+
+        // Test certification is restored.
+        \tool_certification\api::restore_certification($certification->get('id'));
+        $this->assertTrue($condition->is_configuration_valid());
+
+        // Archive the tenant and delete the tenant.
+        $manager = new \tool_tenant\manager();
+        $manager->archive_tenant($tenant->id);
+        $manager->delete_tenant($tenant->id);
+        $this->assertFalse($condition->is_configuration_valid());
+
+        $certification = $certificationgenerator->generate_certification(['tenantid' => $tenant->id]);
+
+        // Delete certification.
+        \tool_certification\api::archive_certification($certification->get('id'));
+        $certification = new \tool_certification\certification($certification->get('id'));
+        \tool_certification\api::delete_certification($certification);
+        $this->assertFalse($condition->is_configuration_valid());
+    }
+}
