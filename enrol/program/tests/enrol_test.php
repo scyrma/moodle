4aafc5fc228d367117d0cfe24b74934e94322911
Moodle Workplace <workplace@moodle.com>
2020-05-12 08:01:54 +0000

    Moodle Workplace plugin enrol_program (build 46390)

---
+++ enrol/program/tests/enrol_test.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for enrol_program.
+ *
+ * @package   enrol_program
+ * @copyright 2018 Moodle Pty Ltd <support@moodle.com>
+ * @author    2018 David Matamoros <davidmc@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class enrol_program_enrol_testcase
+ *
+ * @copyright 2018 Moodle Pty Ltd <support@moodle.com>
+ * @author    2018 David Matamoros <davidmc@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class enrol_program_enrol_testcase extends \core_privacy\tests\provider_testcase {
+
+    /**
+     * Dummy assert to test Travis.
+     */
+    public function test_dummy() {
+        $this->resetAfterTest();
+        $var = true;
+        $this->assertEquals(true, $var);
+
+    }
+}
