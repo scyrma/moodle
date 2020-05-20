943169a3ea7d79ecaff028b04de6900025a32aa1
Marina Glancy <marina@moodle.com>
2020-05-20 16:49:44 +0200

    WP-1633 show workplace license on installation

--- admin/renderer.php
+++ admin/renderer.php
@@ -33,6 +33,11 @@ class core_admin_renderer extends plugin_renderer_base {
         global $CFG;
         $output = '';
 
+        /** @uses tool_wp\workplace::copyright_notice() */
+        if ($wp = component_class_callback('tool_wp\workplace', 'copyright_notice', [$this, $this->page->url])) {
+            return $wp;
+        }
+
         $copyrightnotice = text_to_html(get_string('gpl3'));
         $copyrightnotice = str_replace('target="_blank"', 'onclick="this.target=\'_blank\'"', $copyrightnotice); // extremely ugly validation hack
 
