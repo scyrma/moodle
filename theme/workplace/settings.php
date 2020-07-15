59bf52372a5706f7f20b6c6f36492cb68c758d10
Moodle Workplace <workplace@moodle.com>
2020-07-15 09:29:38 +0000

    Moodle Workplace plugin theme_workplace (build 50981)

---
+++ theme/workplace/settings.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package     theme_workplace
+ * @copyright   2020 Moodle Pty Ltd <support@moodle.com>
+ * @author      2020 Workplace team
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+
+    $name = 'theme_workplace/wpmenumodal';
+    $title = get_string('wpmenumodal', 'theme_workplace');
+    $description = get_string('wpmenumodaldesc', 'theme_workplace');
+    $setting = new admin_setting_configcheckbox($name, $title, $description, 0);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+}
+
+defined('MOODLE_INTERNAL') || die;
