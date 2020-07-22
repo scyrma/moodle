2eb5e9c2e218777726227ff69f1b8d51392c3b9c
Cameron Ball <cameron@moodle.com>
2020-07-22 14:29:14 +0800

    mod_attendance: commit 1f76cc463c

--- mod/attendance/locallib.php
+++ mod/attendance/locallib.php
@@ -177,12 +177,21 @@ function attendance_form_sessiondate_selector (MoodleQuickForm $mform) {
     }
 
     $sesendtime = array();
-    $sesendtime[] =& $mform->createElement('static', 'from', '', get_string('from', 'attendance'));
-    $sesendtime[] =& $mform->createElement('select', 'starthour', get_string('hour', 'form'), $hours, false, true);
-    $sesendtime[] =& $mform->createElement('select', 'startminute', get_string('minute', 'form'), $minutes, false, true);
-    $sesendtime[] =& $mform->createElement('static', 'to', '', get_string('to', 'attendance'));
-    $sesendtime[] =& $mform->createElement('select', 'endhour', get_string('hour', 'form'), $hours, false, true);
-    $sesendtime[] =& $mform->createElement('select', 'endminute', get_string('minute', 'form'), $minutes, false, true);
+    if (!right_to_left()) {
+        $sesendtime[] =& $mform->createElement('static', 'from', '', get_string('from', 'attendance'));
+        $sesendtime[] =& $mform->createElement('select', 'starthour', get_string('hour', 'form'), $hours, false, true);
+        $sesendtime[] =& $mform->createElement('select', 'startminute', get_string('minute', 'form'), $minutes, false, true);
+        $sesendtime[] =& $mform->createElement('static', 'to', '', get_string('to', 'attendance'));
+        $sesendtime[] =& $mform->createElement('select', 'endhour', get_string('hour', 'form'), $hours, false, true);
+        $sesendtime[] =& $mform->createElement('select', 'endminute', get_string('minute', 'form'), $minutes, false, true);
+    } else {
+        $sesendtime[] =& $mform->createElement('static', 'from', '', get_string('from', 'attendance'));
+        $sesendtime[] =& $mform->createElement('select', 'startminute', get_string('minute', 'form'), $minutes, false, true);
+        $sesendtime[] =& $mform->createElement('select', 'starthour', get_string('hour', 'form'), $hours, false, true);
+        $sesendtime[] =& $mform->createElement('static', 'to', '', get_string('to', 'attendance'));
+        $sesendtime[] =& $mform->createElement('select', 'endminute', get_string('minute', 'form'), $minutes, false, true);
+        $sesendtime[] =& $mform->createElement('select', 'endhour', get_string('hour', 'form'), $hours, false, true);
+    }
     $mform->addGroup($sesendtime, 'sestime', get_string('time', 'attendance'), array(' '), true);
 }
 
@@ -458,7 +467,7 @@ function attendance_can_student_mark($sess, $log = true) {
             $record = $DB->get_record_select('attendance_log', $sql, $params);
         } else {
             // Assume ATTENDANCE_SHAREDIP_FORCED.
-            $sql = 'sessionid = ? AND studentid <> ? ipaddress = ?';
+            $sql = 'sessionid = ? AND studentid <> ? AND ipaddress = ?';
             $params = array($sess->id, $USER->id, getremoteaddr());
             $record = $DB->get_record_select('attendance_log', $sql, $params);
         }
@@ -509,7 +518,7 @@ function attendance_exporttotableed($data, $filename, $format) {
     // Sending HTTP headers.
     $workbook->send($filename);
     // Creating the first worksheet.
-    $myxls = $workbook->add_worksheet('Attendances');
+    $myxls = $workbook->add_worksheet(get_string('modulenameplural', 'attendance'));
     // Format types.
     $formatbc = $workbook->add_format();
     $formatbc->set_bold(1);
@@ -634,18 +643,29 @@ function attendance_construct_sessions_data_for_add($formdata, mod_attendance_st
                     $sess->absenteereport = $absenteereport;
                     $sess->studentpassword = '';
                     $sess->includeqrcode = 0;
+                    $sess->rotateqrcode = 0;
+                    $sess->rotateqrcodesecret = '';
+
+                    if (!empty($formdata->usedefaultsubnet)) {
+                        $sess->subnet = $att->subnet;
+                    } else {
+                        $sess->subnet = $formdata->subnet;
+                    }
+                    $sess->automark = $formdata->automark;
+                    $sess->automarkcompleted = 0;
+                    if (!empty($formdata->preventsharedip)) {
+                        $sess->preventsharedip = $formdata->preventsharedip;
+                    }
+                    if (!empty($formdata->preventsharediptime)) {
+                        $sess->preventsharediptime = $formdata->preventsharediptime;
+                    }
+
                     if (isset($formdata->studentscanmark)) { // Students will be able to mark their own attendance.
                         $sess->studentscanmark = 1;
-                        if (!empty($formdata->usedefaultsubnet)) {
-                            $sess->subnet = $att->subnet;
-                        } else {
-                            $sess->subnet = $formdata->subnet;
-                        }
-                        $sess->automark = $formdata->automark;
                         if (isset($formdata->autoassignstatus)) {
                             $sess->autoassignstatus = 1;
                         }
-                        $sess->automarkcompleted = 0;
+
                         if (!empty($formdata->randompassword)) {
                             $sess->studentpassword = attendance_random_string();
                         } else if (!empty($formdata->studentpassword)) {
@@ -653,6 +673,11 @@ function attendance_construct_sessions_data_for_add($formdata, mod_attendance_st
                         }
                         if (!empty($formdata->includeqrcode)) {
                             $sess->includeqrcode = $formdata->includeqrcode;
+                        }
+                        if (!empty($formdata->rotateqrcode)) {
+                            $sess->rotateqrcode = $formdata->rotateqrcode;
+                            $sess->studentpassword = attendance_random_string();
+                            $sess->rotateqrcodesecret = attendance_random_string();
                         }
                         if (!empty($formdata->preventsharedip)) {
                             $sess->preventsharedip = $formdata->preventsharedip;
@@ -694,6 +719,24 @@ function attendance_construct_sessions_data_for_add($formdata, mod_attendance_st
         $sess->automarkcompleted = 0;
         $sess->absenteereport = $absenteereport;
         $sess->includeqrcode = 0;
+        $sess->rotateqrcode = 0;
+        $sess->rotateqrcodesecret = '';
+
+        if (!empty($formdata->usedefaultsubnet)) {
+            $sess->subnet = $att->subnet;
+        } else {
+            $sess->subnet = $formdata->subnet;
+        }
+
+        if (!empty($formdata->automark)) {
+            $sess->automark = $formdata->automark;
+        }
+        if (!empty($formdata->preventsharedip)) {
+            $sess->preventsharedip = $formdata->preventsharedip;
+        }
+        if (!empty($formdata->preventsharediptime)) {
+            $sess->preventsharediptime = $formdata->preventsharediptime;
+        }
 
         if (isset($formdata->studentscanmark) && !empty($formdata->studentscanmark)) {
             // Students will be able to mark their own attendance.
@@ -708,6 +751,11 @@ function attendance_construct_sessions_data_for_add($formdata, mod_attendance_st
             }
             if (!empty($formdata->includeqrcode)) {
                 $sess->includeqrcode = $formdata->includeqrcode;
+            }
+            if (!empty($formdata->rotateqrcode)) {
+                $sess->rotateqrcode = $formdata->rotateqrcode;
+                $sess->studentpassword = attendance_random_string();
+                $sess->rotateqrcodesecret = attendance_random_string();
             }
             if (!empty($formdata->usedefaultsubnet)) {
                 $sess->subnet = $att->subnet;
@@ -1044,4 +1092,99 @@ function construct_session_full_date_time($datetime, $duration) {
     $sessinfo .= ' '.attendance_construct_session_time($datetime, $duration);
 
     return $sessinfo;
+}
+
+/**
+ * Render the session password.
+ *
+ * @param stdClass $session
+ */
+function attendance_renderpassword($session) {
+    echo html_writer::tag('h2', get_string('passwordgrp', 'attendance'));
+    echo html_writer::span($session->studentpassword, 'student-password');
+}
+
+/**
+ * Render the session QR code.
+ *
+ * @param stdClass $session
+ */
+function attendance_renderqrcode($session) {
+    global $CFG;
+
+    if (strlen($session->studentpassword) > 0) {
+        $qrcodeurl = $CFG->wwwroot . '/mod/attendance/attendance.php?qrpass=' .
+            $session->studentpassword . '&sessid=' . $session->id;
+    } else {
+        $qrcodeurl = $CFG->wwwroot . '/mod/attendance/attendance.php?sessid=' . $session->id;
+    }
+
+    echo html_writer::tag('h3', get_string('qrcode', 'attendance'));
+
+    $barcode = new TCPDF2DBarcode($qrcodeurl, 'QRCODE');
+    $image = $barcode->getBarcodePngData(15, 15);
+    echo html_writer::img('data:image/png;base64,' . base64_encode($image), get_string('qrcode', 'attendance'));
+}
+
+/**
+ * Generate QR code passwords.
+ *
+ * @param stdClass $session
+ */
+function attendance_generate_passwords($session) {
+    global $DB;
+    $attconfig = get_config('attendance');
+    $password = array();
+
+    for ($i = 0; $i < 30; $i++) {
+        array_push($password, array("attendanceid" => $session->id,
+            "password" => mt_rand(1000, 10000), "expirytime" => time() + ($attconfig->rotateqrcodeinterval * $i)));
+    }
+
+    $DB->insert_records('attendance_rotate_passwords', $password);
+}
+
+/**
+ * Render JS for rotate QR code passwords.
+ *
+ * @param stdClass $session
+ */
+function attendance_renderqrcoderotate($session) {
+    // Load required js.
+    echo html_writer::tag('script', '',
+        [
+            'src' => 'js/qrcode/qrcode.min.js',
+            'type' => 'text/javascript'
+        ]
+    );
+    echo html_writer::tag('script', '',
+        [
+            'src' => 'js/password/attendance_QRCodeRotate.js',
+            'type' => 'text/javascript'
+        ]
+    );
+    echo html_writer::tag('div', '', ['id' => 'rotate-time']); // Div to display timer.
+    echo html_writer::tag('h3', get_string('passwordgrp', 'attendance'));
+    echo html_writer::tag('div', '', ['id' => 'text-password']); // Div to display password.
+    echo html_writer::tag('h3', get_string('qrcode', 'attendance'));
+    echo html_writer::tag('div', '', ['id' => 'qrcode']); // Div to display qr code.
+    // Js to start the password manager.
+    echo '
+    <script type="text/javascript">
+        let qrCodeRotate = new attendance_QRCodeRotate();
+        qrCodeRotate.start(' . $session->id . ', document.getElementById("qrcode"), document.getElementById("text-password"),
+        document.getElementById("rotate-time"));
+    </script>';
+}
+
+/**
+ * Return QR code passwords.
+ *
+ * @param stdClass $session
+ */
+function attendance_return_passwords($session) {
+    global $DB;
+
+    $sql = 'SELECT * FROM {attendance_rotate_passwords} WHERE attendanceid = ? AND expirytime > ? ORDER BY expirytime ASC';
+    return json_encode($DB->get_records_sql($sql, ['attendanceid' => $session->id, time()], $strictness = IGNORE_MISSING));
 }
