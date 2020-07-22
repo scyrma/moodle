77118dbf5df0ebbc0780a5c1c19cb26455e723ef
Cameron Ball <cameron@moodle.com>
2020-07-22 14:26:05 +0800

    mod_checklist: commit 06aead1a3e

--- mod/checklist/renderer.php
+++ mod/checklist/renderer.php
@@ -182,8 +182,8 @@ class mod_checklist_renderer extends plugin_renderer_base {
                     continue;
                 }
 
-                if ($status->is_checkgroupings() && $item->grouping) {
-                    if (!in_array($item->grouping, $groupings)) {
+                if ($status->is_checkgroupings() && $item->groupingid) {
+                    if (!in_array($item->groupingid, $groupings)) {
                         continue; // Current user is not a member of this item's grouping, so skip.
                     }
                 }
@@ -229,11 +229,11 @@ class mod_checklist_renderer extends plugin_renderer_base {
 
                 $checkclass = '';
                 if ($item->is_heading()) {
-                    $optional = ' class="itemheading '.$itemcolour.'" ';
+                    $optional = ' class="itemheading '.$itemcolour.' ml-1" ';
                 } else if ($item->is_required()) {
-                    $optional = ' class="'.$itemcolour.'" ';
+                    $optional = ' class="'.$itemcolour.' ml-1" ';
                 } else {
-                    $optional = ' class="itemoptional '.$itemcolour.'" ';
+                    $optional = ' class="itemoptional '.$itemcolour.' ml-1" ';
                     $checkclass = ' itemoptional';
                 }
 
@@ -491,6 +491,7 @@ class mod_checklist_renderer extends plugin_renderer_base {
     protected function checklist_item_link(checklist_item $item) {
         $out = '';
         if ($url = $item->get_link_url()) {
+            $attrs = [];
             $out .= '&nbsp;';
             switch ($item->get_link_type()) {
                 case checklist_item::LINK_MODULE:
@@ -501,9 +502,12 @@ class mod_checklist_renderer extends plugin_renderer_base {
                     break;
                 case checklist_item::LINK_URL:
                     $icon = $this->output->pix_icon('follow_link', get_string('linktourl', 'checklist'), 'mod_checklist');
+                    if ($item->openlinkinnewwindow) {
+                        $attrs['target'] = '_blank';
+                    }
                     break;
             }
-            $out .= html_writer::link($url, $icon);
+            $out .= html_writer::link($url, $icon, $attrs);
         }
         return $out;
     }
@@ -870,7 +874,7 @@ class mod_checklist_renderer extends plugin_renderer_base {
 </script>
 ENDSCRIPT;
 
-        return $out;
+        return html_writer::span($out, 'checklistformitem');
     }
 
     /**
@@ -901,7 +905,20 @@ ENDSCRIPT;
         ];
         $out .= html_writer::empty_tag('input', $attr);
 
-        return $out;
+        $attr = [
+            'type' => 'checkbox',
+            'class' => 'form-control',
+            'id' => 'id_openlinkinnewwindow',
+        ];
+        $out .= html_writer::checkbox(
+            'openlinkinnewwindow',
+            1,
+            $item ? (bool) $item->openlinkinnewwindow : false,
+            get_string('openlinkinnewwindow', 'mod_checklist'),
+            $attr
+        );
+
+        return html_writer::span($out, 'checklistformitem');
     }
 
     /**
@@ -916,12 +933,12 @@ ENDSCRIPT;
 
         $out .= '<br>';
         $out .= html_writer::label(get_string('grouping', 'mod_checklist'), 'id_grouping').' ';
-        $selected = $item ? $item->grouping : null;
+        $selected = $item ? $item->groupingid : null;
         $groupings = checklist_class::get_course_groupings($status->get_courseid());
-        $out .= html_writer::select($groupings, 'grouping', $selected, [0 => get_string('anygrouping', 'mod_checklist')],
+        $out .= html_writer::select($groupings, 'groupingid', $selected, [0 => get_string('anygrouping', 'mod_checklist')],
                                     ['id' => 'id_grouping']);
 
-        return $out;
+        return html_writer::span($out, 'checklistformitem');
     }
 
     /**
