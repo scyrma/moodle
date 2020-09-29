4f1c0fe5f7a7fe8fbeff4cfac4b69cd3064f6488
Cameron Ball <cameron@moodle.com>
2020-09-29 15:10:13 +0800

    MC-2622: Disable core's scheduled task for H5P

--- lib/classes/task/h5p_get_content_types_task.php
+++ lib/classes/task/h5p_get_content_types_task.php
@@ -59,6 +59,9 @@ class h5p_get_content_types_task extends scheduled_task {
      * Execute the task.
      */
     public function execute() {
+        // BEGIN MOODLECLOUD HACK
+        return;
+        // END MOODLECLOUD HACK
         // MDL-68579, avoid execute the task through behat tests.
         if (defined('BEHAT_SITE_RUNNING')) {
             return true;
