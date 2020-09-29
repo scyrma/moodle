1a847605ea87cea56af924faceb653ebe62dc5a8
Cameron Ball <cameron@moodle.com>
2020-09-29 18:44:59 +0800

    MC-2453: Refine query for ignoring H5P files

--- admin/tool/fileslist/classes/container.php
+++ admin/tool/fileslist/classes/container.php
@@ -71,9 +71,11 @@ final class container {
                     "filearea <> 'draft' AND " .
                     "component <> 'tool_recyclebin' AND " .
                     "(component <> 'backup' OR mimetype <> 'application/vnd.moodle.backup') AND " .
-                    "(component <> 'core_h5p' OR filearea <> 'libraries') AND " .
-                    "(component <> 'core_h5p' OR filearea <> 'export') AND " .
-                    "component <> 'assignfeedback_editpdf' ORDER BY filesize DESC LIMIT :limit OFFSET :offset",
+                    "component <> 'assignfeedback_editpdf' AND " .
+                    "component <> 'core_h5p' AND " .
+                    "component <> 'contentbank' " .
+                    "UNION SELECT * from {files} WHERE (component = 'core_h5p' AND filearea = 'content' AND filesize > 0) " .
+                    "ORDER BY filesize DESC LIMIT :limit OFFSET :offset",
                     [
                         'limit' => $limit,
                         'offset' => $offset,
