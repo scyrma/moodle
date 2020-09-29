8e248bd0d562fd57a598116ff2d211c961d05644
Cameron Ball <cameron@moodle.com>
2020-09-29 17:18:21 +0800

    MC-2622: Don't run the H5P task on template sites

--- local/moodlecloud/classes/task/h5p_get_content_types_task.php
+++ local/moodlecloud/classes/task/h5p_get_content_types_task.php
@@ -41,7 +41,11 @@ class h5p_get_content_types_task extends \core\task\scheduled_task {
     }
 
     public function execute() {
-        global $CFG, $DB;
+        global $CFG, $DB, $dynamicsite;
+
+        if (substr($dynamicsite, 0, 8) == 'template') {
+            return;
+        }
 
         autoloader::register();
 
