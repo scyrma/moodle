b1663327a39166f9d8238633d2193f647df0fc1c
Moodle Workplace <workplace@moodle.com>
2020-12-01 12:07:16 +0000

    Moodle Workplace plugin tool_program (build 60492)

---
+++ admin/tool/program/classes/output/tab/program_users_tab.php
@@ -0,0 +1,181 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace Code is dual-licensed under the terms of both the
+// single GNU General Public Licence version 3.0, dated 29 June 2007
+// and the terms of the proprietary Moodle Workplace Licence strictly
+// controlled by Moodle Pty Ltd and its certified premium partners.
+// Wherever conflicting terms exist, the terms of the MWL are binding
+// and shall prevail.
+
+/**
+ * File for class program_users_tab.
+ *
+ * @package   tool_program
+ * @copyright 2018 Moodle Pty Ltd <support@moodle.com>
+ * @author    2018 Mitxel Moriana <mitxel@tresipunt.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace tool_program\output\tab;
+
+use html_writer;
+use renderer_base;
+use stdClass;
+use tool_program\api;
+use tool_program\constants;
+use tool_program\permission;
+use tool_program\persistent\program;
+use tool_reportbuilder\system_report_factory;
+use tool_program\local\reports\allocations_report;
+use tool_wp\output\content_with_heading;
+use tool_wp\output\tab;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class program_users_tab.
+ *
+ * @package   tool_program
+ * @copyright 2018 Moodle Pty Ltd <support@moodle.com>
+ * @author    2018 Mitxel Moriana <mitxel@tresipunt.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class program_users_tab extends tab {
+
+    /** @var program current program */
+    protected $program = null;
+
+    /**
+     * Function to export the renderer data in a format that is suitable for a
+     * mustache template. This means:
+     * 1. No complex types - only stdClass, array, int, string, float, bool
+     * 2. Any additional info that is required for the template is pre-calculated (e.g. capability checks).
+     *
+     * @param renderer_base $output Used to do a final render of any components that need to be rendered for export.
+     * @return stdClass|array
+     */
+    public function export_for_template(\renderer_base $output) {
+
+        // Check if tool_reportbuilder is installed.
+        if (class_exists('\\tool_reportbuilder\\system_report_factory')) {
+            // Users list.
+            $report = system_report_factory::create(allocations_report::class, ['id' => $this->data['id']]);
+            $userstable = $report->output();
+        } else {
+            $str = get_string('reportbuilderuserlist', 'tool_program');
+            $userstable = html_writer::tag('div', $str, ['class' => 'alert alert-warning']);
+        }
+        $content = new content_with_heading($userstable, $this->get_tab_label());
+
+        // We check if we have permission to show allocation button.
+        $canshowbutton = permission::can_allocate_anybody($this->get_program(), false, false);
+        if ($canshowbutton) {
+            $addbuttontitle = get_string('allocateusers', 'tool_program');
+            $params = [];
+            $program = $this->get_program();
+            if ((int)$program->get('allowdirectallocation') === 0 || !api::is_allocation_window_open($program)) {
+                // Get strings and date for allocation window closed modal.
+                $params = $this->get_data_for_modal($program);
+            }
+            $content->add_button($addbuttontitle, null, $params, false);
+        }
+        return $content->export_for_template($output);
+    }
+
+    /**
+     * The label to be displayed on the tab
+     *
+     * @return string
+     */
+    public function get_tab_label(): string {
+        return get_string('users', 'tool_program');
+    }
+
+    /**
+     * Current program
+     *
+     * @return program
+     */
+    protected function get_program() : program {
+        if (!$this->program) {
+            $this->program = new program(!empty($this->data['id']) ? $this->data['id'] : 0);
+        }
+        return $this->program;
+    }
+
+    /**
+     * Check permission of the current user to access this tab
+     *
+     * @return bool
+     */
+    public function is_available(): bool {
+        return permission::can_view_allocated_users($this->get_program());
+    }
+
+    /**
+     * Template to use to display tab contents
+     *
+     * @return string
+     */
+    public function get_template(): string {
+        return 'tool_program/edit_program_user_allocation';
+    }
+
+    /**
+     * Returns correct string and calculated date for allocation window closed modal in user allocation list.
+     *
+     * @param program $program
+     * @return mixed
+     * @throws \coding_exception
+     */
+    private function get_data_for_modal(program $program) {
+        // Check if direct allocation to the program is disabled.
+        if ((int)$program->get('allowdirectallocation') === 0) {
+            $params['data-directallocationdisabled'] = 1;
+            return $params;
+        }
+
+        // Check if allocation window to the program is active.
+        $params['data-allocationwindow'] = 'closed';
+
+        if ((int)$program->get('allocationstartdatetype') === constants::DATE_ABSOLUTE &&
+            $program->get('allocationstartdateabsolute') > time()) {
+            $params['data-allocationwindowtype'] = 'allocationwindowstartson';
+            $params['data-allocationwindowtime'] = userdate($program->get('allocationstartdateabsolute'),
+                get_string('strftimedatefullshort'));
+
+        } else if ((int)$program->get('allocationenddatetype') === constants::DATE_ABSOLUTE &&
+            $program->get('allocationenddateabsolute') < time()) {
+            $params['data-allocationwindowtype'] = 'allocationwindowendedon';
+            $params['data-allocationwindowtime'] = userdate($program->get('allocationenddateabsolute'),
+                get_string('strftimedatefullshort'));
+
+        } else if ((int)$program->get('allocationenddatetype') === constants::DATE_AFTER_ALLOCATION_STARTS &&
+            (int)$program->get('allocationstartdatetype') === constants::DATE_ABSOLUTE ) {
+            $params['data-allocationwindowtype'] = 'allocationwindowendedon';
+            // Calculate absolute date from the relative one.
+            $startdate = (int)$program->get('allocationstartdateabsolute');
+            $enddaterelative = (int)$program->get('allocationenddaterelative');
+            $enddate = strtotime('+' . $enddaterelative, $startdate);
+            $params['data-allocationwindowtime'] = userdate($enddate, get_string('strftimedatefullshort'));
+        }
+
+        return $params;
+    }
+}
---
+++ admin/tool/program/classes/task/deallocate_from_previous_tenant_programs.php
@@ -0,0 +1,105 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace Code is dual-licensed under the terms of both the
+// single GNU General Public Licence version 3.0, dated 29 June 2007
+// and the terms of the proprietary Moodle Workplace Licence strictly
+// controlled by Moodle Pty Ltd and its certified premium partners.
+// Wherever conflicting terms exist, the terms of the MWL are binding
+// and shall prevail.
+
+/**
+ * Program ad-hoc task for de-allocating users from previous tenant programs.
+ *
+ * This is triggered by upgrade script only. Until now when user was moved between tenants
+ * we were keeping user allocation to previous programs in previous tenants.
+ * Now user needs to be deallocated from all programs in those previous tenants.
+ * See WP-1015 for details.
+ *
+ * @package     tool_program
+ * @copyright   2020 Moodle Pty Ltd <support@moodle.com>
+ * @author      2020 David Matamoros <davidmc@moodle.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace tool_program\task;
+
+use core\task\adhoc_task;
+use tool_program\api;
+use tool_tenant\tenancy;
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Ad-hoc task deallocate_from_previous_tenant_programs
+ *
+ * @package     tool_program
+ * @copyright   2020 Moodle Pty Ltd <support@moodle.com>
+ * @author      2020 David Matamoros <davidmc@moodle.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class deallocate_from_previous_tenant_programs extends adhoc_task {
+
+    /**
+     * Execute the task
+     *
+     * @return void
+     */
+    public function execute() {
+        global $DB, $CFG;
+        require_once($CFG->dirroot . '/group/lib.php');
+
+        if (!tenancy::is_site_multi_tenant()) {
+            return;
+        }
+
+        // Deallocate users from programs that do not belong to the users current tenant.
+        $defaulttenantid = \tool_tenant\tenancy::get_default_tenant_id();
+        $sql = "
+            SELECT tpu.*
+            FROM {tool_program_users} tpu
+            JOIN {tool_program} tp ON tpu.programid = tp.id
+            LEFT JOIN {tool_tenant_user} ttu ON ttu.userid = tpu.userid
+            WHERE tpu.certificationid = 0 AND tp.tenantid <> COALESCE(ttu.tenantid, $defaulttenantid)
+        ";
+        $allocations = $DB->get_records_sql($sql);
+
+        foreach ($allocations as $allocation) {
+            api::deallocate_user($allocation->programid, $allocation->userid);
+        }
+
+        // Remove users from all component-less tenant groups that do not belong to their current tenant,
+        // regardless of any program allocations.
+        $defaulttenantid = \tool_tenant\tenancy::get_default_tenant_id();
+        $sql = "
+            SELECT gm.id, gm.groupid, gm.userid
+              FROM {groups_members} gm
+              JOIN {tool_tenant_group} ttg ON ttg.groupid = gm.groupid
+              JOIN {groups} g ON g.courseid = ttg.courseid AND gm.groupid = g.id
+         LEFT JOIN {tool_tenant_user} ttu ON ttu.userid = gm.userid
+             WHERE ttg.component IS NULL AND ttg.area IS NULL AND ttg.itemid IS NULL
+               AND ttg.tenantid <> COALESCE(ttu.tenantid, $defaulttenantid)
+               AND gm.component = 'enrol_program'
+        ";
+        $params = [];
+        $records = $DB->get_records_sql($sql, $params);
+        foreach ($records as $record) {
+            groups_remove_member($record->groupid, $record->userid);
+        }
+    }
+}
