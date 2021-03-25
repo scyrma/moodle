cd63b0efe6f2f253bb7479118cc91153a3b94584
Moodle Workplace <workplace@moodle.com>
2021-03-25 15:42:19 +0000

    Moodle Workplace plugin tool_dynamicrule (build 66594)

---
+++ admin/tool/dynamicrule/classes/tool_dynamicrule/outcome/badge.php
@@ -0,0 +1,308 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace Code is dual-licensed under the terms of both the
+// single GNU General Public Licence version 3.0, dated 29 June 2007
+// and the terms of the proprietary Moodle Workplace Licence strictly
+// controlled by Moodle Pty Ltd and its certified premium partners.
+// Wherever conflicting terms exist, the terms of the MWL are binding
+// and shall prevail.
+
+/**
+ * This file contains the backend class for badge outcome.
+ *
+ * @package    tool_dynamicrule
+ * @copyright  2019 Moodle Pty Ltd <support@moodle.com>
+ * @author     2019 Marina Glancy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace tool_dynamicrule\tool_dynamicrule\outcome;
+
+use tool_wp\exporter_base;
+use tool_wp\importer_base;
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once($CFG->libdir . '/badgeslib.php');
+
+/**
+ * The backend class for notification outcome
+ *
+ * @package    tool_dynamicrule
+ * @copyright  2019 Moodle Pty Ltd <support@moodle.com>
+ * @author     2019 Marina Glancy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class badge extends \tool_dynamicrule\outcome_base {
+    /** @var \core_badges\badge */
+    private $badge;
+
+    /**
+     * Returns the title of the outcome
+     *
+     * @return string The title as formated string
+     */
+    public function get_title(): string {
+        return get_string('outcomebadge', 'tool_dynamicrule');
+    }
+
+    /**
+     * Adds outcome's elements to the given mform
+     *
+     * @param \MoodleQuickForm $mform The form to add elements to
+     */
+    public function get_config_form(\MoodleQuickForm $mform) {
+        // Badges selector.
+        $options = [
+            'ajax' => 'tool_dynamicrule/form_badge_selector',
+            'multiple' => false,
+            'class' => 'select_badge',
+        ];
+        $selected = $this->get_selected();
+        $mform->addElement('autocomplete', 'badge', get_string('selectbadge', 'tool_dynamicrule'), $selected, $options);
+        $mform->addHelpButton('badge', 'selectbadge', 'tool_dynamicrule');
+        $mform->addRule('badge', get_string('required'), 'required', null, 'client');
+
+        // Manage badges link.
+        $manageprogurl = new \moodle_url('/badges/index.php?type=1');
+        $manageprogramsstr = get_string('managebadges', 'tool_dynamicrule');
+        $html = \html_writer::tag('a', $manageprogramsstr, ['href' => $manageprogurl, 'target' => '_blank']);
+        $mform->addElement('static', 'managebadge', '', $html);
+    }
+
+    /**
+     * Validates the configform of the outcome
+     *
+     * @param array $data Data from the form
+     * @return array Array with errors for each element
+     */
+    public function validate_config_form(array $data): array {
+        global $DB, $USER;
+        $errors = [];
+
+        $badge = new \core_badges\badge($data['badge']);
+        // Badge must be of type site, active and has manual issue criteria defined.
+        if ($badge->type != BADGE_TYPE_SITE || !$badge->is_active() || !$badge->has_manual_award_criteria()) {
+            $errors['badge'] = get_string('errorinvalidbadge', 'tool_dynamicrule');
+            return $errors;
+        }
+
+        // Badge must have only manual award criteria, or use ANY aggregation method in overall criteria.
+        // This may not be required if we use API method badges_award_handle_manual_criteria_review for issuing,
+        // but since we issue directly, we must ensure there are no other criteria requirements in place.
+        if (count($badge->criteria) > 2 && $badge->get_aggregation_method() != BADGE_CRITERIA_AGGREGATION_ANY) {
+            $errors['badge'] = get_string('errorbadgehasextracriteria', 'tool_dynamicrule');
+            return $errors;
+        }
+
+        // Check manual issue criteria permissions.
+        if (!is_siteadmin()) {
+            $acceptedroles = array_keys($badge->criteria[BADGE_CRITERIA_TYPE_MANUAL]->params);
+            $roles = get_user_roles(\context_system::instance(), $USER->id);
+            $roleids = array_column($roles, 'roleid');
+            if (empty(array_intersect($acceptedroles, $roleids))) {
+                $errors['badge'] = get_string('errorbadgenopermission', 'tool_dynamicrule');
+            }
+        }
+
+        return $errors;
+    }
+
+    /**
+     * Helper function called before outcome is applied to user.
+     */
+    public function setup_for_applying(): void {
+        $this->badge = new \core_badges\badge($this->get_badgeid());
+    }
+
+    /**
+     * Apply this outcome to a given user
+     *
+     * @param \stdClass $user The user object to apply the outcome to
+     */
+    public function apply_to_user(\stdClass $user): void {
+        // Check if user has this badge already. In reality we need to trigger
+        // badges_award_handle_manual_criteria_review, but that is not possible
+        // since existing badge outcomes may have different criteria.
+        // 'nobake' param is only used in unittest, there is no such form setting.
+        if (!$this->badge->is_issued($user->id)) {
+            $this->badge->issue($user->id, !empty($this->get_configdata()['nobake']));
+        }
+    }
+
+    /**
+     * Return the description for the outcome.
+     *
+     * @return string
+     */
+    public function get_description(): string {
+        return get_string('outcomebadgedescription', 'tool_dynamicrule', $this->get_badge_name());
+    }
+
+    /**
+     * Return subject formatted.
+     *
+     * @return string
+     */
+    private function get_badge_name(): string {
+        global $DB;
+        $badgename = $DB->get_field('badge', 'name', ['id' => $this->get_badgeid()]);
+        return format_string($badgename, true, ['context' => \context_system::instance(), 'escape' => false]);
+    }
+
+    /**
+     * Return configured badge id.
+     *
+     * @return null|int
+     */
+    public function get_badgeid() : ?int {
+        if (isset($this->get_configdata()['badge'])) {
+            return $this->get_configdata()['badge'];
+        }
+        return null;
+    }
+
+    /**
+     * Check if badge is not empty.
+     *
+     * @return bool
+     */
+    public function is_configuration_valid(): bool {
+        global $DB;
+
+        if (!$badge = $DB->get_record('badge', ['id' => $this->get_badgeid()])) {
+            // Badge does not exist.
+            return false;
+        }
+
+        // Ensure that the badge is active. For backward compatibility with existing badges,
+        // we don't do check site type and manual issue criteria presence here.
+        $badge = new \core_badges\badge($this->get_badgeid());
+        if (!$badge->is_active()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * If the current user is able to add this outcome.
+     *
+     * @return bool
+     */
+    public function user_can_add(): bool {
+        return has_capability('moodle/badges:awardbadge', \context_system::instance());
+    }
+
+
+    /**
+     * If the current user is able to edit this outcome.
+     *
+     * @param array $configdata
+     * @return bool
+     */
+    public function user_can_edit(array $configdata): bool {
+        $badge = new \core_badges\badge($configdata['badge']);
+        return has_capability('moodle/badges:awardbadge', $badge->get_context());
+    }
+
+    /**
+     * Return id and name of selected badges.
+     *
+     * @return array
+     */
+    private function get_selected(): array {
+        if ($this->get_badgeid()) {
+            $selected = [$this->get_badgeid() => $this->get_badge_name()];
+        } else {
+            $selected = [];
+        }
+        return $selected;
+    }
+
+    /**
+     * Add badge outcome field mapping during export
+     *
+     * @param exporter_base $exporter
+     */
+    public function add_exporter_mapping(exporter_base $exporter): void {
+        $exporter->add_mapping('badge', $this->get_badgeid());
+    }
+
+    /**
+     * Get badge outcome field mapping during import
+     *
+     * @param importer_base $importer
+     */
+    public function get_importer_mapping(importer_base $importer): void {
+        $configdata = $this->get_configdata();
+        $configdata['badge'] = $importer->get_mapping('badge', $this->get_badgeid(), IGNORE_MISSING) ?? 0;
+
+        $this->update_configdata($configdata);
+    }
+
+    /**
+     * If the current user is able to use this outcome.
+     *
+     * Return true if there are site badges with no criteria and not archived.
+     *
+     * @return bool
+     */
+    public static function is_available(): bool {
+        global $DB;
+
+        // We select badges of type site, active and with manual issue criteria.
+        $query = 'SELECT COUNT(1)
+                  FROM {badge} b
+                  JOIN {badge_criteria} bc
+                  ON b.id = bc.badgeid
+                  WHERE (b.status = :badgestatusactive OR b.status = :badgestatusactivelocked)
+                  AND b.type = :badgetype AND bc.criteriatype = :badgecriteriatype';
+
+        $params = [
+            'badgestatusactive' => BADGE_STATUS_ACTIVE,
+            'badgestatusactivelocked' => BADGE_STATUS_ACTIVE_LOCKED,
+            'badgetype' => BADGE_TYPE_SITE,
+            'badgecriteriatype' => BADGE_CRITERIA_TYPE_MANUAL,
+        ];
+
+        return ($DB->count_records_sql($query, $params) > 0);
+    }
+
+    /**
+     * Outcome not available label.
+     *
+     * @return string
+     */
+    public function get_not_available_label(): string {
+        return get_string('noavailablebadges', 'tool_dynamicrule');
+    }
+
+    /**
+     * Outcome broken label.
+     *
+     * Outcomes may provide more detailed information on what is broken when
+     * is_configuration_valid returns false.
+     *
+     * @return string
+     */
+    public function get_broken_description(): string {
+        return get_string('errorinvalidbadge', 'tool_dynamicrule');
+    }
+}
---
+++ admin/tool/dynamicrule/tests/behat/conditions_cohort_member.feature
@@ -0,0 +1,155 @@
+@tool @tool_dynamicrule @javascript @moodleworkplace
+Feature: Dynamicrule conditions cohort_member and cohort_not_member work as expected
+  In order to test the dynamicrule conditions cohort_member and cohort_not_member
+  As a privileged user
+  I need to create a rule with cohort member condition and validate it works as expected
+
+  Background:
+    Given "2" tenants exist with "2" users and "2" courses in each
+    And the following "categories" exist:
+      | name    | category  | idnumber |
+      | Subcat1 | CAT1      | SCAT1    |
+      | Subcat2 | CAT2      | SCAT2    |
+    And the following dynamic rules exist:
+      | name  | tenant  |
+      | Rule0 | Default tenant |
+      | Rule1 | Tenant1 |
+      | Rule2 | Tenant2 |
+
+  Scenario: Check availability of cohort member conditions as admin.
+    When I log in as "admin"
+    And I navigate to "Dynamic rules" in workplace launcher
+    And I follow "Edit rule 'Rule0'"
+    Then "No available cohorts" "icon" should exist
+    # Add system cohort
+    And the following "cohorts" exist:
+      | name                 | idnumber | contextlevel | reference |
+      | System cohort 1      | CHS1     | System       |           |
+    And I navigate to "Dynamic rules" in workplace launcher
+    And I follow "Edit rule 'Rule0'"
+    And "No available cohorts" "icon" should not exist
+    And I should see "User is member of cohort"
+    And I should see "User is not member of cohort"
+
+  Scenario: Check availability of cohort member conditions as tenant.
+    When I log in as "tenantadmin1"
+    And I navigate to "Dynamic rules" in workplace launcher
+    And I follow "Edit rule 'Rule1'"
+    Then "No available cohorts" "icon" should exist
+    # Add system cohort, this should have no effect.
+    And the following "cohorts" exist:
+      | name                 | idnumber | contextlevel | reference |
+      | System cohort 1      | CHS1     | System       |           |
+    And I navigate to "Dynamic rules" in workplace launcher
+    And I follow "Edit rule 'Rule1'"
+    And "No available cohorts" "icon" should exist
+    # Add another tenant cohort, this should have no effect.
+    And the following "cohorts" exist:
+      | name                 | idnumber | contextlevel | reference |
+      | Cohort in Cat 2      | CHCAT2   | Category     | CAT2      |
+    And I navigate to "Dynamic rules" in workplace launcher
+    And I follow "Edit rule 'Rule1'"
+    And "No available cohorts" "icon" should exist
+    # Finally add this tenant cohort.
+    And the following "cohorts" exist:
+      | name                 | idnumber | contextlevel | reference |
+      | Cohort in Cat 1      | CHCAT1   | Category     | CAT1      |
+    And I navigate to "Dynamic rules" in workplace launcher
+    And I follow "Edit rule 'Rule1'"
+    And "No available cohorts" "icon" should not exist
+    And I should see "User is member of cohort"
+    And I should see "User is not member of cohort"
+
+  Scenario Outline: Add a rule with cohort member condition/action as admin.
+    Given the following "cohorts" exist:
+      | name                 | idnumber | contextlevel | reference |
+      | System cohort 1      | CHS1     | System       |           |
+      | Cohort in Cat 1      | CHCAT1   | Category     | CAT1      |
+      | Cohort in Subcat 1   | CHSCAT1  | Category     | SCAT1     |
+      | Cohort in Cat 2      | CHCAT2   | Category     | CAT2      |
+      | Cohort in Subcat 2   | CHSCAT2  | Category     | SCAT2     |
+    When I log in as "admin"
+    And I navigate to "Dynamic rules" in workplace launcher
+    And I follow "Edit rule 'Rule0'"
+    And I follow "<instance>"
+    And I expand the "Cohort" autocomplete
+    And "System cohort 1" "autocomplete_suggestions" should exist
+    And "Cohort in Cat 1" "autocomplete_suggestions" should exist
+    And "Cohort in Subcat 1" "autocomplete_suggestions" should exist
+    And "Cohort in Cat 2" "autocomplete_suggestions" should exist
+    And "Cohort in Subcat 2" "autocomplete_suggestions" should exist
+    And I click on "System cohort 1" item in the autocomplete list
+    And I press "Save changes"
+    Then I should see "Users who are <members> of cohort 'System cohort 1'"
+    Examples:
+      | instance                     | members     |
+      | User is member of cohort     | members     |
+      | User is not member of cohort | not members |
+
+  Scenario Outline: Add a rule with cohort member condition/action as tenant.
+    Given the following "cohorts" exist:
+      | name                 | idnumber | contextlevel | reference |
+      | System cohort 1      | CHS1     | System       |           |
+      | Cohort in Cat 1      | CHCAT1   | Category     | CAT1      |
+      | Cohort in Subcat 1   | CHSCAT1  | Category     | SCAT1     |
+      | Cohort in Cat 2      | CHCAT2   | Category     | CAT2      |
+      | Cohort in Subcat 2   | CHSCAT2  | Category     | SCAT2     |
+    # Tenant 1
+    When I log in as "tenantadmin1"
+    And I navigate to "Dynamic rules" in workplace launcher
+    And I follow "Edit rule 'Rule1'"
+    And I follow "<instance>"
+    And I expand the "Cohort" autocomplete
+    And "System cohort 1" "autocomplete_suggestions" should not exist
+    And "Cohort in Cat 1" "autocomplete_suggestions" should exist
+    And "Cohort in Subcat 1" "autocomplete_suggestions" should exist
+    And "Cohort in Cat 2" "autocomplete_suggestions" should not exist
+    And "Cohort in Subcat 2" "autocomplete_suggestions" should not exist
+    And I click on "Cohort in Subcat 1" item in the autocomplete list
+    And I press "Save changes"
+    Then I should see "Users who are <members> of cohort 'Cohort in Subcat 1'"
+    And I log out
+    # Tenant 2
+    When I log in as "tenantadmin2"
+    And I navigate to "Dynamic rules" in workplace launcher
+    And I follow "Edit rule 'Rule2'"
+    And I follow "<instance>"
+    And I expand the "Cohort" autocomplete
+    And "System cohort 1" "autocomplete_suggestions" should not exist
+    And "Cohort in Cat 1" "autocomplete_suggestions" should not exist
+    And "Cohort in Subcat 1" "autocomplete_suggestions" should not exist
+    And "Cohort in Cat 2" "autocomplete_suggestions" should exist
+    And "Cohort in Subcat 2" "autocomplete_suggestions" should exist
+    And I click on "Cohort in Subcat 2" item in the autocomplete list
+    And I press "Save changes"
+    Then I should see "Users who are <members> of cohort 'Cohort in Subcat 2'"
+    Examples:
+      | instance                     | members     |
+      | User is member of cohort     | members     |
+      | User is not member of cohort | not members |
+
+  Scenario Outline: Add a rule with cohort member condition/action as tenant with system context permission.
+    Given the following "cohorts" exist:
+      | name                 | idnumber | contextlevel | reference |
+      | System cohort 1      | CHS1     | System       |           |
+      | Cohort in Cat 1      | CHCAT1   | Category     | CAT1      |
+      | Cohort in Subcat 1   | CHSCAT1  | Category     | SCAT1     |
+    And the following "system role assigns" exist:
+      | user         | role    | contextlevel |
+      | tenantadmin1 | manager | System       |
+    # Tenant 1
+    When I log in as "tenantadmin1"
+    And I navigate to "Dynamic rules" in workplace launcher
+    And I follow "Edit rule 'Rule1'"
+    And I follow "<instance>"
+    And I expand the "Cohort" autocomplete
+    And "System cohort 1" "autocomplete_suggestions" should exist
+    And "Cohort in Cat 1" "autocomplete_suggestions" should exist
+    And "Cohort in Subcat 1" "autocomplete_suggestions" should exist
+    And I click on "System cohort 1" item in the autocomplete list
+    And I press "Save changes"
+    Then I should see "Users who are <members> of cohort 'System cohort 1'"
+    Examples:
+      | instance                     | members     |
+      | User is member of cohort     | members     |
+      | User is not member of cohort | not members |
