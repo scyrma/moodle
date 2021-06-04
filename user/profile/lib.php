8fe62c605ba62d92653ddb256e80c96a8e9bec92
Marina Glancy <marina@moodle.com>
2021-06-04 12:56:07 +0200

    WP-2457 configure different user profile fields per tenant

--- user/profile/lib.php
+++ user/profile/lib.php
@@ -637,6 +637,11 @@ function profile_get_user_fields_with_data(int $userid): array {
     }
     $sql .= 'ORDER BY uic.sortorder ASC, uif.sortorder ASC ';
     $fields = $DB->get_records_sql($sql, ['userid' => $userid]);
+
+    /** @uses \tool_tenant\profile_manager::filter_field_objects_list() */
+    $fields = component_class_callback('\tool_tenant\profile_manager', 'filter_field_objects_list',
+        [$fields, $userid], $fields);
+
     $data = [];
     foreach ($fields as $field) {
         $field->hasuserdata = !empty($field->hasuserdata);
@@ -738,6 +743,9 @@ function profile_validation(stdClass $usernew, array $files): array {
  */
 function profile_save_data(stdClass $usernew): void {
     global $CFG;
+
+    /** @uses \tool_tenant\profile_manager::before_profile_save_data() */
+    component_class_callback('\tool_tenant\profile_manager', 'before_profile_save_data', [$usernew]);
 
     $fields = profile_get_user_fields_with_data($usernew->id);
     foreach ($fields as $formfield) {
