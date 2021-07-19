b5a057f38574bd80bed2c887b1ecfde2d2a72926
Marina Glancy <marina@moodle.com>
2021-07-19 16:36:47 +0300

    WP-2709 core_user_get_users should not return hidden users

--- user/externallib.php
+++ user/externallib.php
@@ -953,6 +953,11 @@ class core_user_external extends external_api {
         // Finally retrieve each users information.
         $returnedusers = array();
         foreach ($users as $user) {
+            /** @uses \tool_tenant\tenancy::is_user_hidden_by_tenancy */
+            if (component_class_callback('tool_tenant\\tenancy', 'is_user_hidden_by_tenancy', [$user->id])) {
+                continue;
+            }
+
             $userdetails = user_get_user_details_courses($user);
 
             // Return the user only if all the searched fields are returned.
