33739b3ca5a98328db549c6648c065d29642ba7b
Cameron Ball <cameron@moodle.com>
2021-07-30 14:40:54 +0800

    gradeexort_checklist: commit 0cd1175471

--- grade/export/checklist/export.php
+++ grade/export/checklist/export.php
@@ -48,7 +48,7 @@ require_capability('gradeexport/checklist:view', $context);
 $viewall = has_capability('gradeexport/checklist:viewall', $context);
 $viewdistrict = has_capability('gradeexport/checklist:viewdistrict', $context);
 if (!$viewall && (!$viewdistrict || !$district)) {
-    print_error('nopermission', 'gradeexport_checklist');
+    throw new moodle_exception('nopermission', 'gradeexport_checklist');
 }
 
 if (!$viewall) {
@@ -56,14 +56,14 @@ if (!$viewall) {
     $sql .= "WHERE ud.fieldid = uf.id AND uf.shortname = 'district' AND ud.userid = ?";
     $mydistrict = $DB->get_record_sql($sql, [$USER->id]);
     if ($district != $mydistrict->district) {
-        print_error('wrongdistrict', 'gradeexport_checklist');
+        throw new moodle_exception('wrongdistrict', 'gradeexport_checklist');
     }
 }
 
 if ($group) {
     if ($course->groupmode != VISIBLEGROUPS && !has_capability('moodle/site:accessallgroups', $context)) {
         if (!groups_is_member($group)) {
-            print_error('wronggroup', 'gradeexport_checklist');
+            throw new moodle_exception('wronggroup', 'gradeexport_checklist');
         }
     }
 } else {
@@ -76,7 +76,7 @@ if ($group) {
 }
 
 if (!$checklist = $DB->get_record('checklist', array('id' => $checklistid))) {
-    print_error('checklistnotfound', 'gradeexport_checklist');
+    throw new moodle_exception('checklistnotfound', 'gradeexport_checklist');
 }
 
 $studentmarking = ($checklist->teacheredit != CHECKLIST_MARKING_TEACHER);
@@ -98,7 +98,7 @@ if ($district && $district !== 'ALL' && $users) {
     $users = $DB->get_records_sql($sql, array_merge($uparam, [$district]));
 }
 if (!$users) {
-    print_error('nousers', 'gradeexport_checklist');
+    throw new moodle_exception('nousers', 'gradeexport_checklist');
 }
 
 require_once($CFG->dirroot.'/grade/export/checklist/columns.php');
@@ -108,31 +108,70 @@ if (!$percentcol) {
 
 // Useful for debugging.
 if (defined('BEHAT_SITE_RUNNING')) {
+    /**
+     * Class FakeMoodleExcelWorkbook
+     */
     class FakeMoodleExcelWorkbook {
+        /**
+         * FakeMoodleExcelWorkbook constructor.
+         * @param mixed $ignore
+         */
         public function __construct($ignore) {
         }
 
+        /**
+         * Send the finished spreadsheet
+         * @param mixed $ignore
+         */
         public function send($ignore) {
         }
 
+        /**
+         * Write a string to the spreadsheet
+         * @param int $row
+         * @param int $col
+         * @param string $data
+         */
         public function write_string($row, $col, $data) {
             echo "($row, $col) = $data<br/>";
         }
 
+        /**
+         * Write a number to the spreadsheet
+         * @param int $row
+         * @param int $col
+         * @param string $data
+         */
         public function write_number($row, $col, $data) {
             echo "($row, $col) = $data<br/>";
         }
 
+        /**
+         * Add a worksheet to the workbook
+         * @param mixed $ignore
+         * @return FakeMoodleExcelWorkbook
+         */
         public function add_worksheet($ignore) {
             return new FakeMoodleExcelWorkbook($ignore);
         }
 
+        /**
+         * Close the workbook
+         */
         public function close() {
         }
     }
 }
 
-// Only write the data if it exists.
+/**
+ * Only write the data if it exists.
+ * @param object $myxls
+ * @param int $row
+ * @param int $col
+ * @param array $user
+ * @param array $extra
+ * @param string $element
+ */
 function safe_write_string($myxls, $row, $col, $user, $extra, $element) {
     if (isset($user[$element])) {
         $myxls->write_string($row, $col, $user[$element]);
@@ -286,18 +325,16 @@ foreach ($users as $user) {
         } else if ($field === '_startdate') {
             $firstview = null;
             $manager = get_log_manager();
-            $readers = $manager->get_readers('\core\log\sql_reader');
-            /** @var \core\log\sql_reader $reader */
+            $readers = $manager->get_readers('\core\log\sql_internal_table_reader');
+            /** @var \core\log\sql_internal_table_reader $reader */
             $reader = reset($readers);
             if ($reader) {
-                $select = "userid = ? AND courseid = ? AND target = 'course' AND action = 'viewed'";
-                $params = array($user->id, $course->id);
-                $events = $reader->get_events_select($select, $params, 'timecreated ASC', 0, 1);
-                /** @var \core\event\base $event */
-                $event = reset($events);
-                if ($event) {
-                    $firstview = $event->timecreated;
-                }
+                $tablename = $reader->get_internal_log_table_name();
+                $cond = [
+                    'userid' => $user->id, 'courseid' => $course->id,
+                    'target' => 'course', 'action' => 'viewed'
+                ];
+                $firstview = $DB->get_field($tablename, 'MIN(timecreated)', $cond);
             }
             $datestr = '';
             if ($firstview) {
