9ef1440dc17eea042ddafb0a1a512a9524e05e5f
Cameron Ball <cameron@moodle.com>
2021-07-30 11:52:10 +0800

    block_xp: commit e2b230c36b (v3.11.2)

--- blocks/xp/CHANGELOG.md
+++ blocks/xp/CHANGELOG.md
@@ -1,6 +1,78 @@
 Changelog
 =========
 
+v3.11.2
+-------
+
+Bug fixes
+
+- Some CSS styles were not applied for some older Moodle versions
+
+v3.11.1
+-------
+
+Quality of life
+
+- Update information in promotional page
+
+v3.11.0
+-------
+
+New features
+
+- Complete revamp of the user interface to setup the levels
+- Rule picker redesigned to include a description of each rule
+
+Quality of life
+
+- Confirmation asked when deleting a rule or condition with children
+- A help message is displayed when other plugins cause the rules screen to crash
+
+Bug fixes
+
+- The leaderboard was not accessible when using Oracle
+- The filters on the log table could not be removed when empty
+
+Technical changes
+
+- Compatibility with Moodle 3.11
+
+v3.10.2
+-------
+
+Bug fixes
+
+- The filtered_config class did not work with excluded keys
+
+v3.10.1
+-------
+
+Technical changes
+
+- Rules base class includes the get_renderer method for children classes to use
+- Switched to using numday, numweek, etc. language strings for durations in log settings
+- Travis configuration changes
+
+v3.10.0
+-------
+
+New features
+
+- Administrators can enforce the anonymity of the leaderboard
+- An additional capability to control access to the logs was added
+
+Bug fixes
+
+- The report could be stuck in an empty state after using some filters
+
+Quality of life
+
+- Display a warning when leaving the rules page without saving
+- Inform users when the cheat guard settings may become ineffective
+- Keep promo page visible when the add-on has been installed
+
+_Level up! Plus_ users may be required to upgrade the add-on for some of these changes to take effect.
+
 v3.9.0
 ------
 
--- blocks/xp/classes/output/report_table.php
+++ blocks/xp/classes/output/report_table.php
@@ -36,10 +36,10 @@ use pix_icon;
 use renderer_base;
 use stdClass;
 use table_sql;
-use user_picture;
 use block_xp\di;
 use block_xp\local\course_world;
 use block_xp\local\xp\course_user_state_store;
+use block_xp\local\utils\user_utils;
 
 /**
  * Block XP report table class.
@@ -149,7 +149,7 @@ class report_table extends table_sql {
 
         // Define SQL.
         $this->sql = new stdClass();
-        $this->sql->fields = user_picture::fields('u') . ', COALESCE(x.lvl, 1) AS lvl, x.xp, ' .
+        $this->sql->fields = user_utils::picture_fields('u') . ', COALESCE(x.lvl, 1) AS lvl, x.xp, ' .
             context_helper::get_preload_record_columns_sql('ctx');
         $this->sql->from = "{user} u
                        JOIN {context} ctx
@@ -352,12 +352,29 @@ class report_table extends table_sql {
      */
     public function print_nothing_to_display() {
         $issite = di::get('config')->get('context') == CONTEXT_SYSTEM && $this->world->get_courseid() == SITEID;
+        $hasfilters = false;
+        $showfilters = false;
+
+        if ($this->can_be_reset()) {
+            $hasfilters = true;
+            $showfilters = true;
+        }
+
+        // Render button to allow user to reset table preferences, and the initial bars if some filters
+        // are used. If none of the filters are used and there is nothing to display it just means that
+        // the course is empty and thus we do not show anything but a message.
+        echo $this->render_reset_button();
+        if ($showfilters) {
+            $this->print_initials_bar();
+        }
+
+        $message = get_string($issite ? 'reportisempty' : 'reportisemptyenrolstudents', 'block_xp');
+        if ($hasfilters) {
+            $message = get_string('nothingtodisplay', 'core');
+        }
 
         echo \html_writer::div(
-            \block_xp\di::get('renderer')->notification_without_close(
-                get_string($issite ? 'reportisempty' : 'reportisemptyenrolstudents', 'block_xp'),
-                'info'
-            ),
+            \block_xp\di::get('renderer')->notification_without_close($message, 'info'),
             '',
             ['style' => 'margin: 1em 0']
         );
