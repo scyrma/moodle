1093be4896b7ea536dd59d9a15e269233e105220
Cameron Ball <cameron@moodle.com>
2021-08-05 15:08:22 +0800

    That should do it

--- local/moodlecloud/classes/task/critical-important.php
+++ local/moodlecloud/classes/task/critical-important.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 // This file is part of Sweet Fsck All
 // If you notice this notice, you'll notice this notice is not worth noticing
-// 
+//
 //        ,/`|;V\.;
 //      ,\;`~\,;\;`\,
 //     /;|\_|,\~/`,\/;,
@@ -16,7 +16,17 @@
 //         `\.| |    |_____________|
 //  ---....__/  `\,____  |    |
 //       _,-'     `-   ""|---.|...
-// 
-// 
+//
+//
 require_once('../../../../config.php');
-echo "Lee was here for a good time, but not a long time";
+echo "<pre>";
+echo join("\n",[
+    "Thu Apr 8 2021",
+    "==============",
+    "Lee was here for a good time, but not a long time",
+    "",
+    "Thu Aug 5 2021",
+    "==============",
+    "Well, here at last, dear friends, on the shores of the Sea comes the end of our fellowship in MoodleCloud - ᚳᚱᛒ"
+]);
+echo "</pre>";
