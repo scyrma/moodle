6478754b7f5d7cdccfe848224cedb5786f2c49dd
Marina Glancy <marina@moodle.com>
2021-09-16 19:00:57 +0200

    WP-2816 WP-2870 pass arguments to the tenant callback

--- my/lib.php
+++ my/lib.php
@@ -57,7 +57,8 @@ function my_get_page(?int $userid, int $private = MY_PAGE_PRIVATE, string $pagen
     }
 
     /** @uses \tool_tenant\dashboard_manager::show_tenant_dashboard_if_enabled() */
-    if ($page = component_class_callback('\tool_tenant\dashboard_manager', 'show_tenant_dashboard_if_enabled', [])) {
+    if ($page = component_class_callback('\tool_tenant\dashboard_manager',
+            'show_tenant_dashboard_if_enabled', [$userid, $private])) {
         return $page;
     }
 
