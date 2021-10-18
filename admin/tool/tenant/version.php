82677e4fb832d124ba18d47b9bb7a37b15e5ee9a
Moodle Workplace <workplace@moodle.com>
2021-10-18 02:17:44 +0000

    Moodle Workplace plugin tool_tenant (build 77444)

--- admin/tool/tenant/version.php
+++ admin/tool/tenant/version.php
@@ -38,10 +38,10 @@ defined('MOODLE_INTERNAL') || die();
 
 // This plugin is part of Moodle Workplace product.
 $plugin->component    = 'tool_tenant';
-$plugin->release      = '3.11.3+ (Build: 20211011)';
-$plugin->version      = 2021101100;
+$plugin->release      = '3.11.3+ (Build: 20211018)';
+$plugin->version      = 2021101800;
 $plugin->requires     = 2021051703.00;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [
-    'tool_wp'         => 2021101100,
+    'tool_wp'         => 2021101800,
 ];
