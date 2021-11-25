9149d9486b8e1f10b7b1bd9806a4001fc70512b4
Moodle Workplace <workplace@moodle.com>
2021-11-25 16:13:16 +0000

    Moodle Workplace plugin tool_datastore (build 79698)

---
+++ admin/tool/datastore/classes/local/action/base.php
@@ -0,0 +1,151 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * Class containing the base abstract class for actions.
+ *
+ * @package   tool_datastore
+ * @copyright 2018 Moodle Pty Ltd <support@moodle.com>
+ * @author    2018 Alberto Lara Hernández <albertolara@moodle.com>
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace tool_datastore\local\action;
+
+use tool_datastore\local\models\action;
+use tool_datastore\local\models\entity;
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Abstract class to be implemented for an action to be stored.
+ *
+ * @copyright 2018 Moodle Pty Ltd <support@moodle.com>
+ * @author    2018 Alberto Lara Hernández <albertolara@moodle.com>
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+abstract class base {
+
+    /** @var int Id of the row when the event is created */
+    protected $datastoreeventid;
+    /** @var \core\event\base Event object */
+    private $event;
+    /** @var string Event name */
+    private $eventname;
+
+    /**
+     * base constructor.
+     *
+     * @param \core\event\base $event
+     */
+    public function __construct(\core\event\base $event) {
+        $this->event = $event;
+        $this->eventname = $event->target . '_' . $event->action;
+    }
+
+    /**
+     * Store the event action.
+     *
+     * @return bool
+     */
+    public function save_action(): bool {
+        try {
+            $actionid = action::create_action(
+                $this->eventname,
+                $this->event->relateduserid,
+                $this->event->courseid
+            );
+            $this->datastoreeventid = $actionid;
+            $this->save_entities();
+        } catch (\Exception $ex) {
+            debugging('Error save_action: '.$ex->getMessage(), DEBUG_NORMAL, $ex->getTrace());
+        }
+
+        return true;
+
+    }
+
+    /**
+     * For each entity related to the event store the data in the table.
+     *
+     * @return bool
+     * @throws \coding_exception
+     * @throws \core\invalid_persistent_exception
+     */
+    private function save_entities() {
+        $relatedentities = $this->related_entities();
+        $stored = [];
+
+        foreach ($relatedentities as $relatedentity => $typeofentity) {
+            $id = $this->get_event_property($relatedentity);
+            if (empty($stored[$typeofentity]) || !in_array($id, $stored[$typeofentity])) {
+                $this->store_entity($typeofentity, $id);
+                $stored[$typeofentity] = isset($stored[$typeofentity]) ? array_merge($stored[$typeofentity], [$id]) : [$id];
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Get a property from the event object.
+     *
+     * @param string $property
+     *
+     * @return mixed
+     */
+    protected function get_event_property(string $property) {
+        return $this->event->{$property};
+    }
+
+    /**
+     * Store the entity if not exists.
+     *
+     * @param string $type
+     * @param int $originalid
+     */
+    protected function store_entity(string $type, int $originalid) {
+        entity::create_entity($this->datastoreeventid, $originalid, $type);
+    }
+
+    /**
+     * Define an array of entities (table to search) and the field to get the main id in the event object.
+     *
+     * For example, for "user" table, "userid" is the field in the event object with the id of the "user" entity.
+     *
+     * @return mixed
+     */
+    abstract protected function related_entities();
+
+    /**
+     * Define the entity fields to index, indexed by the entity name => array of fields
+     *
+     * @return array
+     */
+    abstract protected static function get_fields_to_index(): array;
+}
---
+++ admin/tool/datastore/classes/observer.php
@@ -0,0 +1,83 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * Event observers for datastore.
+ *
+ * @package    tool_datastore
+ * @copyright  2018 Moodle Pty Ltd <support@moodle.com>
+ * @author     2018 Alberto Lara Hernández <albertolara@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace tool_datastore;
+
+use core\event\base;
+use core\event\course_restored;
+use tool_datastore\task\migrate_course_completion;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Event observer for datastore.
+ *
+ * @package    tool_datastore
+ * @copyright  2018 Moodle Pty Ltd <support@moodle.com>
+ * @author     2018 Alberto Lara Hernández <albertolara@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class observer {
+
+    /**
+     * Observe all events to determine whether we have an appropriate action class to store the data
+     *
+     * @param base $event
+     * @return void
+     */
+    public static function observe_all(base $event) {
+        $actionclass = action_factory::create($event);
+        if ($actionclass) {
+            $actionclass->save_action();
+        }
+    }
+
+    /**
+     * Course restored event
+     *
+     * @param course_restored $event
+     * @return void
+     */
+    public static function course_restored(course_restored $event) {
+        $course = $event->get_record_snapshot('course', $event->objectid);
+
+        // Schedule ad-hoc task to migrate completion data from restored course.
+        $task = new migrate_course_completion();
+        $task->set_custom_data(['courseid' => $course->id]);
+
+        \core\task\manager::queue_adhoc_task($task, true);
+    }
+}
