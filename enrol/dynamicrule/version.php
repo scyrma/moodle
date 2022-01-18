a9698f76687a2b2f1b8b6ee90b712e6b6d6d35ab
Moodle Workplace <workplace@moodle.com>
2022-01-18 16:25:54 +0000

    Moodle Workplace plugin enrol_dynamicrule (build 81399)

---
+++ enrol/dynamicrule/version.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace Code is dual-licensed under the terms of both the
+// single GNU General Public Licence version 3.0, dated 29 June 2007
+// and the terms of the proprietary Moodle Workplace Licence strictly
+// controlled by Moodle Pty Ltd and its certified premium partners.
+// Wherever conflicting terms exist, the terms of the MWL are binding
+// and shall prevail.
+
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package     enrol_dynamicrule
+ * @copyright   2018 Moodle Pty Ltd <support@moodle.com>
+ * @author      2018 Marina Glancy <marina@moodle.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// This plugin is part of Moodle Workplace product.
+$plugin->component    = 'enrol_dynamicrule';
+$plugin->release      = '3.10.9';
+$plugin->version      = 2021051240;
+$plugin->requires     = 2020110909.00;
+$plugin->maturity     = MATURITY_STABLE;
