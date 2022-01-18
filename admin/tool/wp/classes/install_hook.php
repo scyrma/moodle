fde8b497ef098f84d09ff0f978a3ea7a4fce4adc
Moodle Workplace <workplace@moodle.com>
2022-01-18 16:55:35 +0000

    Moodle Workplace plugin tool_wp (build 81402)

---
+++ admin/tool/wp/classes/install_hook.php
@@ -0,0 +1,102 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+namespace tool_wp;
+
+/**
+ * Class used to execute hook after install all plugins.
+ *
+ * @package     tool_wp
+ * @copyright   2019 Moodle Pty Ltd <support@moodle.com>
+ * @author      2019 Daniel Neis Araujo <daniel@moodle.com>
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class install_hook {
+
+    /**
+     * This is executed by web and cli install, after installing plugins and setting default config.
+     *
+     */
+    public static function execute() {
+        global $DB, $CFG;
+
+        // Do not do it in the behat/unittests because it will break all core tests.
+        if (!defined('BEHAT_SITE_RUNNING') && !(defined('PHPUNIT_TEST') && PHPUNIT_TEST)) {
+
+            // Remove the "admin bookmarks" block.
+            if ($adminbookmarksblock = $DB->get_record('block_instances', ['blockname' => 'admin_bookmarks'])) {
+                blocks_delete_instance($adminbookmarksblock);
+            }
+
+            // Hide blocks 'recentlyaccessedcourses' and 'myoverview'.
+            $DB->execute('UPDATE {block} set visible=? WHERE name=?', [0, 'recentlyaccessedcourses']);
+
+            // Remove instances of recentlyaccessedcourses' and 'myoverview' from all dashboards (and default dashboard).
+            if ($CFG->tool_wp_installed >= 2) {
+                // Execute if this is the fresh install or it is an upgrade from LMS with $CFG->forcewpsetup.
+                $instances = $DB->get_records_select('block_instances',
+                    'pagetypepattern=:myindex AND (blockname=:b1 OR blockname=:b2)',
+                    ['myindex' => 'my-index', 'b1' => 'myoverview', 'b2' => 'recentlyaccessedcourses']);
+                foreach ($instances as $blockinstance) {
+                    blocks_delete_instance($blockinstance);
+                }
+            }
+
+            // Set "Force login" to true.
+            set_config('forcelogin', 1);
+            // Disable donation banner.
+            set_config('showcampaigncontent', 0);
+            // Disable "user feedback" banner.
+            set_config('enableuserfeedback', 0);
+            // Disable Moodle.net integration.
+            set_config('enablemoodlenet', 0, 'tool_moodlenet');
+
+            // Update mobile config to point to Workplace app.
+            $settings = [
+                ['plugin' => 'tool_mobile', 'name' => 'enablesmartappbanners', 'value' => 1, 'default' => 0],
+                ['plugin' => 'tool_mobile', 'name' => 'iosappid', 'value' => '1470929705', 'default' => '633359593'],
+                ['plugin' => 'tool_mobile', 'name' => 'androidappid', 'value' => 'com.moodle.workplace',
+                    'default' => 'com.moodle.moodlemobile'],
+                ['plugin' => 'tool_mobile', 'name' => 'setuplink', 'value' => 'https://download.moodle.org/mobile',
+                    'default' => ''],
+                ['plugin' => 'tool_mobile', 'name' => 'forcedurlscheme', 'value' => 'mmworkplace', 'default' => 'moodlemobile'],
+                ['name' => 'airnotifiermobileappname', 'value' => 'com.moodle.workplace', 'default' => 'com.moodle.moodlemobile'],
+                ['name' => 'airnotifierappname', 'value' => 'commoodleworkplace', 'default' => 'commoodlemoodlemobile'],
+            ];
+
+            foreach ($settings as $setting) {
+                $plugin = $setting['plugin'] ?? null;
+
+                // Compare current value to default value, if they match then set our own.
+                $currentvalue = get_config($plugin, $setting['name']);
+                if (empty($currentvalue) || (strcasecmp($currentvalue, $setting['default']) === 0)) {
+                    set_config($setting['name'], $setting['value'], $plugin);
+                }
+            }
+        }
+    }
+}
---
+++ admin/tool/wp/tests/behat/exportimport_certificates.feature
@@ -0,0 +1,78 @@
+@tool @tool_certificate @tool_wp @moodleworkplace @javascript
+Feature: Export and import certificates
+  As an administrator
+  I want to export and import certificates
+
+  Background:
+    Given "2" tenants exist with "2" users and "0" courses in each
+    And the following certificate templates exist:
+      | name          | category  |
+      | Certificate 1 | Category1 |
+      | Certificate 2 |           |
+      | Certificate 3 | Category2 |
+    And the following certificate issues exist:
+      | template      | user   |
+      | Certificate 1 | user11 |
+
+  Scenario: Export and import certificates as tenant admin
+    When I log in as "tenantadmin1"
+    And I change window size to "large"
+    And I navigate to "Migration" in workplace launcher
+    And I press "Export"
+    And I click on "Certificates" "radio"
+    And I press "Next"
+    And I click on "Select categories and subcategories manually" "radio"
+    And I press "Next"
+    And I should see "Select at least one category"
+    And I click on "Select certificate templates manually" "radio"
+    And I press "Next"
+    And I should see "Select at least one template"
+    And I click on "Select all certificate templates" "radio"
+    And I press "Next"
+    And I should see "Certificate 1"
+    And I should see "Certificate 2"
+    And I press "Export"
+    And I click on "Proceed" "button" in the "Confirm" "dialogue"
+    And I should see "Scheduled" in the "Status" "table_row"
+    And I click on "Close" "link_or_button"
+    Then the following should exist in the "report-table" table:
+      | Exporter       | Status    |
+      | Certificates   | Scheduled |
+    And I run all adhoc tasks
+    And I navigate to "Export and import" in workplace launcher
+    And I click on "New import from this file" "link"
+    And I should see "Certificates (2)"
+    And I press "Next"
+    And I should see "Instances"
+    And I click on "Select certificate templates manually" "radio"
+    And I press "Next"
+    And I should see "Select at least one template"
+    And I open the autocomplete suggestions list
+    And I click on "Certificate 1" item in the autocomplete list
+    And I press "Next"
+    And I should see "Will be imported"
+    And I should see "Instances (2)"
+    And I should see "Certificate 1"
+    And I should see "Certificate issue: User 11"
+    And I press "Import"
+    And I press "Proceed"
+    And I click on "Close" "link_or_button"
+
+  Scenario Outline: Export all certificates as admin
+    When I log in as "admin"
+    # Switching to this tenant means "Certificate 3" won't be included when exporting only this tenant.
+    And I switch to tenant "Tenant1"
+    And I perform a new export with these options:
+      | Step 1 | Certificates                     | 1            |
+      | Step 1 | Create export from               | <exportfrom> |
+      | Step 2 | Select all certificate templates | 1            |
+    And I click on "View export" "link" in the "Certificates" "table_row"
+    Then I should see "Success" in the "Status" "table_row"
+    And I should see "Instances (<instancecount>)"
+    And I should see "Certificate 1" in the "[data-region=\"exportimport-instances\"]" "css_element"
+    And I should see "Certificate 2" in the "[data-region=\"exportimport-instances\"]" "css_element"
+    And I <shouldornotsee> see "Certificate 3" in the "[data-region=\"exportimport-instances\"]" "css_element"
+    Examples:
+      | exportfrom     | instancecount | shouldornotsee |
+      | Site           | 3             | should         |
+      | Current tenant | 2             | should not     |
