b24e3cf2446236bc60347ec8b2cfefc5fdc69502
Marina Glancy <marina@moodle.com>
2022-05-09 16:51:26 +0200

    MDL-74704 core: Revert "MDL-74511 core: Add phpcs config generator"

--- .grunt/tasks/ignorefiles.js
+++ .grunt/tasks/ignorefiles.js
@@ -21,43 +21,6 @@
  */
 
 module.exports = grunt => {
-
-    /**
-     * Generate the PHPCS configuration.
-     *
-     * @param {Object} thirdPartyPaths
-     */
-    const phpcsIgnore = (thirdPartyPaths) => {
-        const {toXML} = require('jstoxml');
-
-        const config = {
-            _name: 'ruleset',
-            _attrs: {
-                name: "MoodleCore",
-            },
-            _content: [
-                {
-                    rule: {
-                        _attrs: {
-                            ref: './.phpcs.xml.dist',
-                        },
-                    },
-                },
-            ],
-        };
-
-        thirdPartyPaths.forEach(library => {
-            config._content.push({
-                'exclude-pattern': library,
-            });
-        });
-
-        grunt.file.write('.phpcs.xml', toXML(config, {
-            header: true,
-            indent: '  ',
-        }) + "\n");
-    };
-
     /**
      * Generate ignore files (utilising thirdpartylibs.xml data)
      */
@@ -89,8 +52,6 @@ module.exports = grunt => {
             'jsdoc/styles/*.css',
         ].concat(thirdPartyPaths);
         grunt.file.write('.stylelintignore', stylelintIgnores.join('\n') + '\n');
-
-        phpcsIgnore(thirdPartyPaths);
     };
 
     grunt.registerTask('ignorefiles', 'Generate ignore files for linters', handler);
