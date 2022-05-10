b6d0e8a1aab2c7d4aedb6bea92ca2e5f5f2383fa
Moodle Workplace <workplace@moodle.com>
2022-05-10 12:16:11 +0000

    Moodle Workplace plugin enrol_program (build 91194)

---
+++ enrol/program/lang/en/enrol_program.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace Code is dual-licensed under the terms of both the
+// single GNU General Public Licence version 3.0, dated 29 June 2007
+// and the terms of the proprietary Moodle Workplace Licence strictly
+// controlled by Moodle Pty Ltd and its certified premium partners.
+// Wherever conflicting terms exist, the terms of the MWL are binding
+// and shall prevail.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     enrol_program
+ * @category    string
+ * @copyright   2018 Moodle Pty Ltd <support@moodle.com>
+ * @author      2018 Workplace team
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['canntenrol'] = 'Enrolment is disabled or inactive';
+$string['canntenrolearly'] = 'You cannot enrol yet; enrolment starts on {$a}.';
+$string['canntenrollate'] = 'You cannot enrol any more, since enrolment ended on {$a}.';
+$string['canntenrollocked'] = 'You can not enrol, this course is locked.';
+$string['enrolme'] = 'Enrol me';
+$string['pluginname'] = 'Program enrolment';
+$string['privacy:metadata'] = 'The Program enrolment plugin does not store any personal data about any user.';
+$string['program:config'] = 'Configure Program enrolment instances';
+$string['program:enrol'] = 'Enrol users to the course';
+$string['program:manage'] = 'Manage enrolled users';
+$string['program:unenrol'] = 'Unenrol users from the course';
