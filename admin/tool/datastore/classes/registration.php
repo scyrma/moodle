14490a32eb3f5a9d949de07c3a6f810d2acc14f0
Moodle Workplace <workplace@moodle.com>
2022-05-10 12:32:01 +0000

    Moodle Workplace plugin tool_datastore (build 91204)

---
+++ admin/tool/datastore/classes/registration.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * Class registration
+ *
+ * @package     tool_datastore
+ * @copyright   2019 Moodle Pty Ltd <support@moodle.com>
+ * @author      2019 Marina Glancy
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace tool_datastore;
+
+/**
+ * Class registration
+ *
+ * @package     tool_datastore
+ * @copyright   2019 Moodle Pty Ltd <support@moodle.com>
+ * @author      2019 Marina Glancy
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class registration {
+
+    /**
+     * Implementation of callback 'stats' called from tool_wp\\registration::site_info
+     *
+     * Can be called as:
+     * component_class_callback('tool_datastore\\registration', 'stats', [true]);
+     *
+     * @param bool $usestrings return data in human readable form to be displayed on the "Registration" page
+     * @return array
+     */
+    public static function stats($usestrings = false) {
+        global $DB;
+        $count = $DB->count_records('tool_datastore_action', []);
+        return [
+            'wpdatastorerecords' => $count,
+        ];
+    }
+
+}
