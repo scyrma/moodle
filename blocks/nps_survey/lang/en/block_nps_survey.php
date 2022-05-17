29c520f44d1add4690fd22429c7239677ff7dfb1
Praj Basnet <praj.basnet@gmail.com>
2022-05-17 14:58:37 +1000

    MC-4748 blocks/nps_survey

---
+++ blocks/nps_survey/lang/en/block_nps_survey.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English Language strings.
+ *
+ * @package    block_nps_survey
+ * @copyright  MoodleCloud Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'NPS';
+$string['blocktitle'] = 'Your feedback matters';
+
+// Plugin Settings.
+$string['surveytext'] = 'Survey Text';
+$string['surveytextexplanation'] = 'Enter explanation text for the survey here.';
+$string['surveylink'] = 'Survey Link';
+$string['surveylinkexplanation'] = 'Enter the URL for the NPS survey here.';
