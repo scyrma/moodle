eedd47207c6de0d727b1806b33eea90068264218
David Matamoros <davidmc@moodle.com>
2022-06-16 13:02:12 +0200

    WP-3519 Allow user to enrol into courses using course link
    - Fixes the problem when if the course is not visible to the user in
    the course listing, user will never be redirected to the enrolment page

--- course/classes/category.php
+++ course/classes/category.php
@@ -700,7 +700,10 @@ class core_course_category implements renderable, cacheable_object, IteratorAggr
         }
         $categorycontext = isset($course->category) ? context_coursecat::instance($course->category) :
             context_course::instance($course->id)->get_parent_context();
-        return has_capability('moodle/category:viewcourselist', $categorycontext, $user);
+        /** @uses \tool_program\api::is_course_in_user_programs() */
+        return has_capability('moodle/category:viewcourselist', $categorycontext, $user) ||
+            component_class_callback('tool_program\api', 'is_course_in_user_programs',
+                [(int) $course->id, $user], false);
     }
 
     /**
