a0f64171da3e39db34b69ef8b31eb876cbf00a28
Moodle Workplace <workplace@moodle.com>
2022-08-23 16:57:31 +0000

    Moodle Workplace plugin format_wplist (build 100636)

---
+++ course/format/wplist/lang/en/format_wplist.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * Strings for course format.
+ *
+ * @package    format_wplist
+ * @copyright  2018 Moodle Pty Ltd <support@moodle.com>
+ * @author     2018 <bas@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+$string['accordioneffect'] = 'Accordion effect';
+$string['accordioneffect_help'] = 'Select Yes to close open sections when opening another section';
+$string['collapsed'] = 'Collapsed';
+$string['collapsesection'] = 'Collapse section {$a}';
+$string['currentsection'] = 'This section';
+$string['deletesection'] = 'Delete section';
+$string['emptysection'] = 'This section is empty';
+$string['expanded'] = 'Expanded';
+$string['expandsection'] = 'Expand section {$a}';
+$string['hidefromothers'] = 'Hide section';
+$string['highlight'] = 'Highlight section';
+$string['page-course-view-wplists'] = 'Any course main page in Workplace list format';
+$string['page-course-view-wplists-x'] = 'Any course page in Workplace list format';
+$string['pluginname'] = 'Workplace list format';
+$string['privacy:metadata:opensections'] = 'The list of sections the user keeps open on courses.';
+$string['section0name'] = 'General';
+$string['section_completion'] = 'This section is {$a}% completed';
+$string['sectionname'] = 'Topic';
+$string['sectionstate'] = 'Initial section state';
+$string['showfromothers'] = 'Show section';
