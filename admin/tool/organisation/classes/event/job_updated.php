37b50347ae835b8d145bce6ec66068c0ee010e74
Moodle Workplace <workplace@moodle.com>
2022-09-13 11:00:30 +0000

    Moodle Workplace plugin tool_organisation (build 102991)

---
+++ admin/tool/organisation/classes/event/job_updated.php
@@ -0,0 +1,130 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * Plugin event classes are defined here.
+ *
+ * @package     tool_organisation
+ * @copyright   2018 Moodle Pty Ltd <support@moodle.com>
+ * @author      2018 Marina Glancy
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace tool_organisation\event;
+
+use core\event\base;
+use tool_organisation\job;
+use tool_organisation\job_manager;
+
+/**
+ * The job_updated event class.
+ *
+ * @package    tool_organisation
+ * @copyright  2018 Moodle Pty Ltd <support@moodle.com>
+ * @author     2018 Marina Glancy
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class job_updated extends \core\event\base {
+
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['objecttable'] = 'tool_organisation_job';
+        $this->data['crud'] = 'u';
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+    }
+
+    /**
+     * Creates an event from job object
+     *
+     * @param job $job
+     * @param \stdClass $oldrecord
+     * @return job_updated
+     */
+    public static function create_from_object(job $job, \stdClass $oldrecord) : base {
+        $params = [
+            'context' => \context_system::instance(),
+            'objectid' => $job->get('id'),
+            'relateduserid' => $job->get('userid'),
+        ];
+
+        $event = static::create($params);
+        $event->add_record_snapshot(job::TABLE, $job->to_record());
+        return $event;
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return get_string('eventjobupdated', 'tool_organisation');
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "The user with id '$this->userid' updated the job with id '$this->objectid' " .
+            "for user with the id '$this->relateduserid'";
+    }
+
+    /**
+     * Get URL related to the action.
+     *
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new \moodle_url(job_manager::get_job_url());
+    }
+
+    /**
+     * This is used when restoring course logs where it is required that we
+     * map the objectid to it's new value in the new course.
+     *
+     * @return int|string
+     */
+    public static function get_objectid_mapping() {
+        return base::NOT_MAPPED;
+    }
+
+    /**
+     * This is used when restoring course logs where it is required that we
+     * map the information in 'other' to it's new value in the new course.
+     *
+     * @return array|bool
+     */
+    public static function get_other_mapping() {
+        return false;
+    }
+}
