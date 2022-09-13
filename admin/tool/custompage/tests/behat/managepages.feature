40179d9636080518b01ab09a76d57a1e00e02a7b
Moodle Workplace <workplace@moodle.com>
2022-09-13 12:54:40 +0000

    Moodle Workplace plugin tool_custompage (build 103011)

---
+++ admin/tool/custompage/tests/behat/managepages.feature
@@ -0,0 +1,183 @@
+@tool @tool_custompage @moodleworkplace @javascript
+Feature: Manage custom pages
+  In order to manage custom pages
+  A a user
+  I need to be able to create, update and delete pages
+
+  Scenario: Create new global custom page
+    Given I log in as "admin"
+    And I navigate to "Custom pages" in workplace launcher
+    When I open the action menu in "page-header" "region"
+    And I choose "New global page" in the open action menu
+    And I set the following fields in the "New global page" "dialogue" to these values:
+      | Title  | This is my site page |
+      | Weight | -4                   |
+    And I click on "Save" "button" in the "New global page" "dialogue"
+    And I should see "You must supply a value here."
+    And I set the following fields in the "New global page" "dialogue" to these values:
+      | Name   | My site page |
+    And I click on "Save" "button" in the "New global page" "dialogue"
+    Then I should see "My site page"
+    And I should see "Global page" in the ".page-context-header" "css_element"
+    And I should see "Nothing to display"
+
+  Scenario: Create new custom page for current tenant
+    Given I log in as "admin"
+    And I navigate to "Custom pages" in workplace launcher
+    When I open the action menu in "page-header" "region"
+    And I choose "New tenant page" in the open action menu
+    And I set the following fields in the "New page" "dialogue" to these values:
+      | Title  | This is my tenant page |
+      | Weight | -4                     |
+    And I click on "Save" "button" in the "New page" "dialogue"
+    And I should see "You must supply a value here."
+    And I set the following fields in the "New page" "dialogue" to these values:
+      | Name   | My tenant page |
+    And I click on "Save" "button" in the "New page" "dialogue"
+    Then I should see "My tenant page"
+    And I should not see "Global page"
+    And I should see "Nothing to display"
+
+  Scenario: View list of custom pages for tenant as admin
+    Given "2" tenants exist with "0" users and "0" courses in each
+    And the following "tool_custompage > Pages" exist:
+      | name          | weight | global | tenant  |
+      | Tenant 1 page | 0      | 0      | Tenant1 |
+      | Tenant 2 page | 0      | 0      | Tenant2 |
+      | Site page     | -1     | 1      | Tenant2 |
+    When I log in as "admin"
+    And I switch to tenant "Tenant1"
+    And I navigate to "Custom pages" in workplace launcher
+    Then the following should exist in the "reportbuilder-table" table:
+      | Weight | Name          |
+      | -1     | Site page     |
+      | 0      | Tenant 1 page |
+    And I should see "Global page" in the "Site page" "table_row"
+    And I should not see "Global page" in the "Tenant 1 page" "table_row"
+    And I should not see "Tenant 2 page" in the "reportbuilder-table" "table"
+
+  Scenario Outline: View list of custom pages for tenant as tenant admin
+    Given "2" tenants exist with "0" users and "0" courses in each
+    And the following "tool_custompage > Pages" exist:
+      | name          | weight | global | tenant  |
+      | Tenant 1 page | 0      | 0      | Tenant1 |
+      | Tenant 2 page | 0      | 0      | Tenant2 |
+      | Site page 1   | -1     | 1      | Tenant2 |
+      | Site page 2   | 1      | 1      | Tenant2 |
+    And the following "tool_custompage > Audiences" exist:
+      | page        | configdata |
+      | Site page 1 |            |
+    When I log in as "<user>"
+    And I navigate to "Custom pages" in workplace launcher
+    Then I should see "<tenantpage>" in the "reportbuilder-table" "table"
+    And I should not see "<nontenantpage>" in the "reportbuilder-table" "table"
+    And I should see "Site page 1" in the "reportbuilder-table" "table"
+    And I should not see "Site page 2" in the "reportbuilder-table" "table"
+    Examples:
+      | user         | tenantpage    | nontenantpage |
+      | tenantadmin1 | Tenant 1 page | Tenant 2 page |
+      | tenantadmin2 | Tenant 2 page | Tenant 1 page |
+
+  Scenario: Edit details of custom page from page listings
+    Given the following "tool_custompage > Page" exists:
+      | name   | My page |
+      | weight | 0       |
+    When I log in as "admin"
+    And I navigate to "Custom pages" in workplace launcher
+    And I set the following fields in the "My page" "table_row" to these values:
+    # TODO: Behat flakes out on the select inplace editable.
+    #  | Edit weight | 5               |
+      | Edit name   | My updated page |
+    And I reload the page
+    Then the following should exist in the "reportbuilder-table" table:
+      | Weight | Name            |
+      | 0      | My updated page |
+    And I should not see "My page" in the "reportbuilder-table" "table"
+
+  Scenario: Delete custom page from page listings
+    Given the following "tool_custompage > Pages" exist:
+      | name     | weight |
+      | Page one | 0      |
+      | Page two | 1      |
+    When I log in as "admin"
+    And I navigate to "Custom pages" in workplace launcher
+    And I press "Delete" action in the "Page one" report row
+    And I click on "Delete" "button" in the "Delete page" "dialogue"
+    Then I should see "Deleted page"
+    And I should not see "Page one" in the "reportbuilder-table" "table"
+
+  Scenario: Filter page by type
+    Given "1" tenants exist with "0" users and "0" courses in each
+    And the following "tool_custompage > Pages" exist:
+      | name           | weight | global |
+      | My global page | -1     | 1      |
+      | My tenant page | 0      | 0      |
+    When I log in as "admin"
+    And I navigate to "Custom pages" in workplace launcher
+    And I click on "Filters" "button"
+    And I set the following fields in the "Page type" "core_reportbuilder > Filter" to these values:
+      | Page type operator | Is any value |
+    And I click on "Apply" "button" in the "[data-region='report-filters']" "css_element"
+    Then the following should exist in the "reportbuilder-table" table:
+      | Weight | Name           |
+      | -1     | My global page |
+      | 0      | My tenant page |
+    And I set the following fields in the "Page type" "core_reportbuilder > Filter" to these values:
+      | Page type operator | Is equal to |
+      | Page type value    | Global page |
+    And I click on "Apply" "button" in the "[data-region='report-filters']" "css_element"
+    Then the following should exist in the "reportbuilder-table" table:
+      | Weight | Name           |
+      | -1     | My global page |
+    And I should not see "My tenant page" in the "reportbuilder-table" "table"
+    And I set the following fields in the "Page type" "core_reportbuilder > Filter" to these values:
+      | Page type operator | Is equal to |
+      | Page type value    | Tenant page |
+    And I click on "Apply" "button" in the "[data-region='report-filters']" "css_element"
+    Then the following should exist in the "reportbuilder-table" table:
+      | Weight | Name           |
+      | 0      | My tenant page |
+    And I should not see "My global page" in the "reportbuilder-table" "table"
+    And I log out
+    And I log in as "tenantadmin1"
+    And I navigate to "Custom pages" in workplace launcher
+    And I click on "Filters" "button"
+    Then I should not see "Page type" in the "[data-region='report-filters']" "css_element"
+
+  Scenario: Add blocks as tenant admin
+    Given "1" tenants exist with "1" users and "0" courses in each
+    And I log in as "admin"
+    And I navigate to "Users > Organisation > Manage tenants" in site administration
+    And I click on "Tenant1" "link" in the "Tenant1" "tool_wp > Table tree node"
+    And I navigate to "Details" in current page administration
+    And I set the following fields to these values:
+      | No category | 1 |
+    And I press "Save changes"
+    And I log out
+    And I log in as "tenantadmin1"
+    And I navigate to "Custom pages" in workplace launcher
+    And I click on "New page" "button"
+    And I set the following fields in the "New page" "dialogue" to these values:
+      | Name   | My admin tenant page         |
+      | Title  | This is my admin tenant page |
+      | Weight | -10                          |
+    And I click on "Save" "button" in the "New page" "dialogue"
+    Then I should see "My admin tenant page"
+    And I should not see "Global page"
+    And I should see "Nothing to display"
+    When I click on "Edit page" "link"
+    And I switch editing mode on
+    And I click on "Add a block" "link" in the ".drawer-right" "css_element"
+    And I click on "Comments" "link" in the "Add a block" "dialogue"
+    Then "Comments" "block" should exist in the ".drawer-right" "css_element"
+    And I open the "Comments" blocks action menu
+    And I click on "Delete Comments block" "link" in the "Comments" "block"
+    And I click on "Delete" "button" in the "Delete block?" "dialogue"
+    Then "Comments" "block" should not exist in the ".drawer-right" "css_element"
+    And I click on "Add a block" "link" in the "region-main" "region"
+    And I click on "Recently accessed items" "link" in the "Add a block" "dialogue"
+    And "Recently accessed items" "block" should exist in the "region-main" "region"
+    And I open the "Recently accessed items" blocks action menu
+    And I click on "Delete Recently accessed items block" "link" in the "Recently accessed items" "block"
+    And I click on "Delete" "button" in the "Delete block?" "dialogue"
+    And "Recently accessed items" "block" should not exist in the "region-main" "region"
---
+++ admin/tool/custompage/tests/permission_test.php
@@ -0,0 +1,410 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+declare(strict_types=1);
+
+namespace tool_custompage;
+
+use advanced_testcase;
+use context_system;
+use tool_custompage_generator;
+use tool_tenant_generator;
+use tool_tenant\tenancy;
+
+/**
+ * Unit tests for the permission class
+ *
+ * @package     tool_custompage
+ * @covers      \tool_custompage\permission
+ * @copyright   2022 Moodle Pty Ltd <support@moodle.com>
+ * @author      2022 Paul Holden <paulh@moodle.com>
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class permission_test extends advanced_testcase {
+
+    /**
+     * Data provider for {@see test_can_view_pages_list} and {@see test_can_create_pages}
+     *
+     * @return array[]
+     */
+    public function capabilities_provider(): array {
+        return [
+            ['tool/custompage:edit', true],
+            ['tool/custompage:editall', true],
+            [null, false],
+        ];
+    }
+
+    /**
+     * Test whether user can view pages list
+     *
+     * @param string|null $capability
+     * @param bool $expectsuccess
+     * @dataProvider capabilities_provider
+     */
+    public function test_can_view_pages_list(?string $capability, bool $expectsuccess): void {
+        global $DB;
+
+        $this->resetAfterTest();
+
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+
+        if ($capability !== null) {
+            $userrole = $DB->get_field('role', 'id', ['shortname' => 'user']);
+            assign_capability($capability, CAP_ALLOW, $userrole, context_system::instance());
+        }
+
+        $this->assertEquals($expectsuccess, permission::can_view_pages_list());
+    }
+
+    /**
+     * Test that exception is thrown when a user cannot view pages list
+     */
+    public function test_require_can_view_pages_list(): void {
+        $this->expectException(permission_exception::class);
+        $this->expectExceptionMessage('You cannot view the list of pages');
+        permission::require_can_view_pages_list();
+    }
+
+    /**
+     * Test whether user can create pages
+     *
+     * @param string|null $capability
+     * @param bool $expectsuccess
+     * @dataProvider capabilities_provider
+     */
+    public function test_can_create_page(?string $capability, bool $expectsuccess): void {
+        global $DB;
+
+        $this->resetAfterTest();
+
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+
+        if ($capability !== null) {
+            $userrole = $DB->get_field('role', 'id', ['shortname' => 'user']);
+            assign_capability($capability, CAP_ALLOW, $userrole, context_system::instance());
+        }
+
+        $this->assertEquals($expectsuccess, permission::can_create_page());
+    }
+
+    /**
+     * Test that exception is thrown when a user cannot create pages
+     */
+    public function test_require_can_create_page(): void {
+        $this->expectException(permission_exception::class);
+        $this->expectExceptionMessage('You cannot create new pages');
+        permission::require_can_create_page();
+    }
+
+    /**
+     * Test whether user can create global pages
+     */
+    public function test_can_create_global_page(): void {
+        global $DB;
+
+        $this->resetAfterTest();
+
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+
+        $this->assertFalse(permission::can_create_global_page());
+
+        $userrole = $DB->get_field('role', 'id', ['shortname' => 'user']);
+
+        // Being able to edit a tenant page does not allow a user to create global pages.
+        assign_capability('tool/custompage:edit', CAP_ALLOW, $userrole, context_system::instance());
+        $this->assertFalse(permission::can_create_global_page());
+
+        // Now they can.
+        assign_capability('tool/custompage:editall', CAP_ALLOW, $userrole, context_system::instance());
+        $this->assertTrue(permission::can_create_global_page());
+    }
+
+    /**
+     * Test that exception is thrown when a user cannot create global pages
+     */
+    public function test_require_can_create_global_page(): void {
+        $this->expectException(permission_exception::class);
+        $this->expectExceptionMessage('You cannot create new pages');
+        permission::require_can_create_global_page();
+    }
+
+    /**
+     * Test whether user can edit pages in their own tenant
+     */
+    public function test_can_edit_page_tenant(): void {
+        $this->resetAfterTest();
+
+        /** @var tool_tenant_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_tenant');
+
+        $tenant = $generator->create_tenant();
+        tenancy::set_switched_tenant_id($tenant->id);
+
+        $user = $generator->create_user(['tenantid' => $tenant->id, 'tenantadmin' => true]);
+        $this->setUser($user);
+
+        /** @var tool_custompage_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_custompage');
+        $page = $generator->create_page(['name' => 'Tenant page', 'weight' => 1, 'global' => 0]);
+
+        $this->assertTrue(permission::can_edit_page($page));
+    }
+
+    /**
+     * Test whether user can edit pages in other tenants
+     */
+    public function test_can_edit_page_other_tenant(): void {
+        $this->resetAfterTest();
+
+        /** @var tool_tenant_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_tenant');
+        $user = $generator->create_user(['tenantadmin' => true]);
+
+        // Set admin user, create page in other tenant.
+        $this->setAdminUser();
+        $tenant = $generator->create_tenant();
+
+        /** @var tool_custompage_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_custompage');
+        $page = $generator->create_page(['name' => 'Tenant page', 'weight' => 1, 'global' => 0, 'tenantid' => $tenant->id]);
+
+        // Switch back to tenant admin.
+        $this->setUser($user);
+        $this->assertFalse(permission::can_edit_page($page));
+    }
+
+    /**
+     * Test whether user can edit pages that are global
+     */
+    public function test_can_edit_page_global(): void {
+        $this->resetAfterTest();
+
+        /** @var tool_tenant_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_tenant');
+
+        $tenant = $generator->create_tenant();
+        $user = $generator->create_user(['tenantid' => $tenant->id, 'tenantadmin' => true]);
+        $this->setUser($user);
+
+        /** @var tool_custompage_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_custompage');
+        $page = $generator->create_page(['name' => 'Tenant page', 'weight' => 1, 'global' => 1]);
+
+        $this->assertFalse(permission::can_edit_page($page));
+
+        $this->setAdminUser();
+        $this->assertTrue(permission::can_edit_page($page));
+    }
+
+    /**
+     * Test that exception is thrown when a user cannot edit pages
+     */
+    public function test_require_can_edit_page(): void {
+        $this->resetAfterTest();
+
+        /** @var tool_custompage_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_custompage');
+        $page = $generator->create_page((object) ['name' => 'My page', 'weight' => 0]);
+
+        $this->expectException(permission_exception::class);
+        $this->expectExceptionMessage('You cannot edit this page');
+        permission::require_can_edit_page($page);
+    }
+
+    /**
+     * Test whether user can preview page in their own tenant
+     */
+    public function test_can_preview_page_tenant(): void {
+        $this->resetAfterTest();
+
+        /** @var tool_tenant_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_tenant');
+
+        $tenant = $generator->create_tenant();
+        tenancy::set_switched_tenant_id($tenant->id);
+
+        $user = $generator->create_user(['tenantid' => $tenant->id, 'tenantadmin' => true]);
+        $this->setUser($user);
+
+        /** @var tool_custompage_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_custompage');
+        $page = $generator->create_page(['name' => 'Tenant page', 'weight' => 1, 'global' => 0]);
+
+        $this->assertTrue(permission::can_preview_page($page));
+    }
+
+    /**
+     * Test whether user can preview page in other tenants
+     */
+    public function test_can_preview_page_other_tenant(): void {
+        $this->resetAfterTest();
+
+        /** @var tool_tenant_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_tenant');
+        $user = $generator->create_user(['tenantadmin' => true]);
+
+        // Set admin user, create page in other tenant.
+        $this->setAdminUser();
+        $tenant = $generator->create_tenant();
+
+        /** @var tool_custompage_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_custompage');
+        $page = $generator->create_page(['name' => 'Tenant page', 'weight' => 1, 'global' => 0, 'tenantid' => $tenant->id]);
+
+        // Switch back to tenant admin.
+        $this->setUser($user);
+        $this->assertFalse(permission::can_preview_page($page));
+    }
+
+    /**
+     * Test whether user can preview pages that are global
+     */
+    public function test_can_preview_page_global(): void {
+        $this->resetAfterTest();
+
+        /** @var tool_tenant_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_tenant');
+
+        $tenant = $generator->create_tenant();
+        $tenantadmin = $generator->create_user(['tenantid' => $tenant->id, 'tenantadmin' => true]);
+        $tenantuser = $generator->create_user(['tenantid' => $tenant->id]);
+        $this->setUser($tenantadmin);
+
+        /** @var tool_custompage_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_custompage');
+        $page = $generator->create_page(['name' => 'Tenant page', 'weight' => 1, 'global' => 1]);
+
+        $this->assertFalse(permission::can_preview_page($page));
+
+        // Add user to the audience.
+        $generator->create_audience(['pageid' => $page->get('id'), 'configdata' => []]);
+        $this->assertTrue(permission::can_preview_page($page));
+
+        // Switch back to non tenant admin.
+        $this->setUser($tenantuser);
+        $this->assertFalse(permission::can_preview_page($page));
+
+        $this->setAdminUser();
+        $this->assertTrue(permission::can_preview_page($page));
+    }
+
+    /**
+     * Test that exception is thrown when a user cannot preview page
+     */
+    public function test_require_can_preview_page(): void {
+        $this->resetAfterTest();
+
+        /** @var tool_custompage_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_custompage');
+        $page = $generator->create_page((object) ['name' => 'My page', 'weight' => 0]);
+
+        $this->expectException(permission_exception::class);
+        $this->expectExceptionMessage('You cannot preview this page');
+        permission::require_can_preview_page($page);
+    }
+
+    /**
+     * Test whether user can view page
+     */
+    public function test_can_view_page(): void {
+        $this->resetAfterTest();
+
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+
+        /** @var tool_custompage_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_custompage');
+
+        $pageone = $generator->create_page(['name' => 'First page', 'weight' => 0]);
+        $pagetwo = $generator->create_page(['name' => 'Second page', 'weight' => 1]);
+
+        $generator->create_audience(['pageid' => $pageone->get('id'), 'configdata' => []]);
+
+        $this->assertTrue(permission::can_view_page($pageone));
+        $this->assertFalse(permission::can_view_page($pagetwo));
+    }
+
+    /**
+     * Test whether user can view page as someone who can edit it
+     */
+    public function test_can_view_page_as_editor(): void {
+        $this->resetAfterTest();
+
+        /** @var tool_tenant_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_tenant');
+
+        $tenant = $generator->create_tenant();
+        tenancy::set_switched_tenant_id($tenant->id);
+
+        $user = $generator->create_user(['tenantid' => $tenant->id, 'tenantadmin' => true]);
+        $this->setUser($user);
+
+        /** @var tool_custompage_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_custompage');
+        $page = $generator->create_page(['name' => 'Tenant page', 'weight' => 1, 'global' => 0]);
+
+        $this->assertTrue(permission::can_view_page($page));
+    }
+
+    /**
+     * Test that exception is thrown when a user cannot view page
+     */
+    public function test_require_can_view_page(): void {
+        $this->resetAfterTest();
+
+        /** @var tool_custompage_generator $generator */
+        $generator = $this->getDataGenerator()->get_plugin_generator('tool_custompage');
+        $page = $generator->create_page((object) ['name' => 'My page', 'weight' => 0]);
+
+        $this->expectException(permission_exception::class);
+        $this->expectExceptionMessage('You cannot view this page');
+        permission::require_can_view_page($page);
+    }
+
+    /**
+     * Test whether user can duplicate blocks without validation
+     */
+    public function test_can_duplicate_block_without_validation(): void {
+        global $DB;
+        $this->resetAfterTest();
+
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+
+        $this->assertFalse(permission::can_duplicate_block_without_validation((int)$user->id));
+
+        $userrole = $DB->get_field('role', 'id', ['shortname' => 'user']);
+
+        // Assign skipblockvalidation capability to allow user can duplicate blocks without validation.
+        assign_capability('tool/custompage:skipblockvalidation', CAP_ALLOW, $userrole, context_system::instance());
+        $this->assertTrue(permission::can_duplicate_block_without_validation((int)$user->id));
+    }
+}
