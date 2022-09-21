085ce1b4f45d8436569534d32c7c5311594162a2
Praj Basnet <praj.basnet@gmail.com>
2022-09-21 12:17:46 +1000

    blocks/nps_survey v2022091500

--- blocks/nps_survey/lang/en/block_nps_survey.php
+++ blocks/nps_survey/lang/en/block_nps_survey.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * English Language strings.
+ * NPS Survey block Language pack.
  *
  * @package    block_nps_survey
  * @copyright  MoodleCloud Team
@@ -26,7 +26,13 @@ $string['pluginname'] = 'NPS';
 $string['blocktitle'] = 'Your feedback matters';
 
 // Plugin Settings.
+$string['surveytitle'] = 'Survey Title';
+$string['surveytitleexplanation'] = 'Enter the survey title.';
 $string['surveytext'] = 'Survey Text';
 $string['surveytextexplanation'] = 'Enter explanation text for the survey here.';
 $string['surveylink'] = 'Survey Link';
 $string['surveylinkexplanation'] = 'Enter the URL for the NPS survey here.';
+$string['surveyopendate'] = 'Survey Opening Date';
+$string['surveyopendateexplanation'] = 'Enter the opening date for the NPS survey here.';
+$string['surveyclosedate'] = 'Survey Closing Date';
+$string['surveyclosedateexplanation'] = 'Enter the closing date for the NPS survey here.';
