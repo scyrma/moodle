1e72c065a1949388987ffb48fe51e6d3b7035f44
Moodle Workplace <workplace@moodle.com>
2022-11-15 12:07:37 +0000

    Moodle Workplace plugin block_mylearning (build 108519)

---
+++ blocks/mylearning/version.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * Version details for the My learning block.
+ *
+ * @package    block_mylearning
+ * @author     Mikel Martín <mikel@moodle.com>
+ * @copyright  2018 Moodle Pty Ltd <support@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component    = 'block_mylearning';
+$plugin->release      = '3.11.11';
+$plugin->version      = 2022031650;
+$plugin->requires     = 2021051711.00;
+$plugin->maturity     = MATURITY_STABLE;
+$plugin->dependencies = [
+    'tool_program' => 2022031650,
+];
