03e80b25ccbe0bde34a100a67c59e1c1dcb1446f
Moodle Workplace <workplace@moodle.com>
2022-11-15 12:07:37 +0000

    Moodle Workplace plugin tool_organisation (build 108519)

---
+++ admin/tool/organisation/classes/department.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * Class department
+ *
+ * @package     tool_organisation
+ * @copyright   2018 Moodle Pty Ltd <support@moodle.com>
+ * @author      2018 Marina Glancy
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace tool_organisation;
+
+use core\output\inplace_editable;
+
+/**
+ * Class department
+ *
+ * @package     tool_organisation
+ * @copyright   2018 Moodle Pty Ltd <support@moodle.com>
+ * @author      2018 Marina Glancy
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class department extends hierarchy {
+
+    /** The table name. */
+    const TABLE = 'tool_organisation_department';
+
+    /**
+     * Generates inplace_editable object for the name
+     *
+     * @return inplace_editable
+     */
+    public function get_editable_name() : inplace_editable {
+        return new \core\output\inplace_editable(
+            'tool_organisation',
+            'department_name',
+            $this->get('id'),
+            permission::can_create_department($this),
+            $this->get_formatted_name(),
+            $this->get('name'),
+            get_string('editdepartmentname', 'tool_organisation'),
+            get_string('newnamefor', 'tool_organisation', $this->get_formatted_name())
+        );
+    }
+}
+
