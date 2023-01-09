5d9ea157077b928a1321ce389c197b9377313ad8
Moodle Workplace <workplace@moodle.com>
2023-01-09 10:05:47 +0000

    Moodle Workplace plugin tool_wp (build 112198)

--- admin/tool/wp/version.php
+++ admin/tool/wp/version.php
@@ -38,7 +38,7 @@ defined('MOODLE_INTERNAL') || die();
 
 // This plugin is part of Moodle Workplace product.
 $plugin->component    = 'tool_wp';
-$plugin->release      = '4.1beta (Build: 20221216)';
-$plugin->version      = 2022121600;
+$plugin->release      = '4.1beta (Build: 20230109)';
+$plugin->version      = 2023010900;
 $plugin->requires     = 2022112800.00;
 $plugin->maturity     = MATURITY_STABLE;
