524e6cf5d1c76c0c915a7157fb8be9c92cd98823
Moodle Workplace <workplace@moodle.com>
2023-01-11 11:33:10 +0000

    Moodle Workplace plugin tool_custompage (build 112449)

--- admin/tool/custompage/CHANGELOG.md
+++ admin/tool/custompage/CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 4.1 - 2023-01-11
 ### Changed
 - The following local helper methods have been deprecated, their implementation moved to exporters:
   `audience::get_all_audiences_menu_types` -> `page_audience_cards_exporter`
--- admin/tool/custompage/version.php
+++ admin/tool/custompage/version.php
@@ -38,10 +38,10 @@ defined('MOODLE_INTERNAL') || die();
 
 // This plugin is part of Moodle Workplace product.
 $plugin->component    = 'tool_custompage';
-$plugin->release      = '4.1beta';
-$plugin->version      = 2022121600;
+$plugin->release      = '4.1';
+$plugin->version      = 2023011100;
 $plugin->requires     = 2022112800.00;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [
-    'tool_tenant'     => 2022121600,
+    'tool_tenant'     => 2023011100,
 ];
