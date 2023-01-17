bd284c5a4908f0078f6abc1e2d481f6aae42370b
Moodle Workplace <workplace@moodle.com>
2023-01-17 16:27:42 +0000

    Moodle Workplace plugin theme_workplace (build 113561)

---
+++ theme/workplace/classes/api.php
@@ -0,0 +1,117 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * File for API for theme workplace.
+ *
+ * @package   theme_workplace
+ * @copyright 2019 Moodle Pty Ltd <support@moodle.com>
+ * @author    2019 David Matamoros <davidmc@moodle.com>
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace theme_workplace;
+
+/**
+ * Class API for theme workplace.
+ *
+ * @package   theme_workplace
+ * @copyright 2019 Moodle Pty Ltd <support@moodle.com>
+ * @author    2019 David Matamoros <davidmc@moodle.com>
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class api {
+
+    /**
+     * Returns the list of enrolled courses for the current user.
+     *
+     * Course list is ordered by enddate, which is either course end date or enrolment end date,
+     * whichever is lower. If more than one enrolment methods used for the same user, the one with
+     * the latest date is used.
+     *
+     * @return array
+     * @throws \dml_exception
+     * @deprecated
+     */
+    public static function get_enrolled_courses_for_current_user_by_lowest_enddate(): array {
+        global $DB, $USER;
+        debugging('This function is deprecated.', DEBUG_DEVELOPER);
+
+        $params = [];
+
+        // Finds all active enrolments per course and the maximum enrolment end date.
+        $enrolsql = "SELECT e.courseid, MAX(CASE WHEN ue.timeend = 0 THEN 9999999999 ELSE ue.timeend END) AS timeend
+                  FROM {enrol} e
+                  JOIN {user_enrolments} ue ON ue.enrolid = e.id AND ue.userid = :userid
+                 WHERE ue.status = :active
+                   AND e.status = :enabled
+                   AND ue.timestart < :now1
+                   AND (ue.timeend = 0 OR ue.timeend > :now2)
+              GROUP BY e.courseid";
+
+        $params['userid'] = $USER->id;
+        $params['active'] = ENROL_USER_ACTIVE;
+        $params['enabled'] = ENROL_INSTANCE_ENABLED;
+        $params['now1'] = round(time(), -2);
+        $params['now2'] = $params['now1'];
+
+        // Finds all courses where user is enrolled with the enddate = closest of course enddate and enrolments timeend.
+        // Show courses with enddate first.
+        $contextsql = \context_helper::get_preload_record_columns_sql('ctx');
+        $sql = "SELECT course.id, course.fullname, course.shortname, course.category, course.summary,
+                course.summaryformat, course.idnumber, course.startdate, course.visible, course.sortorder,
+                (CASE WHEN course.enddate > 0 AND course.enddate < enrol.timeend THEN course.enddate
+                      WHEN enrol.timeend = 9999999999 THEN 0
+                      ELSE enrol.timeend END) AS enddate,
+                $contextsql
+                FROM ( $enrolsql ) enrol
+                JOIN {course} course ON course.id = enrol.courseid
+                JOIN {context} ctx ON ctx.contextlevel = :coursecontextlevel AND ctx.instanceid = course.id
+            ORDER BY
+          (CASE WHEN course.enddate > 0 AND course.enddate < enrol.timeend
+                THEN course.enddate
+                ELSE enrol.timeend END), course.sortorder, course.id";
+
+        $params['coursecontextlevel'] = CONTEXT_COURSE;
+
+        $courses = $DB->get_records_sql($sql, $params, 0, COURSE_DB_QUERY_LIMIT);
+
+        // Filter out courses that user can't view.
+        foreach ($courses as $course) {
+            if (!$course->visible) {
+                \context_helper::preload_from_record($course);
+                $context = \context_course::instance($course->id);
+                if (!has_capability('moodle/course:viewhiddencourses', $context)) {
+                    unset($courses[$course->id]);
+                    continue;
+                }
+            }
+        }
+
+        return $courses;
+    }
+}
---
+++ theme/workplace/tests/behat/behat_theme_workplace_behat_navigation.php
@@ -0,0 +1,120 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * Navigation step definition overrides for the workplace theme.
+ *
+ * @package    theme_workplace
+ * @category   test
+ * @copyright  2019 Moodle Pty Ltd <support@moodle.com>
+ * @author     2019 Marina Glancy
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+// NOTE: No MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+require_once(__DIR__ . '/../../../../lib/tests/behat/behat_navigation.php');
+
+use Behat\Mink\Exception\ExpectationException as ExpectationException;
+use Behat\Mink\Exception\ElementNotFoundException as ElementNotFoundException;
+
+/**
+ * Step definitions and overrides to navigate through the navigation tree nodes in the workplace theme.
+ *
+ * @package    theme_workplace
+ * @category   test
+ * @copyright  2019 Moodle Pty Ltd <support@moodle.com>
+ * @author     2019 Marina Glancy
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class behat_theme_workplace_behat_navigation extends behat_navigation {
+
+    /**
+     * Navigate to an item within the site administration menu.
+     *
+     * @throws ExpectationException
+     * @param string $nodetext The navigation node/path to follow, excluding "Site administration" itself, eg "Grades > Scales"
+     * @return void
+     */
+    public function i_navigate_to_in_site_administration($nodetext) {
+        $nodelist = array_map('trim', explode('>', $nodetext));
+        if (end($nodelist) === 'Browse list of users') {
+            // Workplace theme hides the "Browse list of users" from the site administration.
+            // Navigate directly.
+            $this->execute('behat_general::i_visit', ['/admin/user.php']);
+            return;
+        }
+
+        try {
+            // Try site administration.
+            parent::i_navigate_to_in_site_administration($nodetext);
+        } catch (ElementNotFoundException $e) {
+            // Try workplace menu.
+            $this->i_navigate_to_in_workplace_launcher(end($nodelist));
+        }
+
+    }
+
+    /**
+     * Go to site administration item
+     *
+     * @Given /^I navigate to "(?P<nodetext_string>(?:[^"]|\\")*)" in workplace launcher$/
+     *
+     * @throws ExpectationException
+     * @param string $nodetext navigation node to click
+     * @return void
+     */
+    public function i_navigate_to_in_workplace_launcher($nodetext) {
+        $replacements = [
+            'Manage custom reports' => 'Report builder',
+            'Export and import' => 'Migration',
+        ];
+
+        // For backwards compatibility with plugins using different names to refer to launcher items.
+        $nodetext = $replacements[$nodetext] ?? $nodetext;
+
+        $this->execute('behat_general::i_click_on', ['#workplace-menulink', 'css_element']);
+        $this->execute('behat_general::i_click_on_in_the', [$nodetext, 'link', '.workplace-menu', 'css_element']);
+    }
+
+    /**
+     * Click on an entry in the user menu.
+     *
+     * @param string $nodetext
+     */
+    public function i_follow_in_the_user_menu($nodetext) {
+        try {
+            parent::i_follow_in_the_user_menu($nodetext);
+        } catch (ElementNotFoundException $ex) {
+            // Locate user menu entry based on "original-title" data XPath.
+            $xpath = "//a[@data-original-title='" . $this->escape($nodetext) . "']";
+            $csspath = '.usermenu .dropdown-menu';
+
+            $this->execute('behat_general::i_click_on_in_the', [$xpath, 'xpath_element', $csspath, 'css_element']);
+        }
+    }
+}
