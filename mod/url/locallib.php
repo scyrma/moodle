d4a6b90cce697f45a9a3885cc1fb8f109b6d63c4
Odei Alba <odeialba@gmail.com>
2023-01-23 13:58:09 +0100

    WP-4011 Add tenant id and id number to URL activities
    Co-authored-by: David Castro <david.castro@moodle.com>
    Co-authored-by: Odei Alba <odei.alba@moodle.com>

--- mod/url/locallib.php
+++ mod/url/locallib.php
@@ -434,6 +434,9 @@ function url_get_variable_options($config) {
         $options[get_string('roles')] = $roleoptions;
     }
 
+    /** @uses \tool_tenant\mod_url::url_get_variable_options() */
+    component_class_callback('tool_tenant\mod_url', 'url_get_variable_options', [$config, &$options]);
+
     return $options;
 }
 
@@ -502,6 +505,9 @@ function url_get_variable_values($url, $cm, $course, $config) {
             $values['course'.$role->shortname] = $role->localname;
         }
     }
+
+    /** @uses \tool_tenant\mod_url::url_get_variable_values() */
+    component_class_callback('tool_tenant\mod_url', 'url_get_variable_values', [$config, &$values]);
 
     return $values;
 }
