60ea51b4c805432cab02ea580923ba97606a6124
Moodle Workplace <workplace@moodle.com>
2023-03-14 14:31:48 +0000

    Moodle Workplace plugin theme_workplace (build 118708)

---
+++ theme/workplace/tests/theme_test.php
@@ -0,0 +1,65 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * Tests for theme_workplace.
+ *
+ * @package    theme_workplace
+ * @category   test
+ * @copyright  2018 Moodle Pty Ltd <support@moodle.com>
+ * @author     2018 Marina Glancy
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace theme_workplace;
+
+use advanced_testcase;
+
+/**
+ * Unit tests for theme_workplace
+ *
+ * @package    theme_workplace
+ * @group      theme_workplace
+ * @copyright  2018 Moodle Pty Ltd <support@moodle.com>
+ * @author     2018 Marina Glancy
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class theme_test extends advanced_testcase {
+
+    /**
+     * Test for theme_workplace_page_init().
+     */
+    public function test_page_init() {
+        $this->resetAfterTest();
+
+        // We need a $USER->id for the programs overview.
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+        $wp = new \theme_workplace\workplace();
+        $this->assertTrue(is_object($wp->dashboard()));
+    }
+}
