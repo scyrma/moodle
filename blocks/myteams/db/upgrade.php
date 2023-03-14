e47b223fac1ce810ea16b652a276eff1c23cd850
Moodle Workplace <workplace@moodle.com>
2023-03-14 15:12:02 +0000

    Moodle Workplace plugin block_myteams (build 118735)

---
+++ blocks/myteams/db/upgrade.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * Upgrade script for block_myteams
+ *
+ * @package   block_myteams
+ * @copyright 2022 Moodle Pty Ltd <support@moodle.com>
+ * @author    2022 Marina Glancy
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+/**
+ * Upgrade
+ *
+ * @param int $oldversion
+ * @return bool
+ */
+function xmldb_block_myteams_upgrade(int $oldversion) {
+    global $DB;
+
+    if ($oldversion < 2022070701) {
+
+        $record = $DB->get_record('block', ['name' => 'myteams']);
+        if ($record && !$record->visible) {
+            $DB->set_field('block', 'visible', 1, ['name' => 'myteams']);
+            add_to_config_log('block_visibility', 0, 1, 'myteams');
+        }
+
+        // Myteams block savepoint reached.
+        upgrade_plugin_savepoint(true, 2022070701, 'block', 'myteams');
+    }
+
+    return true;
+}
