6a380498fb983b7721698ad933a6397ff9ffba17
Moodle Workplace <workplace@moodle.com>
2023-03-14 15:45:25 +0000

    Moodle Workplace plugin tool_dynamicrule (build 118737)

---
+++ admin/tool/dynamicrule/classes/tool_dynamicrule/outcome/cohort.php
@@ -0,0 +1,70 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+namespace tool_dynamicrule\tool_dynamicrule\outcome;
+
+/**
+ * Add to cohort DR outcome
+ *
+ * @package    tool_dynamicrule
+ * @copyright  2020 Moodle Pty Ltd <support@moodle.com>
+ * @author     2020 David Matamoros <davidmc@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class cohort extends cohort_base {
+
+    /**
+     * Returns the title of the outcome
+     *
+     * @return string The title as formated string
+     */
+    public function get_title(): string {
+        return get_string('outcomecohort', 'tool_dynamicrule');
+    }
+
+    /**
+     * Apply this outcome to a given user.
+     *
+     * @param \stdClass $user The user object to apply the outcome to
+     */
+    public function apply_to_user(\stdClass $user): void {
+        global $CFG;
+        require_once($CFG->dirroot.'/cohort/lib.php');
+
+        $cohortid = $this->get_cohortid();
+        cohort_add_member($cohortid, $user->id);
+    }
+
+    /**
+     * Return the description for the outcome.
+     *
+     * @return string
+     */
+    public function get_description(): string {
+        return get_string('outcomecohortdescription', 'tool_dynamicrule', $this->get_cohort_name());
+    }
+}
