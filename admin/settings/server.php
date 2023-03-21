e3aed24da0d0c95524f584ed4f771ed539814bae
David Matamoros <davidmc@moodle.com>
2023-03-21 13:12:28 +0100

    MDL-77370 admin: Remove and use strings from tool_tenant

--- admin/settings/server.php
+++ admin/settings/server.php
@@ -69,8 +69,8 @@ if ($hassiteconfig) {
             CONTACT_SUPPORT_DISABLED => new lang_string('disabled', 'admin'),
         ]
     ));
-    $temp->add(new admin_setting_configtext('servicespage', new lang_string('servicespage', 'admin'),
-        new lang_string('configservicespage', 'admin'), '', PARAM_URL));
+    $temp->add(new admin_setting_configtext('servicespage', new lang_string('core_admin:servicespage', 'tool_tenant'),
+        new lang_string('core_admin:configservicespage', 'tool_tenant'), '', PARAM_URL));
 
     $ADMIN->add('server', $temp);
 
