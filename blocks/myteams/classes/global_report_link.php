be45c294439a9a878f087e7ff3edd83d9a22cbb2
Moodle Workplace <workplace@moodle.com>
2023-05-30 18:44:57 +0000

    Moodle Workplace plugin block_myteams (build 126500)

---
+++ blocks/myteams/classes/global_report_link.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the discrete and self-executable
+// collection of software scripts (plugins and modifications, and any
+// derivations thereof) that are exclusively owned and licensed by
+// Moodle Pty Ltd (Moodle) under the terms of its proprietary Moodle
+// Workplace License ("MWL") made available with Moodle's open software
+// package ("Moodle LMS") offering which itself is freely downloadable
+// at "download.moodle.org" and which is provided by Moodle under a
+// single GNU General Public License version 3.0, dated 29 June 2007
+// ("GPL"). MWL is strictly controlled by Moodle Pty Ltd and its Moodle
+// Certified Premium Partners. Wherever conflicting terms exist, the
+// terms of the MWL shall prevail.
+
+declare(strict_types=1);
+
+namespace block_myteams;
+
+use moodle_url;
+
+/**
+ * Class for global report links
+ *
+ * @package     block_myteams
+ * @copyright   2023 Moodle Pty Ltd <support@moodle.com>
+ * @author      2023 Mikel Martín <mikel@moodle.com>
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class global_report_link {
+
+    /** @var string $title */
+    private $title;
+
+    /** @var moodle_url $url */
+    private $url;
+
+    /**
+     * Constructor.
+     *
+     * @param string $title
+     * @param moodle_url $url
+     */
+    public function __construct(string $title, moodle_url $url) {
+        $this->title = $title;
+        $this->url = $url;
+    }
+
+    /**
+     * Get section name
+     *
+     * @return string
+     */
+    public function get_title(): string {
+        return $this->title;
+    }
+
+    /**
+     * Get section link
+     *
+     * @return moodle_url
+     */
+    public function get_url(): moodle_url {
+        return $this->url;
+    }
+}
---
+++ blocks/myteams/classes/permission.php
@@ -0,0 +1,84 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the discrete and self-executable
+// collection of software scripts (plugins and modifications, and any
+// derivations thereof) that are exclusively owned and licensed by
+// Moodle Pty Ltd (Moodle) under the terms of its proprietary Moodle
+// Workplace License ("MWL") made available with Moodle's open software
+// package ("Moodle LMS") offering which itself is freely downloadable
+// at "download.moodle.org" and which is provided by Moodle under a
+// single GNU General Public License version 3.0, dated 29 June 2007
+// ("GPL"). MWL is strictly controlled by Moodle Pty Ltd and its Moodle
+// Certified Premium Partners. Wherever conflicting terms exist, the
+// terms of the MWL shall prevail.
+
+declare(strict_types=1);
+
+namespace block_myteams;
+
+use core_user;
+use moodle_exception;
+use tool_organisation\organisation;
+use tool_organisation\permission as org_permission;
+use tool_tenant\tenancy;
+
+/**
+ * Block myteams permission class.
+ *
+ * @package    block_myteams
+ * @copyright  2022 Moodle Pty Ltd <support@moodle.com>
+ * @author     2022 Carlos Castillo <carlos.castillo@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class permission {
+
+    /**
+     * Checks if current user can view course progress for given userid or for their team.
+     *
+     * This report is only shown in the "My teams" block, so we need to check if the user is a manager
+     * or a manager over the given user.
+     *
+     * For simplicity, we do not allow, say, global admin, to view the report if they are
+     * not an org manager (because we don't currently display a link anywhere else).
+     *
+     * @param int|null $userid if not specified, checks if current user can view course progress for their team
+     * @return bool
+     */
+    public static function can_view_course_progress(?int $userid = null): bool {
+        $userwithjobs = organisation::get_user_with_jobs();
+        if (!$userwithjobs) {
+            return false;
+        }
+        if ($userid) {
+            return $userwithjobs->is_manager_over_user($userid, organisation::PERM_VIEW_REPORTS);
+        } else {
+            return $userwithjobs->is_manager(organisation::PERM_VIEW_REPORTS);
+        }
+    }
+
+    /**
+     * Require that current user can view course progress for given userid.
+     *
+     * @param int|null $userid
+     * @throws moodle_exception
+     */
+    public static function require_can_view_course_progress(?int $userid = null): void {
+        if (!self::can_view_course_progress($userid)) {
+            throw new moodle_exception('errornopermissionviewreports', 'block_myteams');
+        }
+    }
+}
