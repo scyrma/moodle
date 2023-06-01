b05e4495e4f9eb731f798d40383df09bcd017703
Mathieu Petit-Clair <mathieu@moodle.com>
2023-06-01 11:11:47 +0800

    MC-6361: Patch aws-autoloader with instructions from b4ef9a6.

--- local/aws/sdk/aws-autoloader.php
+++ local/aws/sdk/aws-autoloader.php
@@ -2702,7 +2702,7 @@ $mapping = array(
     'GuzzleHttp\TransferStats' => __DIR__ . '/GuzzleHttp/TransferStats.php',
     'GuzzleHttp\UriTemplate' => __DIR__ . '/GuzzleHttp/UriTemplate.php',
     'GuzzleHttp\Utils' => __DIR__ . '/GuzzleHttp/Utils.php',
-    'GuzzleHttp\functions' => __DIR__ . '/GuzzleHttp/functions.php',
+    'GuzzleHttp\functions' => __DIR__ . '/GuzzleHttp/functions_include.php',
     'GuzzleHttp\functions_include' => __DIR__ . '/GuzzleHttp/functions_include.php',
     'GuzzleHttp\Psr7\AppendStream' => __DIR__ . '/GuzzleHttp/Psr7/AppendStream.php',
     'GuzzleHttp\Psr7\BufferStream' => __DIR__ . '/GuzzleHttp/Psr7/BufferStream.php',
@@ -2733,7 +2733,7 @@ $mapping = array(
     'GuzzleHttp\Psr7\UriNormalizer' => __DIR__ . '/GuzzleHttp/Psr7/UriNormalizer.php',
     'GuzzleHttp\Psr7\UriResolver' => __DIR__ . '/GuzzleHttp/Psr7/UriResolver.php',
     'GuzzleHttp\Psr7\Utils' => __DIR__ . '/GuzzleHttp/Psr7/Utils.php',
-    'GuzzleHttp\Psr7\functions' => __DIR__ . '/GuzzleHttp/Psr7/functions.php',
+    'GuzzleHttp\Psr7\functions' => __DIR__ . '/GuzzleHttp/Psr7/functions_include.php',
     'GuzzleHttp\Psr7\functions_include' => __DIR__ . '/GuzzleHttp/Psr7/functions_include.php',
     'GuzzleHttp\Promise\AggregateException' => __DIR__ . '/GuzzleHttp/Promise/AggregateException.php',
     'GuzzleHttp\Promise\CancellationException' => __DIR__ . '/GuzzleHttp/Promise/CancellationException.php',
@@ -2751,7 +2751,7 @@ $mapping = array(
     'GuzzleHttp\Promise\TaskQueue' => __DIR__ . '/GuzzleHttp/Promise/TaskQueue.php',
     'GuzzleHttp\Promise\TaskQueueInterface' => __DIR__ . '/GuzzleHttp/Promise/TaskQueueInterface.php',
     'GuzzleHttp\Promise\Utils' => __DIR__ . '/GuzzleHttp/Promise/Utils.php',
-    'GuzzleHttp\Promise\functions' => __DIR__ . '/GuzzleHttp/Promise/functions.php',
+    'GuzzleHttp\Promise\functions' => __DIR__ . '/GuzzleHttp/Promise/functions_include.php',
     'GuzzleHttp\Promise\functions_include' => __DIR__ . '/GuzzleHttp/Promise/functions_include.php',
     'Psr\Http\Message\MessageInterface' => __DIR__ . '/Psr/Http/Message/MessageInterface.php',
     'Psr\Http\Message\RequestInterface' => __DIR__ . '/Psr/Http/Message/RequestInterface.php',
@@ -2776,13 +2776,13 @@ $mapping = array(
 
 spl_autoload_register(function ($class) use ($mapping) {
     if (isset($mapping[$class])) {
-        require $mapping[$class];
+        require_once $mapping[$class];
     }
 }, true);
 
-require __DIR__ . '/Aws/functions.php';
-require __DIR__ . '/GuzzleHttp/functions_include.php';
-require __DIR__ . '/GuzzleHttp/Psr7/functions_include.php';
-require __DIR__ . '/GuzzleHttp/Promise/functions_include.php';
-require __DIR__ . '/JmesPath/JmesPath.php';
-require __DIR__ . '/Symfony/Polyfill/Intl/Idn/bootstrap.php';
+require_once __DIR__ . '/Aws/functions.php';
+require_once __DIR__ . '/GuzzleHttp/functions_include.php';
+require_once __DIR__ . '/GuzzleHttp/Psr7/functions_include.php';
+require_once __DIR__ . '/GuzzleHttp/Promise/functions_include.php';
+require_once __DIR__ . '/JmesPath/JmesPath.php';
+require_once __DIR__ . '/Symfony/Polyfill/Intl/Idn/bootstrap.php';
