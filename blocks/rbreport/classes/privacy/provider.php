0a854d4aea859b073fe07c1735e17a2e81dfcdca
Moodle Workplace <workplace@moodle.com>
2023-06-13 12:47:28 +0000

    Moodle Workplace plugin block_rbreport (build 128299)

---
+++ blocks/rbreport/classes/privacy/provider.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of the block_rbreport plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace block_rbreport\privacy;
+
+/**
+ * Privacy Subsystem implementation for block_rbreport.
+ *
+ * @package    block_rbreport
+ * @copyright  2021 Moodle Pty Ltd <support@moodle.com>
+ * @author     2021 Marina Glancy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
+
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason() : string {
+        return 'privacy:metadata:block';
+    }
+}
