b273e240b9f34f0440c43448d8f90653b2d64ba5
Moodle Workplace <workplace@moodle.com>
2023-06-13 13:06:47 +0000

    Moodle Workplace plugin enrol_program (build 128303)

---
+++ enrol/program/classes/empty_form.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the discrete and self-executable
+// collection of software scripts (plugins and modifications, and any
+// derivations thereof) that are exclusively owned and licensed by
+// Moodle Pty Ltd (Moodle) under the terms of its proprietary Moodle
+// Workplace License ("MWL") made available with Moodle's open software
+// package ("Moodle LMS") offering which itself is freely downloadable
+// at "download.moodle.org" and which is provided by Moodle under a
+// single GNU General Public License version 3.0, dated 29 June 2007
+// ("GPL"). MWL is strictly controlled by Moodle Pty Ltd and its Moodle
+// Certified Premium Partners. Wherever conflicting terms exist, the
+// terms of the MWL shall prevail.
+
+/**
+ * Empty enrol_self form. Useful to mimic valid enrol instances UI when the enrolment instance is not available.
+ *
+ * @package   enrol_program
+ * @copyright 2018 Moodle Pty Ltd <support@moodle.com>
+ * @author    2018 Workplace team
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir.'/formslib.php');
+
+/**
+ * Class enrol_program_empty_form
+ *
+ * @copyright 2018 Moodle Pty Ltd <support@moodle.com>
+ * @author    2018 Workplace team
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class enrol_program_empty_form extends moodleform {
+
+    /**
+     * Form definition.
+     * @return void
+     */
+    public function definition() {
+        $this->_form->addElement('header', 'selfheader', $this->_customdata->header);
+        $this->_form->addElement('html', html_writer::div($this->_customdata->info, 'alert alert-warning m-2'));
+    }
+}
