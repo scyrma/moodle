5c3c582eec416f9129589a0add61af48271c1d82
Moodle Workplace <workplace@moodle.com>
2023-06-13 14:55:53 +0000

    Moodle Workplace plugin tool_catalogue (build 128313)

---
+++ admin/tool/catalogue/classes/external/get_user_catalogue_course.php
@@ -0,0 +1,124 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the discrete and self-executable
+// collection of software scripts (plugins and modifications, and any
+// derivations thereof) that are exclusively owned and licensed by
+// Moodle Pty Ltd (Moodle) under the terms of its proprietary Moodle
+// Workplace License ("MWL") made available with Moodle's open software
+// package ("Moodle LMS") offering which itself is freely downloadable
+// at "download.moodle.org" and which is provided by Moodle under a
+// single GNU General Public License version 3.0, dated 29 June 2007
+// ("GPL"). MWL is strictly controlled by Moodle Pty Ltd and its Moodle
+// Certified Premium Partners. Wherever conflicting terms exist, the
+// terms of the MWL shall prevail.
+
+declare(strict_types=1);
+
+namespace tool_catalogue\external;
+
+use context_course;
+use context_system;
+use core_user;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_value;
+use core_external\external_warnings;
+use tool_catalogue\output\renderer;
+use tool_catalogue\permission;
+
+/**
+ * External function get_user_catalogue_course for tool_catalogue.
+ *
+ * @package   tool_catalogue
+ * @copyright 2022 Moodle Pty Ltd <support@moodle.com>
+ * @author    2022 David Matamoros <davidmc@moodle.com>
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class get_user_catalogue_course extends external_api {
+
+    /**
+     * Describes the parameters for get_user_catalogue_course
+     *
+     * @return external_function_parameters
+     */
+    public static function execute_parameters(): external_function_parameters {
+        return new external_function_parameters([
+            'courseid' => new external_value(PARAM_INT, 'The course id'),
+            'userid' => new external_value(PARAM_INT, 'The user id', VALUE_DEFAULT, 0),
+        ]);
+    }
+
+    /**
+     * External function to get user catalogue course
+     *
+     * @param int $courseid
+     * @param int $userid
+     * @return array
+     */
+    public static function execute(int $courseid, int $userid = 0): array {
+        global $PAGE, $USER;
+
+        // Parameter validation.
+        [
+            'courseid' => $courseid,
+            'userid' => $userid,
+        ] = self::validate_parameters(self::execute_parameters(), [
+            'courseid' => $courseid,
+            'userid' => $userid,
+        ]);
+
+        // From web services we don't call require_login(), but rather validate_context.
+        // In this case we need to validate system context because we use this web service also to show course
+        // when the user is not enroled and context_course would fail here.
+        $context = context_system::instance();
+        self::validate_context($context);
+
+        // Default userid to current userid if is not set.
+        $userid = !empty($userid) ? $userid : (int) $USER->id;
+
+        // Check permissions.
+        $user = core_user::get_user($userid, '*', MUST_EXIST);
+        core_user::require_active_user($user, true);
+        $course = get_course($courseid);
+        permission::require_can_view_course_cover($userid, $course);
+
+        /** @var renderer $output */
+        $output = $PAGE->get_renderer('tool_catalogue');
+
+        $related = [
+            'context' => context_course::instance($courseid),
+            'course' => $course,
+        ];
+        $exporter = new course_exporter(null, $related);
+        return [
+            'course' => (array)$exporter->export($output),
+        ];
+    }
+
+    /**
+     * Describes the data returned from the external function.
+     *
+     * @return external_single_structure
+     */
+    public static function execute_returns(): external_single_structure {
+        return new external_single_structure([
+            'course' => course_exporter::get_read_structure(),
+            'warnings' => new external_warnings(),
+        ]);
+    }
+}
