72b005b74666ea7e90000906d97b3f3a109691a6
Moodle Workplace <workplace@moodle.com>
2023-06-13 12:47:28 +0000

    Moodle Workplace plugin block_myinprogress (build 128299)

---
+++ blocks/myinprogress/tests/behat/behat_block_myinprogress.php
@@ -0,0 +1,94 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+require_once(__DIR__ . '/../../../../lib/behat/behat_base.php');
+
+use Behat\Mink\Exception\ElementNotFoundException;
+
+/**
+ * Behat step definitions for block_myinprogress
+ *
+ * @package    block_myinprogress
+ * @category   test
+ * @copyright  2022 Moodle Pty Ltd <support@moodle.com>
+ * @author     2022 Mikel Martín <mikel@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class behat_block_myinprogress extends behat_base {
+
+    /**
+     * Hides a course carousel card
+     *
+     * @When I hide :link course card
+     * @throws ElementNotFoundException Thrown by behat_base::find
+     * @param string $coursename
+     */
+    public function i_hide_coursecarousel_course_card(string $coursename) {
+
+        $this->execute("behat_general::i_hover", [$coursename, 'block_myinprogress > Course card']);
+        $this->execute('behat_general::i_click_on_in_the',
+            [get_string('aria:courseactions', 'tool_catalogue'), 'button', $coursename, 'block_myinprogress > Course card']
+        );
+        $this->execute('behat_general::i_click_on_in_the',
+            [get_string('hidefromview', 'block_myinprogress'), 'link', $coursename, 'block_myinprogress > Course card']
+        );
+    }
+
+    /**
+     * Shows a course carousel card
+     *
+     * @When I show :link course card
+     * @throws ElementNotFoundException Thrown by behat_base::find
+     * @param string $coursename
+     */
+    public function i_show_coursecarousel_course_card(string $coursename) {
+        $this->execute("behat_general::i_hover", [$coursename, 'block_myinprogress > Course card']);
+        $this->execute('behat_general::i_click_on_in_the',
+            [get_string('aria:courseactions', 'tool_catalogue'), 'button', $coursename, 'block_myinprogress > Course card']
+        );
+        $this->execute('behat_general::i_click_on_in_the',
+            [get_string('donthide', 'block_myinprogress'), 'link', $coursename, 'block_myinprogress > Course card']
+        );
+    }
+    /**
+     * Return the list of partial named selectors.
+     *
+     * Those selectors can be used to capture course card elements. Examples:
+     *    And I should see "0% completed" in the "Course 01" "block_myinprogress > Course card"
+     *
+     * @return array
+     */
+    public static function get_partial_named_selectors(): array {
+        return [
+            new behat_component_named_selector('Course card', [
+                "//*[contains(@class, 'block_myinprogress')]//div[contains(@class, 'card dashboard-card')][contains(., %locator%)]",
+            ]),
+        ];
+    }
+}
