6f0b5f36356882986e16c5cf76e47d0f0485cd7b
Moodle Workplace <workplace@moodle.com>
2023-06-13 12:22:37 +0000

    Moodle Workplace plugin tool_dynamicrule (build 128289)

---
+++ admin/tool/dynamicrule/classes/task/process_rules.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+/**
+ * Dynamic rules cron task.
+ *
+ * @package    tool_dynamicrule
+ * @copyright  2018 Moodle Pty Ltd <support@moodle.com>
+ * @author     2018 Daniel Neis Araujo <daniel@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+namespace tool_dynamicrule\task;
+
+/**
+ * This tasks process the rules
+ *
+ * @package    tool_dynamicrule
+ * @copyright  2018 Moodle Pty Ltd <support@moodle.com>
+ * @author     2018 Daniel Neis Araujo <daniel@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class process_rules extends \core\task\scheduled_task {
+
+    /**
+     * Task name.
+     */
+    public function get_name() {
+        return get_string('taskprocessrules', 'tool_dynamicrule');
+    }
+
+    /**
+     * Delete all expired items.
+     */
+    public function execute() {
+        \tool_dynamicrule\api::process_rules();
+    }
+}
