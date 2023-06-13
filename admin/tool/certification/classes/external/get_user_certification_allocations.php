c583d97a8859eac53d7dbb560e579027831dd24c
Moodle Workplace <workplace@moodle.com>
2023-06-13 14:55:53 +0000

    Moodle Workplace plugin tool_certification (build 128313)

---
+++ admin/tool/certification/classes/external/get_user_certification_allocations.php
@@ -0,0 +1,97 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the discrete and self-executable
+// collection of software scripts (plugins and modifications, and any
+// derivations thereof) that are exclusively owned and licensed by
+// Moodle Pty Ltd (Moodle) under the terms of its proprietary Moodle
+// Workplace License ("MWL") made available with Moodle's open software
+// package ("Moodle LMS") offering which itself is freely downloadable
+// at "download.moodle.org" and which is provided by Moodle under a
+// single GNU General Public License version 3.0, dated 29 June 2007
+// ("GPL"). MWL is strictly controlled by Moodle Pty Ltd and its Moodle
+// Certified Premium Partners. Wherever conflicting terms exist, the
+// terms of the MWL shall prevail.
+
+namespace tool_certification\external;
+
+use core_external\external_api;
+use context_system;
+use core_external\external_function_parameters;
+use core_external\external_multiple_structure;
+use core_external\external_value;
+use tool_certification\certification_user;
+use tool_certification\permission as certificationpermission;
+
+/**
+ * Class to get all certification allocations for given user.
+ *
+ * @package    tool_certification
+ * @author     2022 Odei Alba <odei.alba@moodle.com>
+ * @copyright  2022 Moodle Pty Ltd <support@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class get_user_certification_allocations extends external_api {
+    /**
+     * Parameters for execute
+     * @return external_function_parameters
+     */
+    public static function execute_parameters(): external_function_parameters {
+        return new external_function_parameters([
+            'userid' => new external_value(PARAM_INT, 'ID of the user', VALUE_REQUIRED),
+        ]);
+    }
+
+    /**
+     * Get all current user certification allocations
+     * @param int $userid
+     * @return array
+     */
+    public static function execute(int $userid): array {
+        global $PAGE;
+        // Parameter validation.
+        [
+            'userid' => $userid,
+        ] = self::validate_parameters(self::execute_parameters(), [
+            'userid' => $userid,
+        ]);
+
+        certificationpermission::require_can_view_user_progress($userid);
+
+        // We always must call validate_context in a webservice.
+        $context = context_system::instance();
+        self::validate_context($context);
+
+        $renderer = $PAGE->get_renderer('core');
+        $certificationusers = certification_user::get_records(['userid' => $userid]);
+
+        $result = array_map(function($certificationuser) use ($renderer, $context) {
+            return (new certification_user_exporter($certificationuser, ['context' => $context]))->export($renderer);
+        }, $certificationusers);
+
+        return $result;
+    }
+
+    /**
+     * Return for execute
+     * @return external_multiple_structure
+     */
+    public static function execute_returns(): external_multiple_structure {
+        return new external_multiple_structure(
+            certification_user_exporter::get_read_structure()
+        );
+    }
+}
