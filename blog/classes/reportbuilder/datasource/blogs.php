f0ff0b542c0534f8337b5d2620ac8ac30025d7fa
David Carrillo <davidmc@moodle.com>
2023-06-27 12:24:02 +0200

    WP-4110 Add callbacks to filter datasources by tenant
    Co-authored-by: Paul Holden <paulh@moodle.com>

--- blog/classes/reportbuilder/datasource/blogs.php
+++ blog/classes/reportbuilder/datasource/blogs.php
@@ -56,6 +56,11 @@ class blogs extends datasource {
 
         $this->add_entity($blogentity);
 
+        /** @uses \tool_tenant\tenancy::get_users_subquery() */
+        $tenantsql = component_class_callback(\tool_tenant\tenancy::class, 'get_users_subquery',
+            [false, false, "{$postalias}.userid"], '1=1');
+        $this->add_base_condition_sql($tenantsql);
+
         // Join the files entity.
         $fileentity = (new file())
             ->set_entity_title(new lang_string('blogattachment', 'core_blog'));
