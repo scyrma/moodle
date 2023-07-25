fac1001afc0dfd77abf8fd5d9a7e9f2c45a9f6bd
Paul Holden <paulh@moodle.com>
2023-07-25 15:35:05 +0100

    WP-4110 restore user entity left join on custom reports.
    Changing to an inner join broke LMS tests, and also fundamentally
    changed the behaviour of the reports.

--- admin/classes/reportbuilder/datasource/task_logs.php
+++ admin/classes/reportbuilder/datasource/task_logs.php
@@ -55,7 +55,7 @@ class task_logs extends datasource {
         /** @uses \tool_tenant\tenancy::get_users_subquery() */
         $tenantsql = component_class_callback(\tool_tenant\tenancy::class, 'get_users_subquery',
             [false, false, "{$tasklogalias}.userid"], '1=1');
-        $this->add_base_condition_sql("{$tasklogalias}.userid = 0 OR ({$tenantsql})");
+        $this->add_base_condition_sql("({$tasklogalias}.userid = 0 OR {$tenantsql})");
 
         // Join the user entity to represent the associated user.
         $userentity = new user();
