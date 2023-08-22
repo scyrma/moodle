e226480b7c250e6dcc54f64d296bb086f97c047b
Moodle Workplace <workplace@moodle.com>
2023-08-22 18:19:24 +0000

    Moodle Workplace plugin theme_workplace (build 132994)

---
+++ theme/workplace/classes/output/core_reportbuilder_renderer.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the discrete and self-executable
+// collection of software scripts (plugins and modifications, and any
+// derivations thereof) that are exclusively owned and licensed by
+// Moodle Pty Ltd (Moodle) under the terms of its proprietary Moodle
+// Workplace License ("MWL") made available with Moodle's open software
+// package ("Moodle LMS") offering which itself is freely downloadable
+// at "download.moodle.org" and which is provided by Moodle under a
+// single GNU General Public License version 3.0, dated 29 June 2007
+// ("GPL"). MWL is strictly controlled by Moodle Pty Ltd and its Moodle
+// Certified Premium Partners. Wherever conflicting terms exist, the
+// terms of the MWL shall prevail.
+
+namespace theme_workplace\output;
+
+use html_writer;
+
+/**
+ * Report renderer class
+ *
+ * @package    theme_workplace
+ * @copyright  2023 Moodle Pty Ltd <support@moodle.com>
+ * @author     Pedro Jordao <pedro.jordao@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class core_reportbuilder_renderer extends \core_reportbuilder\output\renderer {
+
+    /**
+     * Renders the New report button
+     *
+     * @return string
+     */
+    public function render_new_report_button(): string {
+        $icon = $this->pix_icon('t/add', '', 'moodle', ['class' => 'mt-1']);
+        return html_writer::tag('button', $icon . get_string('newreport', 'core_reportbuilder'), [
+            'class' => 'btn btn-primary my-auto',
+            'data-action' => 'report-create',
+        ]);
+    }
+}
