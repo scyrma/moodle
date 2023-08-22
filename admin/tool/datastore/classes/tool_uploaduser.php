95c51653edb62d98afad88de0b5d0224dbd5e426
Moodle Workplace <workplace@moodle.com>
2023-08-22 18:04:08 +0000

    Moodle Workplace plugin tool_datastore (build 132990)

---
+++ admin/tool/datastore/classes/tool_uploaduser.php
@@ -0,0 +1,81 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+namespace tool_datastore;
+
+use stdClass;
+use uu_progress_tracker;
+
+/**
+ * Class tool_uploaduser
+ *
+ * This implements methods to be use on uploading users CSV files.
+ *
+ * @package     tool_datastore
+ * @copyright   2019 Moodle Pty Ltd <support@moodle.com>
+ * @author      2019 Daniel Neis <daniel@moodle.com>
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class tool_uploaduser {
+
+    /**
+     * Callback for tool_uploaduser that process updated user.
+     *
+     * @param stdClass $user
+     * @param array $filecolumns
+     * @param uu_progress_tracker $upt
+     * @param array $ccache the course cache
+     */
+    public static function process_user_after_enrol($user, $filecolumns, $upt, &$ccache) {
+        global $DB;
+        foreach ($filecolumns as $column) {
+            if (preg_match('/^coursecompleted(\d+)$/', $column, $matches)) {
+                $i = $matches[1];
+                if (empty($user->{'coursecompleted'.$i})) {
+                    continue;
+                }
+                $coursecompleted = $user->{'coursecompleted'.$i};
+                if (!array_key_exists($coursecompleted, $ccache)) {
+                    if (!$course = $DB->get_record('course', ['shortname' => $coursecompleted], 'id, shortname')) {
+                        $upt->track('enrolments', get_string('unknowncourse', 'error', s($coursecompleted)), 'error');
+                        return;
+                    }
+                    $ccache[$coursecompleted] = $course;
+                    $ccache[$coursecompleted]->groups = null;
+                }
+                if (\tool_datastore\permission::can_upload_course_completion($user, $ccache[$coursecompleted])) {
+                    if (empty($user->{'coursecompleteddate'.$i})) {
+                        $coursecompleteddate = time();
+                    } else {
+                        $coursecompleteddate = strtotime($user->{'coursecompleteddate'.$i});
+                    }
+                    \tool_datastore\helper::add_course_completion($user->id, $ccache[$coursecompleted]->id, $coursecompleteddate);
+                }
+            }
+        }
+    }
+}
