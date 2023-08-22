9c2c6b3a7f09c6a4fc5375453487d3a1dec8802e
Moodle Workplace <workplace@moodle.com>
2023-08-22 17:30:31 +0000

    Moodle Workplace plugin tool_wp (build 132988)

---
+++ admin/tool/wp/classes/output/page_header_button.php
@@ -0,0 +1,85 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+namespace tool_wp\output;
+
+use renderer_base;
+
+/**
+ * Class heading_button
+ *
+ * @package     tool_wp
+ * @copyright   2019 Moodle Pty Ltd <support@moodle.com>
+ * @author      2019 Marina Glancy
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class page_header_button implements \templatable {
+
+    /** @var string */
+    protected $title;
+    /** @var array */
+    protected $attributes;
+
+    /**
+     * heading_button constructor.
+     *
+     * @param string $title
+     * @param array $attributes
+     */
+    public function __construct(string $title, array $attributes = []) {
+        $this->title = $title;
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * Export for template
+     *
+     * @param renderer_base $output
+     * @return array|\stdClass
+     */
+    public function export_for_template(renderer_base $output) {
+        $data = ['title' => $this->title, 'attributes' => []];
+        foreach ($this->attributes as $key => $value) {
+            if ($key === 'class') {
+                $data['class'] = $value;
+            } else {
+                $data['attributes'][] = ['name' => $key, 'value' => $value];
+            }
+        }
+        return $data;
+    }
+
+    /**
+     * Renders the button
+     *
+     * @param renderer_base $output
+     * @return string
+     */
+    public function render(renderer_base $output) : string {
+        return $output->render_from_template('tool_wp/page_header_button', $this->export_for_template($output));
+    }
+}
