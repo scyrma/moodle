d15601f6576619285b56ae12461554a679f68c5c
Moodle Workplace <workplace@moodle.com>
2023-08-22 18:06:37 +0000

    Moodle Workplace plugin block_myinprogress (build 132992)

---
+++ blocks/myinprogress/classes/constants.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the discrete and self-executable
+// collection of software scripts (plugins and modifications, and any
+// derivations thereof) that are exclusively owned and licensed by
+// Moodle Pty Ltd (Moodle) under the terms of its proprietary Moodle
+// Workplace License ("MWL") made available with Moodle's open software
+// package ("Moodle LMS") offering which itself is freely downloadable
+// at "download.moodle.org" and which is provided by Moodle under a
+// single GNU General Public License version 3.0, dated 29 June 2007
+// ("GPL"). MWL is strictly controlled by Moodle Pty Ltd and its Moodle
+// Certified Premium Partners. Wherever conflicting terms exist, the
+// terms of the MWL shall prevail.
+
+namespace block_myinprogress;
+
+/**
+ * Class constants
+ *
+ * @package   block_myinprogress
+ * @copyright 2022 Moodle Pty Ltd <support@moodle.com>
+ * @author    2022 Mikel Martín <mikel@moodle.com>
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class constants {
+    /** @var string String used for the filter for visible cards */
+    public const FILTER_VISIBLE = 'nothidden';
+    /** @var string Hidden courses user preference */
+    public const PREFERENCE_HIDDEN_COURSES = 'block_myinprogress_hidden_courses';
+    /** @var string Show hidden cards user preference */
+    public const PREFERENCE_SHOW_HIDDEN_CARDS = 'block_myinprogress_show_hidden_cards';
+}
