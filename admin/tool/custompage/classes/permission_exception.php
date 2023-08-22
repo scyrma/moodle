45c14e72a0eaeb7bfc83cbb53fdcee28aa74be54
Moodle Workplace <workplace@moodle.com>
2023-08-22 17:30:30 +0000

    Moodle Workplace plugin tool_custompage (build 132988)

---
+++ admin/tool/custompage/classes/permission_exception.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+declare(strict_types=1);
+
+namespace tool_custompage;
+
+use moodle_exception;
+
+/**
+ * Exception class thrown for permission related failures
+ *
+ * @package     tool_custompage
+ * @copyright   2022 Moodle Pty Ltd <support@moodle.com>
+ * @author      2022 Paul Holden <paulh@moodle.com>
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class permission_exception extends moodle_exception {
+
+    /**
+     * Constructor
+     *
+     * @param string $errorcode Specifific error code for permission, must have accompanying string translation
+     */
+    public function __construct(string $errorcode) {
+        parent::__construct($errorcode, 'tool_custompage');
+    }
+}
