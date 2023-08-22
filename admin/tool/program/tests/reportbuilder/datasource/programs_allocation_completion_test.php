e01ee3b7f27deb624acd3b6db937f8b8f274f88b
Moodle Workplace <workplace@moodle.com>
2023-08-22 18:04:09 +0000

    Moodle Workplace plugin tool_program (build 132990)

---
+++ admin/tool/program/tests/reportbuilder/datasource/programs_allocation_completion_test.php
@@ -0,0 +1,215 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+declare(strict_types=1);
+
+namespace tool_program\reportbuilder\datasource;
+
+use core_reportbuilder_generator;
+use core_reportbuilder_testcase;
+use tool_program_generator;
+use core_reportbuilder\reportbuilder\audience\manual;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once("{$CFG->dirroot}/reportbuilder/tests/helpers.php");
+
+/**
+ * Programs allocation completion datasource tests.
+ *
+ * @covers     \tool_program\reportbuilder\datasource\programs_allocation_completion
+ * @package    tool_program
+ * @copyright  2022 Moodle Pty Ltd <support@moodle.com>
+ * @author     2022 David Matamoros <davidmc@moodle.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class programs_allocation_completion_test extends core_reportbuilder_testcase {
+
+    /** @var tool_program_generator */
+    protected $generator;
+    /** @var core_reportbuilder_generator */
+    protected $rbgenerator;
+
+    /**
+     * setUp.
+     */
+    public function setUp(): void {
+        $this->generator = self::getDataGenerator()->get_plugin_generator('tool_program');
+        $this->rbgenerator = self::getDataGenerator()->get_plugin_generator('core_reportbuilder');
+    }
+
+    /**
+     * Test programs allocation completion datasource
+     */
+    public function test_programs_allocation_completion_datasource(): void {
+        $this->resetAfterTest();
+
+        $program = $this->generator->generate_program((object)['fullname' => 'My program']);
+        $basesetid = $program->get_base_set()->get('id');
+        $course = self::getDataGenerator()->create_course(['fullname' => 'My Lionel course']);
+        $this->generator->add_course_to_set((int)$course->id, $basesetid);
+        $user1 = $this->getDataGenerator()->create_user(['firstname' => 'Lionel', 'lastname' => 'Kenobi']);
+        $this->generator->allocate_user_to_program($program->get('id'), (int)$user1->id);
+
+        $report = $this->rbgenerator->create_report([
+            'name' => 'Programs allocation completion',
+            'source' => programs_allocation_completion::class,
+            'default' => false,
+        ]);
+
+        // Add program fullname column to the report.
+        $this->rbgenerator->create_column(['reportid' => $report->get('id'), 'uniqueidentifier' => 'program:fullname']);
+        // Add program user allocation source column to the report.
+        $this->rbgenerator->create_column(['reportid' => $report->get('id'), 'uniqueidentifier' => 'program_user:allocationtype']);
+        // Add user fullname column to the report.
+        $this->rbgenerator->create_column(['reportid' => $report->get('id'), 'uniqueidentifier' => 'user:fullname']);
+
+        $content = $this->get_custom_report_content($report->get('id'));
+        $this->assertCount(1, $content);
+
+        $contentrow = array_values(reset($content));
+        $this->assertEquals([
+            'My program', // Program fullname.
+            'Manual', // Allocation type.
+            'Lionel Kenobi', // User full name.
+        ], $contentrow);
+    }
+
+    /**
+     * Test programs allocation completion datasource with reports column
+     */
+    public function test_programs_allocation_completion_reports_datasource(): void {
+        $this->resetAfterTest();
+
+        $program = $this->generator->generate_program((object)['fullname' => 'My program']);
+        $basesetid = $program->get_base_set()->get('id');
+        $course = self::getDataGenerator()->create_course(['fullname' => 'My Lionel course']);
+        $this->generator->add_course_to_set((int)$course->id, $basesetid);
+        $user1 = $this->getDataGenerator()->create_user(['firstname' => 'Lionel', 'lastname' => 'Kenobi']);
+        $user2 = $this->getDataGenerator()->create_user(['firstname' => 'Luke', 'lastname' => 'Skywalker']);
+        $this->generator->allocate_users_to_program($program->get('id'), [$user1->id, $user2->id]);
+
+        $report = $this->rbgenerator->create_report([
+            'name' => 'Programs allocation completion',
+            'source' => programs_allocation_completion::class,
+            'default' => false,
+        ]);
+
+        // Add audience.
+        $this->rbgenerator->create_audience([
+            'reportid' => $report->get('id'),
+            'classname' => manual::class,
+            'configdata' => ['users' => [$user2->id]],
+        ]);
+
+        // Add program fullname column to the report.
+        $this->rbgenerator->create_column(['reportid' => $report->get('id'), 'uniqueidentifier' => 'program:fullname']);
+        // Add user fullname column to the report.
+        $this->rbgenerator->create_column(['reportid' => $report->get('id'), 'uniqueidentifier' => 'user:fullname'])
+            ->set('sortenabled', true)
+            ->update();
+        // Add program user allocation progress column to the report.
+        $this->rbgenerator->create_column(['reportid' => $report->get('id'), 'uniqueidentifier' => 'program_user:programprogress']);
+        // Add program user allocation progress with overview column to the report.
+        $this->rbgenerator->create_column(['reportid' => $report->get('id'),
+            'uniqueidentifier' => 'program_user:programprogresswithoverview']);
+
+        // Validate as admin. Admin should see progress for all users.
+        self::setAdminUser();
+        $content = $this->get_custom_report_content($report->get('id'));
+
+        $this->assertCount(2, $content);
+        $contentrow = array_values(array_pop($content));
+        // Check program_user:programprogresswithoverview is not empty.
+        $this->assertNotEmpty(array_pop($contentrow));
+        // Check remaning columns.
+        $this->assertEquals([
+            'My program',
+            'Luke Skywalker',
+            '0%',
+        ], $contentrow);
+        $contentrow = array_values(array_pop($content));
+        // Check program_user:programprogresswithoverview is not empty.
+        $this->assertNotEmpty(array_pop($contentrow));
+        // Check remaning columns.
+        $this->assertEquals([
+            'My program',
+            'Lionel Kenobi',
+            '0%',
+        ], $contentrow);
+
+        // Validate as audience permitted user.
+        self::setUser($user2);
+        $content = $this->get_custom_report_content($report->get('id'));
+
+        $this->assertCount(2, $content);
+        // Report viewing user should show progress.
+        $contentrow = array_values(array_pop($content));
+        // Check program_user:programprogresswithoverview is not empty.
+        $this->assertNotEmpty(array_pop($contentrow));
+        // Check remaning columns.
+        $this->assertEquals([
+            'My program',
+            'Luke Skywalker',
+            '0%',
+        ], $contentrow);
+
+        // Other user should not show progress.
+        $contentrow = array_values(array_pop($content));
+        // Check program_user:programprogresswithoverview is empty.
+        $this->assertEmpty(array_pop($contentrow));
+        // Check remaning columns.
+        $this->assertEquals([
+            'My program',
+            'Lionel Kenobi',
+            '',
+        ], $contentrow);
+    }
+
+    /**
+     * Stress test datasource
+     */
+    public function test_stress_datasource(): void {
+        $this->resetAfterTest();
+        $this->setAdminUser();
+
+        $program = $this->generator->generate_program((object) ['fullname' => 'My program']);
+
+        // Add a program course.
+        $course = $this->getDataGenerator()->create_course();
+        $this->generator->add_course_to_set((int) $course->id, $program->get_base_set()->get('id'));
+
+        // Add a program user.
+        $user = $this->getDataGenerator()->create_user();
+        $this->generator->allocate_user_to_program($program->get('id'), (int) $user->id);
+
+        $this->datasource_stress_test_columns(programs_allocation_completion::class);
+        $this->datasource_stress_test_columns_aggregation(programs_allocation_completion::class);
+        $this->datasource_stress_test_conditions(programs_allocation_completion::class, 'program_user:suspended');
+    }
+}
---
+++ admin/tool/program/tests/tool_dynamicrule/condition/program_suspended_test.php
@@ -0,0 +1,291 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the collection of software scripts
+// (plugins and modifications, and any derivations thereof) that are
+// exclusively owned and licensed by Moodle under the terms of this
+// proprietary Moodle Workplace License ("MWL") alongside Moodle's open
+// software package offering which itself is freely downloadable at
+// "download.moodle.org" and which is provided by Moodle under a single
+// GNU General Public License version 3.0, dated 29 June 2007 ("GPL").
+// MWL is strictly controlled by Moodle Pty Ltd and its certified
+// premium partners. Wherever conflicting terms exist, the terms of the
+// MWL are binding and shall prevail.
+
+namespace tool_program\tool_dynamicrule\condition;
+
+use advanced_testcase;
+use tool_dynamicrule_generator;
+use tool_program_generator;
+use tool_dynamicrule\api;
+use tool_program\constants;
+use tool_dynamicrule\rule;
+use tool_tenant_generator;
+
+/**
+ * Unit tests for condition program_suspended class.
+ *
+ * @covers     \tool_program\tool_dynamicrule\condition\program_suspended
+ * @package    tool_program
+ * @group      tool_program
+ * @copyright  2019 Moodle Pty Ltd <support@moodle.com>
+ * @author     2019 Mitxel Moriana <mitxel@tresipunt.com>
+ * @license    Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+class program_suspended_test extends advanced_testcase {
+
+    /** @var tool_program_generator */
+    protected $generator;
+
+    /** @var tool_tenant_generator */
+    protected $tenantgenerator;
+
+    /** @var tool_dynamicrule_generator */
+    protected $drgenerator;
+
+    /**
+     * Set up
+     */
+    public function setUp(): void {
+        $this->generator = self::getDataGenerator()->get_plugin_generator('tool_program');
+        $this->tenantgenerator = self::getDataGenerator()->get_plugin_generator('tool_tenant');
+        $this->drgenerator = self::getDataGenerator()->get_plugin_generator('tool_dynamicrule');
+        $this->resetAfterTest();
+    }
+
+    /**
+     * Test supports_rule_types
+     */
+    public function test_supports_rule_types(): void {
+        $condition = program_suspended::instance();
+        $this->assertEquals(rule::TYPE_NORMAL + rule::TYPE_SHARED, $condition->supports_rule_types());
+    }
+
+    /**
+     * Test get_title
+     */
+    public function test_get_title(): void {
+        $condition = program_suspended::instance();
+        $this->assertNotEmpty($condition->get_title());
+    }
+
+    /**
+     * Test get_category
+     */
+    public function test_get_category(): void {
+        $condition = program_suspended::instance();
+        $this->assertEquals(get_string('pluginname', 'tool_program'), $condition->get_category());
+    }
+
+    /**
+     * Test validate_config_form
+     */
+    public function test_validate_config_form(): void {
+        $condition = program_suspended::instance();
+        $configform = ['programid' => -2];
+        $validationerrors = $condition->validate_config_form($configform);
+        $this->assertArrayHasKey('programid', $validationerrors);
+    }
+
+    /**
+     * Test status suspended condition matching
+     */
+    public function test_get_matching_users_given_suspended_status(): void {
+        [$tenant, [$user1, $user2, $user3]] = $this->tenantgenerator->create_tenant_and_users(3);
+
+        $program = $this->generator->generate_program((object)['tenantid' => $tenant->id]);
+        $programusers = $this->generator->allocate_users_to_program($program->get('id'), [$user1->id, $user2->id, $user3->id]);
+        $programuser1 = $programusers[$user1->id];
+
+        // Set program allocation to Suspended for this user.
+        $now = time();
+        $programuser1->set('status', constants::STATUS_OVERRIDE_SUSPENDED);
+        $programuser1->set('timesuspended', $now);
+        $programuser1->update();
+
+        // Test users that have program1 with status Suspended.
+        $rule = $this->drgenerator->create_rule(['enabled' => 1, 'tenantid' => $tenant->id]);
+        $configdata = ['programid' => $program->get('id')];
+        program_suspended::create($rule->id, $configdata);
+
+        $this->assertEquals(1, api::count_matching_users($rule->id));
+        $users = api::get_matching_users($rule->id);
+        $this->assertEqualsCanonicalizing([$user1->id], array_column($users, 'id'));
+
+        // Test users do not match if "on or after" date is set and they completed program before.
+        $rule = $this->drgenerator->create_rule(['enabled' => 1, 'tenantid' => $tenant->id]);
+        $date = strtotime('+1 year');
+        $configdata = [
+            'programid' => $program->get('id'),
+            'conditiondateenabled' => true,
+            'conditiondate' => $date,
+        ];
+        program_suspended::create($rule->id, $configdata);
+
+        $this->assertEquals(0, api::count_matching_users($rule->id));
+
+        // Test users match if "on or after" date is set and they completed program after.
+        $rule = $this->drgenerator->create_rule(['enabled' => 1, 'tenantid' => $tenant->id]);
+        $date = strtotime('-1 year');
+        $configdata = [
+            'programid' => $program->get('id'),
+            'conditiondateenabled' => true,
+            'conditiondate' => $date,
+        ];
+        program_suspended::create($rule->id, $configdata);
+
+        $this->assertEquals(1, api::count_matching_users($rule->id));
+        $users = api::get_matching_users($rule->id);
+        $this->assertEqualsCanonicalizing([$user1->id], array_column($users, 'id'));
+    }
+
+    /**
+     * Test shared program suspended status matching in shared rule
+     */
+    public function test_get_matching_users_given_suspended_status_shared_rule(): void {
+        $sharedspaceid = \tool_tenant\sharedspace::enable_shared_space();
+        [$tenant, [$user1, $user2, $user3]] = $this->tenantgenerator->create_tenant_and_users(3);
+        [$tenant2, [$user21, $user22, $user23]] = $this->tenantgenerator->create_tenant_and_users(3);
+
+        $program1 = $this->generator->generate_program_with_course((object)['tenantid' => $sharedspaceid]);
+        $programuser1 = $this->generator->allocate_user_to_program($program1->get('id'), $user1->id);
+        $programuser21 = $this->generator->allocate_user_to_program($program1->get('id'), $user21->id);
+        $this->generator->allocate_users_to_program($program1->get('id'), [$user2->id, $user3->id, $user22->id, $user23->id]);
+
+        // Set program allocation to Suspended.
+        $now = time();
+        $programuser1->set('status', constants::STATUS_OVERRIDE_SUSPENDED);
+        $programuser1->set('timesuspended', $now);
+        $programuser1->update();
+        $programuser21->set('status', constants::STATUS_OVERRIDE_SUSPENDED);
+        $programuser21->set('timesuspended', $now);
+        $programuser21->update();
+
+        // Test users that matched the dynamic rule condition.
+        $rule = $this->drgenerator->create_rule(['tenantid' => $sharedspaceid]);
+        $configdata = ['programid' => $program1->get('id')];
+        program_suspended::create($rule->id, $configdata);
+        $this->assertEquals(2, api::count_matching_users($rule->id));
+        $users = api::get_matching_users($rule->id);
+        $this->assertEqualsCanonicalizing([$user1->id, $user21->id], array_column($users, 'id'));
+    }
+
+    /**
+     * Test get_description
+     */
+    public function test_get_description(): void {
+        $program = $this->generator->generate_program();
+
+        $rule = $this->drgenerator->create_rule();
+        $configdata = ['programid' => $program->get('id')];
+        /** @var program_suspended $condition */
+        $condition = program_suspended::create($rule->id, $configdata);
+
+        $expectedstr = get_string('conditionprogramsuspendeddescription', 'tool_program', $program->get('fullname'));
+        $this->assertEquals($expectedstr, $condition->get_description());
+
+        // Description when date is enabled.
+        $rule = $this->drgenerator->create_rule();
+        $now = time();
+        $configdata = [
+            'programid' => $program->get('id'),
+            'conditiondateenabled' => true,
+            'conditiondate' => $now,
+        ];
+        /** @var program_suspended $condition */
+        $condition = program_suspended::create($rule->id, $configdata);
+
+        $options = ['programname' => $program->get('fullname')];
+        $options['conditiondate'] = userdate($now, get_string('strftimedatetimeshort'));
+        $expected = get_string('conditionprogramsuspendeddescriptionwithdate', 'tool_program', $options);
+        $this->assertEquals($expected, $condition->get_description());
+    }
+
+    /**
+     * Test is_configuration_valid
+     */
+    public function test_is_configuration_valid(): void {
+        $tenant = $this->tenantgenerator->create_tenant();
+        $program1 = $this->generator->generate_program((object)['tenantid' => $tenant->id]);
+        $rule1 = $this->drgenerator->create_rule(['tenantid' => $tenant->id]);
+
+        // Empty configuration.
+        $condition1 = program_suspended::create($rule1->id, []);
+        $this->assertFalse($condition1->is_configuration_valid());
+
+        // Users in program1.
+        $configdata = ['programid' => $program1->get('id')];
+        $condition1 = program_suspended::create($rule1->id, $configdata);
+
+        $this->assertTrue($condition1->is_configuration_valid());
+
+        // Test program is archived.
+        \tool_program\api::archive_program($program1);
+        $this->assertFalse($condition1->is_configuration_valid());
+
+        // Restore program.
+        \tool_program\api::restore_program($program1);
+        $this->assertTrue($condition1->is_configuration_valid());
+
+        // Delete program.
+        \tool_program\api::archive_program($program1);
+        \tool_program\api::delete_program($program1);
+        $this->assertFalse($condition1->is_configuration_valid());
+    }
+
+    /**
+     * Test user_can_add
+     */
+    public function test_user_can_add(): void {
+        $tenant = $this->tenantgenerator->create_tenant();
+
+        $program1 = $this->generator->generate_program((object)['tenantid' => $tenant->id]);
+
+        $user = self::getDataGenerator()->create_user(); // User in default tenant.
+        $this->tenantgenerator->allocate_user($user->id, $tenant->id);
+        self::setUser($user);
+
+        $rule1 = $this->drgenerator->create_rule(['tenantid' => $tenant->id]);
+        $configdata = ['programid' => $program1->get('id')];
+        program_suspended::create($rule1->id, $configdata);
+        $this->assertFalse(program_suspended::instance()->user_can_add());
+
+        $this->generator->assign_allocateuser_capability($user->id, $program1->get_context());
+        $this->assertTrue(program_suspended::instance()->user_can_add());
+    }
+
+    /**
+     * Test user_can_edit
+     */
+    public function test_user_can_edit(): void {
+        $tenant = $this->tenantgenerator->create_tenant();
+        $program1 = $this->generator->generate_program((object)['tenantid' => $tenant->id]);
+
+        $user = self::getDataGenerator()->create_user(); // User in default tenant.
+        self::setUser($user);
+
+        $rule1 = $this->drgenerator->create_rule(['tenantid' => $tenant->id]);
+        $configdata = ['programid' => $program1->get('id')];
+        program_suspended::create($rule1->id, $configdata);
+        $this->assertFalse(program_suspended::instance()->user_can_edit($configdata));
+
+        $this->generator->assign_allocateuser_capability($user->id, $program1->get_context());
+        $this->assertFalse(program_suspended::instance()->user_can_edit($configdata));
+
+        $this->tenantgenerator->allocate_user($user->id, $tenant->id);
+        $this->assertTrue(program_suspended::instance()->user_can_edit($configdata));
+    }
+}
