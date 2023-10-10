c5bc64972264398db127cc75948ff3707e2772da
Moodle Workplace <workplace@moodle.com>
2023-10-10 19:50:34 +0000

    Moodle Workplace plugin block_rbreport (build 136655)

---
+++ blocks/rbreport/db/access.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of the block_rbreport plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Rbreport block capabilities.
+ *
+ * @package    block_rbreport
+ * @author     Marina Glancy
+ * @copyright  2021 Moodle Pty Ltd <support@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    'block/rbreport:myaddinstance' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'user' => CAP_ALLOW,
+        ],
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks',
+    ],
+    'block/rbreport:addinstance' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'manager' => CAP_ALLOW,
+        ],
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks',
+    ],
+];
