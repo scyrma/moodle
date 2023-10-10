3f763f7f69a66f4209e7e309e803711ce6fac5df
Moodle Workplace <workplace@moodle.com>
2023-10-10 19:50:35 +0000

    Moodle Workplace plugin theme_workplace (build 136655)

---
+++ theme/workplace/config.php
@@ -0,0 +1,210 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the discrete and self-executable
+// collection of software scripts (plugins and modifications, and any
+// derivations thereof) that are exclusively owned and licensed by
+// Moodle Pty Ltd (Moodle) under the terms of its proprietary Moodle
+// Workplace License ("MWL") made available with Moodle's open software
+// package ("Moodle LMS") offering which itself is freely downloadable
+// at "download.moodle.org" and which is provided by Moodle under a
+// single GNU General Public License version 3.0, dated 29 June 2007
+// ("GPL"). MWL is strictly controlled by Moodle Pty Ltd and its Moodle
+// Certified Premium Partners. Wherever conflicting terms exist, the
+// terms of the MWL shall prevail.
+
+/**
+ * The configuration for theme_workplace is defined here.
+ *
+ * @package     theme_workplace
+ * @copyright   2018 Moodle Pty Ltd <support@moodle.com>
+ * @author      2018 Workplace team
+ * @license     Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(__DIR__ . '/lib.php');
+
+$THEME->name = 'workplace';
+
+$THEME->doctype = 'html5';
+
+$THEME->parents = [
+    'boost',
+];
+
+$THEME->sheets = [];
+$THEME->scss = function($theme) {
+    return theme_workplace_get_main_scss_content($theme);
+};
+$THEME->editor_sheets = [];
+$THEME->usefallback = false;
+$THEME->enable_dock = false;
+$THEME->extrascsscallback = 'theme_workplace_get_extra_scss';
+$THEME->prescsscallback = 'theme_workplace_get_pre_scss';
+$THEME->precompiledcsscallback = 'theme_boost_get_precompiled_css';
+$THEME->yuicssmodules = [];
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+$THEME->requiredblocks = '';
+$THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
+$THEME->iconsystem = \core\output\icon_system::FONTAWESOME;
+$THEME->usescourseindex = true;
+$THEME->haseditswitch = true;
+$THEME->activityheaderconfig = [
+    'notitle' => true,
+];
+// Make sure the Site Home is not shown on the primary navigation.
+$THEME->removedprimarynavitems = ['home'];
+
+$THEME->layouts = [
+    // Most backwards compatible layout without the blocks - this is the layout used by default.
+    'base' => [
+        'file' => 'drawers.php',
+        'regions' => [],
+    ],
+    // Standard layout with blocks, this is recommended for most pages with general information.
+    'standard' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    // Standard layout without main navbar and breadcrumbs.
+    'standardnonav' => [
+        'file' => 'drawers.php',
+        'regions' => ['content', 'side-pre'],
+        'defaultregion' => 'side-pre',
+        'options' => ['nomainnavbar' => true, 'nonavbar' => true],
+    ],
+    // Main course page.
+    'course' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+        'options' => ['langmenu' => true],
+    ],
+    'coursecategory' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    // Part of course, typical for modules - default page layout if $cm specified in require_login().
+    'incourse' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    // The site home page.
+    'frontpage' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+        'options' => ['nonavbar' => true],
+    ],
+    // Server administration scripts.
+    'admin' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    // My courses page.
+    'mycourses' => [
+        'file' => 'drawers.php',
+        'regions' => [],
+        'options' => [],
+    ],
+    // My dashboard page.
+    'mydashboard' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+        'options' => ['nonavbar' => true, 'langmenu' => true],
+    ],
+    // My public page.
+    'mypublic' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'login' => [
+        'file' => 'login.php',
+        'regions' => [],
+        'options' => ['langmenu' => true],
+    ],
+
+    // Pages that appear in pop-up windows - no navigation, no blocks, no header and bare activity header.
+    'popup' => [
+        'file' => 'columns1.php',
+        'regions' => [],
+        'options' => [
+            'nofooter' => true,
+            'nonavbar' => true,
+            'activityheader' => [
+                'notitle' => true,
+                'nocompletion' => true,
+                'nodescription' => true,
+            ],
+        ],
+    ],
+    // No blocks and minimal footer - used for legacy frame layouts only!
+    'frametop' => [
+        'file' => 'columns1.php',
+        'regions' => [],
+        'options' => [
+            'nofooter' => true,
+            'nocoursefooter' => true,
+            'activityheader' => [
+                'nocompletion' => true,
+            ],
+        ],
+    ],
+    // Embeded pages, like iframe/object embeded in moodleform - it needs as much space as possible.
+    'embedded' => [
+        'file' => 'embedded.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    // Used during upgrade and install, and for the 'This site is undergoing maintenance' message.
+    // This must not have any blocks, links, or API calls that would lead to database or cache interaction.
+    // Please be extremely careful if you are modifying this layout.
+    'maintenance' => [
+        'file' => 'maintenance.php',
+        'regions' => [],
+    ],
+    // Should display the content and basic headers only.
+    'print' => [
+        'file' => 'columns1.php',
+        'regions' => [],
+        'options' => ['nofooter' => true, 'nonavbar' => false, 'noactivityheader' => true],
+    ],
+    // The pagelayout used when a redirection is occuring.
+    'redirect' => [
+        'file' => 'embedded.php',
+        'regions' => [],
+    ],
+    // The pagelayout used for reports.
+    'report' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    // The pagelayout used for safebrowser and securewindow.
+    'secure' => [
+        'file' => 'secure.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+];
