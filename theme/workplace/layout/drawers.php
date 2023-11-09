e4afb6b4b67fa0e9765196aa9d88338bdd2e40c2
Moodle Workplace <workplace@moodle.com>
2023-11-09 11:24:11 +0000

    Moodle Workplace plugin theme_workplace (build 139539)

---
+++ theme/workplace/layout/drawers.php
@@ -0,0 +1,127 @@
+<?php
+// This file is part of Moodle Workplace https://moodle.com/workplace based on Moodle
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Moodle Workplace™ Code is the discrete and self-executable
+// collection of software scripts (plugins and modifications, and any
+// derivations thereof) that are exclusively owned and licensed by
+// Moodle Pty Ltd (Moodle) under the terms of its proprietary Moodle
+// Workplace License ("MWL") made available with Moodle's open software
+// package ("Moodle LMS") offering which itself is freely downloadable
+// at "download.moodle.org" and which is provided by Moodle under a
+// single GNU General Public License version 3.0, dated 29 June 2007
+// ("GPL"). MWL is strictly controlled by Moodle Pty Ltd and its Moodle
+// Certified Premium Partners. Wherever conflicting terms exist, the
+// terms of the MWL shall prevail.
+
+/**
+ * A drawer based layout for the workplace theme.
+ *
+ * @uses \tool_wp\workplace::workplace_license_redirect()
+ *
+ * @package   theme_workplace
+ * @copyright 2021 Moodle Pty Ltd <support@moodle.com>
+ * @author    2021 Workplace team
+ * @license   Moodle Workplace License, distribution is restricted, contact support@moodle.com
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $PAGE;
+
+require_once($CFG->libdir . '/behat/lib.php');
+require_once($CFG->dirroot . '/course/lib.php');
+
+// Add block button in editing mode.
+$addblockbutton = $OUTPUT->addblockbutton();
+
+if (isloggedin()) {
+    $courseindexopen = (get_user_preferences('drawer-open-index', true) == true);
+    $blockdraweropen = (get_user_preferences('drawer-open-block') == true);
+} else {
+    $courseindexopen = false;
+    $blockdraweropen = false;
+}
+
+if (defined('BEHAT_SITE_RUNNING')) {
+    $blockdraweropen = true;
+}
+
+$extraclasses = ['uses-drawers'];
+if ($courseindexopen) {
+    $extraclasses[] = 'drawer-open-index';
+}
+
+$blockshtml = $OUTPUT->blocks('side-pre');
+$hasblocks = (strpos($blockshtml, 'data-block=') !== false || !empty($addblockbutton));
+if (!$hasblocks) {
+    $blockdraweropen = false;
+}
+$courseindex = core_course_drawer();
+if (!$courseindex) {
+    $courseindexopen = false;
+}
+
+$bodyattributes = $OUTPUT->body_attributes($extraclasses);
+$boostrenderer = new \theme_boost\output\core_renderer($PAGE, null);
+$forceblockdraweropen = $boostrenderer->firstview_fakeblocks();
+
+$secondarynavigation = false;
+$overflow = '';
+if ($PAGE->has_secondary_navigation()) {
+    $tablistnav = $PAGE->has_tablist_secondary_navigation();
+    $moremenu = new \core\navigation\output\more_menu($PAGE->secondarynav, 'nav-tabs', true, $tablistnav);
+    $secondarynavigation = $moremenu->export_for_template($OUTPUT);
+    $overflowdata = $PAGE->secondarynav->get_overflow_menu_data();
+    if (!is_null($overflowdata)) {
+        $overflow = $overflowdata->export_for_template($OUTPUT);
+    }
+}
+
+$primary = new core\navigation\output\primary($PAGE);
+$renderer = $PAGE->get_renderer('core');
+$primarymenu = $primary->export_for_template($renderer);
+$buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_actions() && !$PAGE->has_secondary_navigation();
+// If the settings menu will be included in the header then don't add it here.
+$regionmainsettingsmenu = $buildregionmainsettings ? $OUTPUT->region_main_settings_menu() : false;
+
+$header = $PAGE->activityheader;
+$headercontent = $header->export_for_template($renderer);
+
+$templatecontext = [
+    'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
+    'output' => $OUTPUT,
+    'sidepreblocks' => $blockshtml,
+    'hasblocks' => $hasblocks,
+    'bodyattributes' => $bodyattributes,
+    'courseindexopen' => $courseindexopen,
+    'blockdraweropen' => $blockdraweropen,
+    'courseindex' => $courseindex,
+    'primarymoremenu' => $primarymenu['moremenu'],
+    'secondarymoremenu' => $secondarynavigation ?: false,
+    'mobileprimarynav' => $primarymenu['mobileprimarynav'],
+    'usermenu' => $primarymenu['user'],
+    'langmenu' => $primarymenu['lang'],
+    'forceblockdraweropen' => $forceblockdraweropen,
+    'regionmainsettingsmenu' => $regionmainsettingsmenu,
+    'hasregionmainsettingsmenu' => !empty($regionmainsettingsmenu),
+    'overflow' => $overflow,
+    'headercontent' => $headercontent,
+    'addblockbutton' => $addblockbutton,
+    'hasnavbar' => empty($PAGE->layout_options['nomainnavbar']),
+];
+
+component_class_callback('tool_wp\workplace', 'workplace_license_redirect', [$PAGE->url]);
+echo $OUTPUT->render_from_template('theme_boost/drawers', $templatecontext);
